b4d0dd2447fbd85d50dc859ac0c2df4359f660b9
imkiva <libkernelpanic@gmail.com>
2020-10-20 20:40:59 +0800

    DOC: implement Flatten#flatDoc(Doc)

--- pretty/src/main/java/org/mzi/pretty/doc/Flatten.java
+++ pretty/src/main/java/org/mzi/pretty/doc/Flatten.java
@@ -16,8 +16,59 @@ sealed interface Flatten {
   }
 
   static Flatten flatDoc(@NotNull Doc doc) {
-    // TODO: flat doc
-    return new NeverFlat();
+    if (doc instanceof Doc.FlatAlt alt) {
+      return new Flattened(flatten(alt.preferWhenFlatten()));
+
+    } else if (doc instanceof Doc.Line) {
+      return new NeverFlat();
+
+    } else if (doc instanceof Doc.Union u) {
+      return new Flattened(u.first());
+
+    } else if (doc instanceof Doc.Nest n) {
+      var result = flatDoc(n.doc());
+      if (result instanceof Flattened f) {
+        return new Flattened(new Doc.Nest(n.indent(), f.flattenedDoc()));
+      } else {
+        return result;
+      }
+
+    } else if (doc instanceof Doc.Cat c) {
+      return flatCat(c);
+
+    } else if (doc instanceof Doc.Empty
+      || doc instanceof Doc.PlainText
+      || doc instanceof Doc.HyperText) {
+      return new AlreadyFlat();
+
+    } else if (doc instanceof Doc.Fail) {
+      return new NeverFlat();
+    }
+
+    throw new IllegalStateException("unreachable");
+  }
+
+  private static @NotNull Flatten flatCat(@NotNull Doc.Cat cat) {
+    var l = flatDoc(cat.first());
+    var r = flatDoc(cat.second());
+
+    if (l instanceof NeverFlat || r instanceof NeverFlat) {
+      return new NeverFlat();
+    } else if (l instanceof AlreadyFlat && r instanceof AlreadyFlat) {
+      return new AlreadyFlat();
+    }
+
+    if (l instanceof Flattened x) {
+      if (r instanceof Flattened y) {
+        return new Flattened(new Doc.Cat(x.flattenedDoc(), y.flattenedDoc()));
+      } else if (r instanceof AlreadyFlat) {
+        return new Flattened(new Doc.Cat(x.flattenedDoc(), cat.second()));
+      }
+    } else if (l instanceof AlreadyFlat && r instanceof Flattened y) {
+      return new Flattened(new Doc.Cat(cat.first(), y.flattenedDoc()));
+    }
+
+    throw new IllegalStateException("unreachable");
   }
 
   /**
