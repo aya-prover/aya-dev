6f0d7dde88bcd78c1cf30d98d4e142fdf3f400c9
imkiva <libkernelpanic@gmail.com>
2020-10-20 13:38:07 +0800

    PRETTY: empty() and nest() factory function

--- pretty/src/main/java/org/mzi/pretty/doc/Doc.java
+++ pretty/src/main/java/org/mzi/pretty/doc/Doc.java
@@ -1,11 +1,14 @@
 package org.mzi.pretty.doc;
 
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * @author kiva
  */
 public sealed interface Doc {
+  /// --------------------------- Doc Variants
+
   /**
    * The empty document; conceptually the unit of 'Cat'
    */
@@ -43,5 +46,27 @@ public sealed interface Doc {
    * Document indented by a number of columns
    */
   record Nest(int indent, @NotNull Doc doc) implements Doc {
+  }
+
+  /// --------------------------- DocFactory functions
+  @Contract("-> new")
+  static @NotNull Doc empty() {
+    return new Empty();
+  }
+
+  /**
+   * lays out the document {@param doc} with the current nesting level
+   * (indentation of the following lines) increased by {@param indent}.
+   * Negative values are allowed, and decrease the nesting level accordingly.
+   *
+   * @param indent indentation of the following lines
+   * @param doc    the document to lay out
+   * @return indented document
+   */
+  @Contract("_, _ -> new")
+  static @NotNull Doc nest(int indent, @NotNull Doc doc) {
+    return indent == 0
+      ? doc
+      : new Nest(indent, doc);
   }
 }
