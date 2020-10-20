838dc679e3a4517f49a103c7f3581887f932cfe0
Xy Ren <xy.r@outlook.com>
2020-10-20 22:20:29 +0800

    CONCRETE: Add Param class

---
+++ base/src/main/java/org/mzi/concrete/term/Param.java
@@ -0,0 +1,15 @@
+package org.mzi.concrete.term;
+
+import org.jetbrains.annotations.NotNull;
+import org.mzi.api.ref.Ref;
+
+/**
+ * @author re-xyr
+ */
+public record Param(
+  @NotNull Ref ref,
+  @NotNull Expr type,
+  boolean explicit
+) {
+  // TODO[xyr]: is it better to implement a Tele-like class?
+}
