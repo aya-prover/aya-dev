1a90ec058f765e92b4e6928afab75feeaf85832e
Xy Ren <xy.r@outlook.com>
2020-10-20 22:35:10 +0800

    CONCRETE: Add Param.Visitor (it looks strange now. may need improvement)

--- base/src/main/java/org/mzi/concrete/term/Param.java
+++ base/src/main/java/org/mzi/concrete/term/Param.java
@@ -1,5 +1,6 @@
 package org.mzi.concrete.term;
 
+import asia.kala.collection.immutable.ImmutableSeq;
 import org.jetbrains.annotations.NotNull;
 import org.mzi.api.ref.Ref;
 
@@ -11,5 +12,8 @@ public record Param(
   @NotNull Expr type,
   boolean explicit
 ) {
+  public interface Visitor<P, R> {
+    R visitParams(@NotNull ImmutableSeq<Param> params, P p);
+  }
   // TODO[xyr]: is it better to implement a Tele-like class?
 }
