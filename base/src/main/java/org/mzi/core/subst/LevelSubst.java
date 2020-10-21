d049fa1b6f9ed14259f0ee05f0e7985827351f81
ice1000 <ice1000kotlin@foxmail.com>
2020-10-22 00:07:17 +0800

    SORT: introduce LevelSubst

---
+++ base/src/main/java/org/mzi/core/subst/LevelSubst.java
@@ -0,0 +1,76 @@
+package org.mzi.core.subst;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.mzi.api.ref.Ref;
+import org.mzi.ref.LevelVar;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.mzi.core.term.UnivTerm.Level;
+
+/**
+ * References in Arend:
+ * <a href="https://github.com/JetBrains/Arend/blob/master/base/src/main/java/org/arend/core/subst/StdLevelSubstitution.java"
+ * >StdLevelSubstitution.java</a>,
+ * <a href="https://github.com/JetBrains/Arend/blob/master/base/src/main/java/org/arend/core/subst/SimpleLevelSubstitution.java"
+ * >SimpleLevelSubstitution.java</a>, etc.
+ */
+public interface LevelSubst {
+  boolean isEmpty();
+  @Nullable Level get(@NotNull Ref ref);
+  @NotNull LevelSubst subst(@NotNull LevelSubst subst);
+  @NotNull LevelSubst EMPTY = new Simple(Collections.emptyMap());
+
+  class Std implements LevelSubst {
+      private final @NotNull Level uLevel;
+      private final @NotNull Level hLevel;
+
+      public Std(@NotNull Level uLevel, @NotNull Level hLevel) {
+        this.uLevel = uLevel;
+        this.hLevel = hLevel;
+      }
+
+      @Override public boolean isEmpty() {
+        return uLevel.var() == LevelVar.UP && uLevel.varOnly() && hLevel.var() == LevelVar.HP && hLevel.varOnly();
+      }
+
+      @Override public Level get(@NotNull Ref ref) {
+        return ref == LevelVar.UP ? uLevel : ref == LevelVar.HP ? hLevel : null;
+      }
+
+      @Override
+      public @NotNull LevelSubst subst(@NotNull LevelSubst substitution) {
+        return new Std(uLevel.subst(substitution), hLevel.subst(substitution));
+      }
+  }
+
+  class Simple implements LevelSubst {
+    private final @NotNull Map<@NotNull Ref, @NotNull Level> map;
+
+    public Simple(@NotNull Map<@NotNull Ref, @NotNull Level> map) {
+      this.map = map;
+    }
+
+    public void add(@NotNull Ref ref, @NotNull Level level) {
+      map.put(ref, level);
+    }
+
+    @Override public boolean isEmpty() {
+      return map.isEmpty();
+    }
+
+    @Override public @Nullable Level get(@NotNull Ref ref) {
+      return map.get(ref);
+    }
+
+    @Override public @NotNull LevelSubst subst(@NotNull LevelSubst subst) {
+      if (isEmpty()) return this;
+      var result = new Simple(new HashMap<>());
+      for (var entry : map.entrySet()) result.map.put(entry.getKey(), entry.getValue().subst(subst));
+      return result;
+    }
+  }
+}
--- base/src/main/java/org/mzi/core/term/UnivTerm.java
+++ base/src/main/java/org/mzi/core/term/UnivTerm.java
@@ -1,8 +1,10 @@
 package org.mzi.core.term;
 
+import asia.kala.collection.Seq;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.mzi.core.subst.LevelSubst;
 import org.mzi.ref.LevelVar;
 import org.mzi.util.Decision;
 
@@ -47,5 +49,89 @@ public record UnivTerm() implements Term {
     @Contract(pure = true) public boolean isProp() {
       return closed() && constant == -1;
     }
+
+    @Contract(pure = true) public boolean withMaxConstant() {
+      return var != null && (max > 0 || var.kind() == LevelVar.Kind.H && max == 0);
+    }
+
+    @Contract(pure = true) public boolean varOnly() {
+      return var != null && constant == 0 && !withMaxConstant();
+    }
+
+    @Contract(pure = true) public int maxAddConstant() {
+      return constant + max;
+    }
+
+    @Contract(pure = true) public @NotNull Level add(int constant) {
+      return constant == 0 || this == INF ? this : new Level(var, this.constant + constant, max);
+    }
+
+    public @Nullable Level max(@NotNull Level level) {
+      if (Seq.of(this, level).contains(INF)) return INF;
+
+      if (var != null && level.var != null) {
+        if (var == level.var) {
+          int newConstant = Math.max(constant, level.constant);
+          return new Level(var, newConstant,
+            Math.max(constant + max, level.constant + level.max) - newConstant);
+        } else return null;
+      }
+      if (var == null && level.var == null) return new Level(Math.max(constant, level.constant));
+
+      int newConstant = var == null ? constant : level.constant;
+      Level lvl = var == null ? level : this;
+      return newConstant <= lvl.maxAddConstant() ? lvl :
+        new Level(lvl.var, lvl.constant,
+          Math.max(lvl.max, newConstant - lvl.constant));
+    }
+
+    public Level subst(@NotNull LevelSubst subst) {
+      if (var == null || this == INF) return this;
+      var level = subst.get(var);
+      if (level == null) return this;
+      if (level == INF || varOnly()) return level;
+      if (level.var != null)
+        return new Level(level.var, level.constant + constant,
+          Math.max(level.max, max - level.constant));
+      else return new Level(Math.max(level.constant, max) + constant);
+    }
+
+    /*
+    public static boolean compare(Level level1, Level level2, CMP cmp, Equations equations, Concrete.SourceNode sourceNode) {
+      if (cmp == CMP.GE) {
+        return compare(level2, level1, CMP.LE, equations, sourceNode);
+      }
+
+      if (level1 == INF) {
+        return level2 == INF || !level2.closed() && (equations == null || equations.addEquation(INFINITY, level2, CMP.LE, sourceNode));
+      }
+      if (level2 == INF) {
+        return cmp == CMP.LE || !level1.closed() && (equations == null || equations.addEquation(INFINITY, level1, CMP.LE, sourceNode));
+      }
+
+      if (level2.var() == null && level1.var() != null && !(level1.var() instanceof InferenceLevelVariable))
+        return false;
+
+      if (level1.var() == null && cmp == CMP.LE) {
+        if (level1.constant <= level2.constant + level2.max) {
+          return true;
+        }
+      }
+
+      if (level1.var() == level2.var()) {
+        if (cmp == CMP.LE) {
+          return level1.constant <= level2.constant && level1.getMaxAddedConstant() <= level2.getMaxAddedConstant();
+        } else {
+          return level1.constant == level2.constant && level1.getMaxConstant() == level2.getMaxConstant();
+        }
+      } else {
+        if (equations == null) {
+          return level1.var() instanceof InferenceLevelVariable || level2.var() instanceof InferenceLevelVariable;
+        } else {
+          return equations.addEquation(level1, level2, cmp, sourceNode);
+        }
+      }
+    }
+    */
   }
 }
