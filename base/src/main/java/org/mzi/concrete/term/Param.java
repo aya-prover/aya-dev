86fed169c294da8adddfd1caa5af9ae87ba5cec4
ice1000 <ice1000kotlin@foxmail.com>
2020-10-22 04:31:08 +0800

    CONCRETE: fix TODOs, closes #35

--- base/src/main/java/org/mzi/concrete/term/Param.java
+++ base/src/main/java/org/mzi/concrete/term/Param.java
@@ -1,6 +1,5 @@
 package org.mzi.concrete.term;
 
-import asia.kala.collection.immutable.ImmutableSeq;
 import org.jetbrains.annotations.NotNull;
 import org.mzi.api.ref.Ref;
 
@@ -12,8 +11,4 @@ public record Param(
   @NotNull Expr type,
   boolean explicit
 ) {
-  public interface Visitor<P, R> {
-    R visitParams(@NotNull ImmutableSeq<Param> params, P p);
-  }
-  // TODO[xyr]: is it better to implement a Tele-like class?
 }
