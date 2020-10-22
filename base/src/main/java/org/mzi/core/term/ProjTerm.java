17ab5b826ccb3f32b00a7715982384004b16db03
ice1000 <ice1000kotlin@foxmail.com>
2020-10-23 04:45:34 +0800

    CORE: minor

--- base/src/main/java/org/mzi/core/term/ProjTerm.java
+++ base/src/main/java/org/mzi/core/term/ProjTerm.java
@@ -1,19 +1,19 @@
 package org.mzi.core.term;
 
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.mzi.util.Decision;
 
 /**
  * @author re-xyr
  */
-public record ProjTerm(@NotNull Term tup, @NotNull int ix) implements Term {
-  @Override
-  public <P, R> R accept(@NotNull Visitor<P, R> visitor, P p) {
+public record ProjTerm(@NotNull Term tup, int ix) implements Term {
+  @Override public <P, R> R accept(@NotNull Visitor<P, R> visitor, P p) {
     return visitor.visitProj(this, p);
   }
 
-  @Override
-  public @NotNull Decision whnf() {
-    return Decision.NO;
+  @Contract(pure = true) @Override public @NotNull Decision whnf() {
+    if (tup instanceof TupTerm) return Decision.NO;
+    else return tup.whnf();
   }
 }
