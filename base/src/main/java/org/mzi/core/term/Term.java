bf4153e3b2dbd0e1bc647fc3a7e7190846583a2c
ice1000 <ice1000kotlin@foxmail.com>
2020-10-24 12:47:53 +0800

    COPYRIGHT: add

--- base/src/main/java/org/mzi/core/term/Term.java
+++ base/src/main/java/org/mzi/core/term/Term.java
@@ -1,3 +1,5 @@
+// Copyright (c) 2020-2020 Yinsen (Tesla) Zhang.
+// Use of this source code is governed by the Apache-2.0 license that can be found in the LICENSE file.
 package org.mzi.core.term;
 
 import asia.kala.Unit;
--- tester/build.gradle.kts
+++ tester/build.gradle.kts
@@ -1,3 +1,5 @@
+// Copyright (c) 2020-2020 Yinsen (Tesla) Zhang.
+// Use of this source code is governed by the Apache-2.0 license that can be found in the LICENSE file.
 dependencies {
   api("org.junit.jupiter:junit-jupiter:5.7.0")
   api("org.hamcrest:hamcrest:2.2")
