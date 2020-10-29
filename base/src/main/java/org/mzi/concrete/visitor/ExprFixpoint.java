5b255f9077feca79f6ad15a502caf1151968f293
imkiva <libkernelpanic@gmail.com>
2020-10-29 13:08:53 +0800

    CONCRETE: use Objects.equals()

--- base/src/main/java/org/mzi/concrete/visitor/ExprFixpoint.java
+++ base/src/main/java/org/mzi/concrete/visitor/ExprFixpoint.java
@@ -8,6 +8,8 @@ import org.mzi.concrete.Expr;
 import org.mzi.concrete.Param;
 import org.mzi.generic.Arg;
 
+import java.util.Objects;
+
 public interface ExprFixpoint<P> extends Expr.Visitor<P, @NotNull Expr> {
   @Override default @NotNull Expr visitRef(Expr.@NotNull RefExpr expr, P p) {
     return expr;
@@ -19,14 +21,14 @@ public interface ExprFixpoint<P> extends Expr.Visitor<P, @NotNull Expr> {
 
   @Override default @NotNull Expr visitHole(Expr.@NotNull HoleExpr expr, P p) {
     var h = expr.filling() != null ? expr.filling().accept(this, p) : null;
-    if (h == expr.filling()) return expr;
+    if (Objects.equals(h, expr.filling())) return expr;
     return new Expr.HoleExpr(expr.sourcePos(), expr.name(), h);
   }
 
   default @NotNull Buffer<Param> visitParams(@NotNull Buffer<Param> params, P p) {
     return params.view().map(param -> {
       var type = param.type().accept(this, p);
-      if (type == param.type()) return param;
+      if (Objects.equals(type, param.type())) return param;
       return new Param(param.sourcePos(), param.vars(), type, param.explicit());
     }).collect(Buffer.factory());
   }
@@ -34,14 +36,14 @@ public interface ExprFixpoint<P> extends Expr.Visitor<P, @NotNull Expr> {
   @Override default @NotNull Expr visitLam(Expr.@NotNull LamExpr expr, P p) {
     var binds = visitParams(expr.params(), p);
     var body = expr.body().accept(this, p);
-    if (binds.sameElements(expr.params(), true) && body == expr.body()) return expr;
+    if (binds.sameElements(expr.params(), true) && Objects.equals(body, expr.body())) return expr;
     return new Expr.LamExpr(expr.sourcePos(), binds, body);
   }
 
   @Override default @NotNull Expr visitPi(Expr.@NotNull PiExpr expr, P p) {
     var binds = visitParams(expr.params(), p);
     var last = expr.last().accept(this, p);
-    if (binds.sameElements(expr.params(), true) && last == expr.last()) return expr;
+    if (binds.sameElements(expr.params(), true) && Objects.equals(last, expr.last())) return expr;
     return new Expr.PiExpr(expr.sourcePos(), binds, last, expr.kind());
   }
 
@@ -57,14 +59,14 @@ public interface ExprFixpoint<P> extends Expr.Visitor<P, @NotNull Expr> {
 
   default @NotNull Arg<Expr> visitArg(@NotNull Arg<Expr> arg, P p) {
     var term = arg.term().accept(this, p);
-    if (term == arg.term()) return arg;
+    if (Objects.equals(term, arg.term())) return arg;
     return new Arg<>(term, arg.explicit());
   }
 
   @Override default @NotNull Expr visitApp(Expr.@NotNull AppExpr expr, P p) {
     var function = expr.function().accept(this, p);
     var arg = expr.argument().map(x -> visitArg(x, p));
-    if (function == expr.function() && arg.sameElements(expr.argument(), true)) return expr;
+    if (Objects.equals(function, expr.function()) && arg.sameElements(expr.argument(), true)) return expr;
     return new Expr.AppExpr(expr.sourcePos(), function, arg);
   }
 
@@ -76,13 +78,13 @@ public interface ExprFixpoint<P> extends Expr.Visitor<P, @NotNull Expr> {
 
   @Override default @NotNull Expr visitProj(Expr.@NotNull ProjExpr expr, P p) {
     var tup = expr.tup().accept(this, p);
-    if (tup == expr.tup()) return expr;
+    if (Objects.equals(tup, expr.tup())) return expr;
     return new Expr.ProjExpr(expr.sourcePos(), tup, expr.ix());
   }
 
   @Override default @NotNull Expr visitTyped(Expr.@NotNull TypedExpr expr, P p) {
     var e = expr.expr().accept(this, p);
-    if (e == expr.expr()) return expr;
+    if (Objects.equals(e, expr.expr())) return expr;
     return new Expr.TypedExpr(expr.sourcePos(), e, expr.type());
   }
 }
