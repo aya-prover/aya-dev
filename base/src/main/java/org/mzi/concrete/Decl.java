a8b23e17ed5fce250ce7f77136178fc3f17c36f9
imkiva <libkernelpanic@gmail.com>
2020-10-29 15:07:26 +0800

    CONCRETE: Decl.DataDecl

--- base/src/main/java/org/mzi/concrete/Decl.java
+++ base/src/main/java/org/mzi/concrete/Decl.java
@@ -2,6 +2,7 @@
 // Use of this source code is governed by the Apache-2.0 license that can be found in the LICENSE file.
 package org.mzi.concrete;
 
+import asia.kala.Tuple2;
 import asia.kala.collection.mutable.Buffer;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
@@ -20,6 +21,61 @@ import java.util.EnumSet;
  */
 public sealed interface Decl extends Stmt {
   @Contract(pure = true) @NotNull DefVar<? extends Decl> ref();
+
+  sealed interface DataBody {
+    record DataCtor(
+      @NotNull String name,
+      @NotNull Buffer<Param> telescope,
+      @NotNull Buffer<String> elim,
+      @NotNull Buffer<Clause> clauses,
+      boolean coerce
+    ) implements DataBody {}
+
+    record DataClause(
+      @NotNull Buffer<String> elim,
+      @NotNull Buffer<Tuple2<Pattern, DataCtor>> clauses
+    ) implements DataBody {}
+  }
+
+  /**
+   * concrete data definition
+   *
+   * @author kiva
+   */
+  final class DataDecl implements Decl {
+    public final @NotNull SourcePos sourcePos;
+    public final @NotNull DefVar<DataDecl> ref;
+    public final @NotNull Buffer<Param> telescope;
+    public final @NotNull Expr result;
+    public final @NotNull Buffer<DataBody> body;
+    public final @NotNull Buffer<Stmt> abuseBlock;
+
+    public DataDecl(
+      @NotNull SourcePos sourcePos,
+      @NotNull String name,
+      @NotNull Buffer<Param> telescope,
+      @NotNull Expr result,
+      @NotNull Buffer<DataBody> body,
+      @NotNull Buffer<Stmt> abuseBlock
+    ) {
+      this.sourcePos = sourcePos;
+      this.telescope = telescope;
+      this.result = result;
+      this.body = body;
+      this.abuseBlock = abuseBlock;
+      this.ref = new DefVar<>(this, name);
+    }
+
+    @Override
+    public @NotNull DefVar<? extends Decl> ref() {
+      return this.ref;
+    }
+
+    @Override
+    public @NotNull SourcePos sourcePos() {
+      return this.sourcePos;
+    }
+  }
 
   /**
    * concrete function definition, corresponding to {@link org.mzi.core.def.FnDef}.
