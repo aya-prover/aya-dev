171c1877a951d1ff4cd235908898b4c4777e77d0
ice1000 <ice1000kotlin@foxmail.com>
2020-10-30 17:02:39 +0800

    refix #70: also tester

--- tester/build.gradle.kts
+++ tester/build.gradle.kts
@@ -1,8 +1,9 @@
 // Copyright (c) 2020-2020 Yinsen (Tesla) Zhang.
 // Use of this source code is governed by the Apache-2.0 license that can be found in the LICENSE file.
 dependencies {
-  api("org.junit.jupiter:junit-jupiter:5.7.0")
-  api("org.hamcrest:hamcrest:2.2")
+  val deps: java.util.Properties by rootProject.ext
+  api("org.junit.jupiter:junit-jupiter:${deps.getProperty("version.junit")}")
+  api("org.hamcrest:hamcrest:${deps.getProperty("version.hamcrest")}")
   api(project(":base"))
   implementation(project(":parser"))
 }
