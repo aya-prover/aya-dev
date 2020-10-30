39f0433e7b13d32993a8498555a2edfcdac7b187
ice1000 <ice1000kotlin@foxmail.com>
2020-10-31 02:31:52 +0800

    MISC: move file

--- base/src/test/java/org/mzi/tyck/DefEqTest.java
+++ base/src/test/java/org/mzi/tyck/DefEqTest.java
@@ -1,6 +1,6 @@
 // Copyright (c) 2020-2020 Yinsen (Tesla) Zhang.
 // Use of this source code is governed by the Apache-2.0 license that can be found in the LICENSE file.
-package org.mzi.core;
+package org.mzi.tyck;
 
 import asia.kala.collection.mutable.Buffer;
 import org.jetbrains.annotations.NotNull;
