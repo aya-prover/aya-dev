d4e415913a0cda67f7a1e275fbc610899b350698
ice1000 <ice1000kotlin@foxmail.com>
2020-10-31 18:41:41 +0800

    MISC: cosmetics

--- tester/build.gradle.kts
+++ tester/build.gradle.kts
@@ -2,8 +2,8 @@
 // Use of this source code is governed by the Apache-2.0 license that can be found in the LICENSE file.
 dependencies {
   val deps: java.util.Properties by rootProject.ext
-  api("org.junit.jupiter:junit-jupiter:${deps.getProperty("version.junit")}")
-  api("org.hamcrest:hamcrest:${deps.getProperty("version.hamcrest")}")
+  api("org.junit.jupiter", "junit-jupiter", version = deps.getProperty("version.junit"))
+  api("org.hamcrest", "hamcrest", version = deps.getProperty("version.hamcrest"))
   api(project(":base"))
   implementation(project(":parser"))
 }
