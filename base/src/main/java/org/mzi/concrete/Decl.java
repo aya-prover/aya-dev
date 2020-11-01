343fb895819b4894ec4289a130f24e3686606ea6
ice1000 <ice1000kotlin@foxmail.com>
2020-11-02 02:58:13 +0800

    ASSOC: base -> api

--- base/src/main/java/org/mzi/concrete/Decl.java
+++ base/src/main/java/org/mzi/concrete/Decl.java
@@ -8,7 +8,7 @@ import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.mzi.api.error.SourcePos;
-import org.mzi.generic.Assoc;
+import org.mzi.api.util.Assoc;
 import org.mzi.generic.Modifier;
 import org.mzi.ref.DefVar;
 
