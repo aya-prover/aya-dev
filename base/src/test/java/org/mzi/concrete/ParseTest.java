3a86d774031a0dccf7748a07f7eeb2bee494959a
ice1000 <ice1000kotlin@foxmail.com>
2020-11-02 02:16:42 +0800

    TEST: sorry, I was stupid.

--- base/src/test/java/org/mzi/concrete/ParseTest.java
+++ base/src/test/java/org/mzi/concrete/ParseTest.java
@@ -3,10 +3,12 @@
 package org.mzi.concrete;
 
 import asia.kala.collection.immutable.ImmutableSeq;
+import asia.kala.collection.immutable.ImmutableVector;
 import asia.kala.collection.mutable.Buffer;
 import org.intellij.lang.annotations.Language;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mzi.api.Global;
 import org.mzi.api.error.SourcePos;
@@ -22,115 +24,111 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ParseTest {
+  @BeforeAll public static void enableTest() {
+    Global.enterTestMode();
+  }
+
   @Test
   public void successCmd() {
-    Global.runInTestMode(() -> {
-      parseCmd("\\open A");
-      parseCmd("\\open A.B");
-      parseCmd("\\open A \\using ()");
-      parseCmd("\\open A \\using () \\hiding ()");
-      parseCmd("\\open A \\hiding ()");
-      parseCmd("\\open A \\hiding () \\using ()");
-      parseCmd("\\import A");
-      parseCmd("\\import A.B");
-      parseCmd("\\import A.B \\using ()");
-      parseTo("\\open Boy.Next.Door \\hiding (boy) \\using (door)", new Stmt.CmdStmt(
-        SourcePos.NONE,
-        Stmt.CmdStmt.Cmd.Open,
-        "Boy.Next.Door",
-        ImmutableSeq.of("door"),
-        ImmutableSeq.of("boy")
-      ));
-    });
+    parseCmd("\\open A");
+    parseCmd("\\open A.B");
+    parseCmd("\\open A \\using ()");
+    parseCmd("\\open A \\using () \\hiding ()");
+    parseCmd("\\open A \\hiding ()");
+    parseCmd("\\open A \\hiding () \\using ()");
+    parseCmd("\\import A");
+    parseCmd("\\import A.B");
+    parseCmd("\\import A.B \\using ()");
+    parseTo("\\open Boy.Next.Door \\hiding (boy) \\using (door)", new Stmt.CmdStmt(
+      SourcePos.NONE,
+      Stmt.CmdStmt.Cmd.Open,
+      "Boy.Next.Door",
+      ImmutableVector.of("door"),
+      ImmutableVector.of("boy")
+    ));
   }
 
   @Test
   public void successLiteral() {
-    Global.runInTestMode(() -> {
-      assertTrue(MziProducer.parseExpr("diavolo") instanceof Expr.UnresolvedExpr);
-      parseUniv("\\Prop");
-      parseUniv("\\Set");
-      parseUniv("\\Set0");
-      parseUniv("\\Set233");
-      parseUniv("\\2-Type");
-      parseUniv("\\2-Type2");
-      parseUniv("\\114-Type514");
-      parseUniv("\\hType2");
-      parseUniv("\\h-Type2");
-      parseUniv("\\oo-Type2");
-    });
+    assertTrue(MziProducer.parseExpr("diavolo") instanceof Expr.UnresolvedExpr);
+    parseUniv("\\Prop");
+    parseUniv("\\Set");
+    parseUniv("\\Set0");
+    parseUniv("\\Set233");
+    parseUniv("\\2-Type");
+    parseUniv("\\2-Type2");
+    parseUniv("\\114-Type514");
+    parseUniv("\\hType2");
+    parseUniv("\\h-Type2");
+    parseUniv("\\oo-Type2");
   }
 
   @Test
   public void successDecl() {
-    Global.runInTestMode(() -> {
-      assertTrue(MziProducer.parseDecl("\\def a => 1") instanceof Decl.FnDecl);
-      assertTrue(MziProducer.parseDecl("\\def a (b : X) => b") instanceof Decl.FnDecl);
-      assertTrue(MziProducer.parseDecl("\\def a (f : \\Pi a b c d -> a) => b") instanceof Decl.FnDecl);
-      assertTrue(MziProducer.parseDecl("\\def a (t : \\Sigma a b ** s) => b") instanceof Decl.FnDecl);
-      assertTrue(MziProducer.parseDecl("\\data Unit") instanceof Decl.DataDecl);
-      assertTrue(MziProducer.parseDecl("\\data Unit \\abusing {}") instanceof Decl.DataDecl);
-      assertTrue(MziProducer.parseDecl("\\data Unit : A \\abusing {}") instanceof Decl.DataDecl);
-      assertTrue(MziProducer.parseDecl("\\data T {A : \\114-Type514} : A \\abusing {}") instanceof Decl.DataDecl);
-      parseTo("\\def id {A : \\114-Type514} (a : A) : A => a", new Decl.FnDecl(
-        SourcePos.NONE,
-        EnumSet.noneOf(Modifier.class),
-        null,
-        "id",
-        Buffer.of(
-          new Param(SourcePos.NONE, Buffer.of(new LocalVar("A")), new Expr.UnivExpr(SourcePos.NONE, 514, 114), false),
-          new Param(SourcePos.NONE, Buffer.of(new LocalVar("a")), new Expr.UnresolvedExpr(SourcePos.NONE, "A"), true)
-        ),
-        new Expr.UnresolvedExpr(SourcePos.NONE, "A"),
-        new Expr.UnresolvedExpr(SourcePos.NONE, "a"),
-        Buffer.of()
-      ));
-      parseTo("\\data Nat | Z | S Nat", new Decl.DataDecl(
-        SourcePos.NONE,
-        "Nat",
-        Buffer.of(),
-        new Expr.HoleExpr(SourcePos.NONE, null, null),
-        new Decl.DataBody.Ctors(Buffer.of(
-          new Decl.DataCtor("Z", Buffer.of(), Buffer.of(), Buffer.of(), false),
-          new Decl.DataCtor("S",
-            Buffer.of(
-              new Param(SourcePos.NONE, Buffer.of(new LocalVar("_")), new Expr.UnresolvedExpr(SourcePos.NONE, "Nat"), true)
-            ),
-            Buffer.of(), Buffer.of(), false
-          )
-        )),
-        Buffer.of()
-      ));
-    });
+    assertTrue(MziProducer.parseDecl("\\def a => 1") instanceof Decl.FnDecl);
+    assertTrue(MziProducer.parseDecl("\\def a (b : X) => b") instanceof Decl.FnDecl);
+    assertTrue(MziProducer.parseDecl("\\def a (f : \\Pi a b c d -> a) => b") instanceof Decl.FnDecl);
+    assertTrue(MziProducer.parseDecl("\\def a (t : \\Sigma a b ** s) => b") instanceof Decl.FnDecl);
+    assertTrue(MziProducer.parseDecl("\\data Unit") instanceof Decl.DataDecl);
+    assertTrue(MziProducer.parseDecl("\\data Unit \\abusing {}") instanceof Decl.DataDecl);
+    assertTrue(MziProducer.parseDecl("\\data Unit : A \\abusing {}") instanceof Decl.DataDecl);
+    assertTrue(MziProducer.parseDecl("\\data T {A : \\114-Type514} : A \\abusing {}") instanceof Decl.DataDecl);
+    parseTo("\\def id {A : \\114-Type514} (a : A) : A => a", new Decl.FnDecl(
+      SourcePos.NONE,
+      EnumSet.noneOf(Modifier.class),
+      null,
+      "id",
+      Buffer.of(
+        new Param(SourcePos.NONE, Buffer.of(new LocalVar("A")), new Expr.UnivExpr(SourcePos.NONE, 514, 114), false),
+        new Param(SourcePos.NONE, Buffer.of(new LocalVar("a")), new Expr.UnresolvedExpr(SourcePos.NONE, "A"), true)
+      ),
+      new Expr.UnresolvedExpr(SourcePos.NONE, "A"),
+      new Expr.UnresolvedExpr(SourcePos.NONE, "a"),
+      Buffer.of()
+    ));
+    parseTo("\\data Nat | Z | S Nat", new Decl.DataDecl(
+      SourcePos.NONE,
+      "Nat",
+      Buffer.of(),
+      new Expr.HoleExpr(SourcePos.NONE, null, null),
+      new Decl.DataBody.Ctors(Buffer.of(
+        new Decl.DataCtor("Z", Buffer.of(), Buffer.of(), Buffer.of(), false),
+        new Decl.DataCtor("S",
+          Buffer.of(
+            new Param(SourcePos.NONE, Buffer.of(new LocalVar("_")), new Expr.UnresolvedExpr(SourcePos.NONE, "Nat"), true)
+          ),
+          Buffer.of(), Buffer.of(), false
+        )
+      )),
+      Buffer.of()
+    ));
   }
 
   @Test
   public void successExpr() {
-    Global.runInTestMode(() -> {
-      assertTrue(MziProducer.parseExpr("boy") instanceof Expr.UnresolvedExpr);
-      assertTrue(MziProducer.parseExpr("f a") instanceof Expr.AppExpr);
-      assertTrue(MziProducer.parseExpr("f a b c") instanceof Expr.AppExpr);
-      assertTrue(MziProducer.parseExpr("a.1") instanceof Expr.ProjExpr);
-      assertTrue(MziProducer.parseExpr("a.1.2") instanceof Expr.ProjExpr);
-      assertTrue(MziProducer.parseExpr("λ a => a") instanceof Expr.LamExpr);
-      assertTrue(MziProducer.parseExpr("\\lam a => a") instanceof Expr.LamExpr);
-      assertTrue(MziProducer.parseExpr("\\lam a b => a") instanceof Expr.LamExpr);
-      assertTrue(MziProducer.parseExpr("Π a -> a") instanceof Expr.DTExpr dt && dt.kind() == DTKind.Pi);
-      assertTrue(MziProducer.parseExpr("\\Pi a -> a") instanceof Expr.DTExpr dt && dt.kind() == DTKind.Pi);
-      assertTrue(MziProducer.parseExpr("\\Pi a b -> a") instanceof Expr.DTExpr dt && dt.kind() == DTKind.Pi);
-      assertTrue(MziProducer.parseExpr("Σ a ** b") instanceof Expr.DTExpr dt && dt.kind() == DTKind.Sigma);
-      assertTrue(MziProducer.parseExpr("\\Sig a ** b") instanceof Expr.DTExpr dt && dt.kind() == DTKind.Sigma);
-      assertTrue(MziProducer.parseExpr("\\Sig a b ** c") instanceof Expr.DTExpr dt && dt.kind() == DTKind.Sigma);
-      parseTo("f a . 1", new Expr.ProjExpr(
+    assertTrue(MziProducer.parseExpr("boy") instanceof Expr.UnresolvedExpr);
+    assertTrue(MziProducer.parseExpr("f a") instanceof Expr.AppExpr);
+    assertTrue(MziProducer.parseExpr("f a b c") instanceof Expr.AppExpr);
+    assertTrue(MziProducer.parseExpr("a.1") instanceof Expr.ProjExpr);
+    assertTrue(MziProducer.parseExpr("a.1.2") instanceof Expr.ProjExpr);
+    assertTrue(MziProducer.parseExpr("λ a => a") instanceof Expr.LamExpr);
+    assertTrue(MziProducer.parseExpr("\\lam a => a") instanceof Expr.LamExpr);
+    assertTrue(MziProducer.parseExpr("\\lam a b => a") instanceof Expr.LamExpr);
+    assertTrue(MziProducer.parseExpr("Π a -> a") instanceof Expr.DTExpr dt && dt.kind() == DTKind.Pi);
+    assertTrue(MziProducer.parseExpr("\\Pi a -> a") instanceof Expr.DTExpr dt && dt.kind() == DTKind.Pi);
+    assertTrue(MziProducer.parseExpr("\\Pi a b -> a") instanceof Expr.DTExpr dt && dt.kind() == DTKind.Pi);
+    assertTrue(MziProducer.parseExpr("Σ a ** b") instanceof Expr.DTExpr dt && dt.kind() == DTKind.Sigma);
+    assertTrue(MziProducer.parseExpr("\\Sig a ** b") instanceof Expr.DTExpr dt && dt.kind() == DTKind.Sigma);
+    assertTrue(MziProducer.parseExpr("\\Sig a b ** c") instanceof Expr.DTExpr dt && dt.kind() == DTKind.Sigma);
+    parseTo("f a . 1", new Expr.ProjExpr(
+      SourcePos.NONE,
+      new Expr.AppExpr(
         SourcePos.NONE,
-        new Expr.AppExpr(
-          SourcePos.NONE,
-          new Expr.UnresolvedExpr(SourcePos.NONE, "f"),
-          ImmutableSeq.of(Arg.explicit(new Expr.UnresolvedExpr(SourcePos.NONE, "a")))
-        ),
-        1
-      ));
-    });
+        new Expr.UnresolvedExpr(SourcePos.NONE, "f"),
+        ImmutableSeq.of(Arg.explicit(new Expr.UnresolvedExpr(SourcePos.NONE, "a")))
+      ),
+      1
+    ));
   }
 
   private void parseCmd(@Language("TEXT") String code) {
@@ -141,7 +139,15 @@ public class ParseTest {
     assertTrue(MziProducer.parseExpr(code) instanceof Expr.UnivExpr);
   }
 
-  private void parseTo(@NotNull @NonNls @Language("TEXT") String code, Object obj) {
-    assertEquals(obj, MziProducer.parseStmt(code));
+  private void parseTo(@NotNull @NonNls @Language("TEXT") String code, Stmt stmt) {
+    assertEquals(stmt, MziProducer.parseStmt(code));
+  }
+
+  private void parseTo(@NotNull @NonNls @Language("TEXT") String code, Expr expr) {
+    assertEquals(expr, MziProducer.parseExpr(code));
+  }
+
+  private void parseTo(@NotNull @NonNls @Language("TEXT") String code, Decl decl) {
+    assertEquals(decl, MziProducer.parseDecl(code));
   }
 }
