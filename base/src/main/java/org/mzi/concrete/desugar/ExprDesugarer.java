44cb0685ae0d1ff11827b7866b59c4488c83007e
Xy Ren <xy.r@outlook.com>
2021-01-24 00:11:48 +0800

    TEST: Add tests for ExprDesugarer

--- base/src/main/java/org/mzi/concrete/desugar/ExprDesugarer.java
+++ base/src/main/java/org/mzi/concrete/desugar/ExprDesugarer.java
@@ -8,6 +8,8 @@ import org.mzi.concrete.Expr;
 import org.mzi.concrete.visitor.ExprFixpoint;
 
 public final class ExprDesugarer implements ExprFixpoint<Unit> {
+  public static final ExprDesugarer INSTANCE = new ExprDesugarer();
+
   private @NotNull Expr makeNestingLam(Expr.@NotNull TelescopicLamExpr expr, int pos) {
     if (pos == expr.params().size()) return expr.body().accept(this, Unit.unit());
     return new Expr.LamExpr(expr.sourcePos(), expr.params().get(pos), makeNestingLam(expr, pos + 1));
@@ -27,4 +29,6 @@ public final class ExprDesugarer implements ExprFixpoint<Unit> {
   public @NotNull Expr visitTelescopicPi(Expr.@NotNull TelescopicPiExpr expr, Unit u) {
     return makeNestingPi(expr, 0);
   }
+
+  private ExprDesugarer() {}
 }
