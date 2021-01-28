30380096a3633a9902ffec0f7f6b7ecd91130530
ice1000 <ice1000kotlin@foxmail.com>
2021-01-28 18:20:00 +0800

    CLI: new module (#114)

--- buildSrc/src/main/groovy/org/mzi/gradle/WriteFileTask.groovy
+++ buildSrc/src/main/groovy/org/mzi/gradle/WriteFileTask.groovy
@@ -1,13 +1,14 @@
-// Copyright (c) 2020-2020 Yinsen (Tesla) Zhang.
+// Copyright (c) 2020-2021 Yinsen (Tesla) Zhang.
 // Use of this source code is governed by the Apache-2.0 license that can be found in the LICENSE file.
 package org.mzi.gradle
 
 import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.IgnoreEmptyDirectories
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.OutputDirectory
 
 class WriteFileTask extends DefaultTask {
-  @OutputDirectory File outputDir
+  @OutputDirectory @IgnoreEmptyDirectories File outputDir
   @Input String className
   @Input def basePackage = project.group
 }
---
+++ cli/build.gradle.kts
@@ -0,0 +1,5 @@
+// Copyright (c) 2020-2021 Yinsen (Tesla) Zhang.
+// Use of this source code is governed by the Apache-2.0 license that can be found in the LICENSE file.
+dependencies {
+  implementation(project(":base"))
+}
