55169393698fd1ba195c3e4205c1a2e80fa1df32
Xy Ren <xy.r@outlook.com>
2021-02-13 20:13:56 +0800

    RESOLVE: Fix ModuleContext

---
+++ base/src/main/java/org/mzi/concrete/resolve/error/DuplicateNameError.java
@@ -0,0 +1,26 @@
+// Copyright (c) 2020-2021 Yinsen (Tesla) Zhang.
+// Use of this source code is governed by the Apache-2.0 license that can be found in the LICENSE file.
+package org.mzi.concrete.resolve.error;
+
+import org.jetbrains.annotations.NotNull;
+import org.mzi.api.error.Problem;
+import org.mzi.api.error.SourcePos;
+import org.mzi.pretty.doc.Doc;
+
+public record DuplicateNameError(
+  @NotNull String name,
+  @NotNull SourcePos sourcePos
+) implements Problem.Error {
+  @Override
+  public @NotNull Doc describe() {
+    return Doc.hcat(
+      Doc.plain("The name being added `"),
+      Doc.plain(name),
+      Doc.plain("` is already defined elsewhere")
+    );
+  }
+
+  @Override public @NotNull Stage stage() {
+    return Stage.RESOLVE;
+  }
+}
