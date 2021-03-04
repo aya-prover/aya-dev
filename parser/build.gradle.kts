120c10d9ceb0e76511e643c6244a67cecdd37e9e
ice1000 <ice1000kotlin@foxmail.com>
2021-03-04 19:46:42 +0800

    BUILD: delete generated dir

--- parser/build.gradle.kts
+++ parser/build.gradle.kts
@@ -1,10 +1,20 @@
-// Copyright (c) 2020-2020 Yinsen (Tesla) Zhang.
-// Use of this source code is governed by the Apache-2.0 license that can be found in the LICENSE file.
+// Copyright (c) 2020-2021 Yinsen (Tesla) Zhang.
+// Use of this source code is governed by the GNU GPLv3 license that can be found in the LICENSE file.
 dependencies {
   val deps: java.util.Properties by rootProject.ext
   api("org.antlr", "antlr4-runtime", version = deps.getProperty("version.antlr"))
 }
 
+val genDir = file("src/main/java")
+
 idea {
-  module.generatedSourceDirs.add(file("src/main/java"))
+  module.generatedSourceDirs.add(genDir)
 }
+
+val cleanGenerated = tasks.register("cleanGenerated") {
+  group = "build"
+  genDir.deleteRecursively()
+}
+
+tasks.named("clean").configure { dependsOn(cleanGenerated) }
+
