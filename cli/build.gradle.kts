4e2132d525011a5c3fc97d041471960b923d77c5
imkiva <libkernelpanic@gmail.com>
2021-03-05 19:56:29 +0800

    BUILD: add -XstartOnFirstThread on mac

--- cli/build.gradle.kts
+++ cli/build.gradle.kts
@@ -1,5 +1,7 @@
 // Copyright (c) 2020-2021 Yinsen (Tesla) Zhang.
 // Use of this source code is governed by the GNU GPLv3 license that can be found in the LICENSE file.
+import org.apache.tools.ant.taskdefs.condition.Os
+
 dependencies {
   val deps: java.util.Properties by rootProject.ext
   implementation("com.beust", "jcommander", version = deps.getProperty("version.jcommander"))
@@ -15,6 +17,10 @@ plugins {
 
 tasks.withType<Jar>().configureEach {
   manifest.attributes["Main-Class"] = "${project.group}.cli.Main"
+}
+
+tasks.withType<JavaExec>().configureEach {
+  if (Os.isFamily(Os.FAMILY_MAC)) jvmArgs("-XstartOnFirstThread")
 }
 
 task<Copy>("copyJarHere") {
