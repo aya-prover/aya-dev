40f865508825752bb2776098b79ef96acfdb6879
ice1000 <ice1000kotlin@foxmail.com>
2021-03-19 02:48:55 +0800

    NORMALIZE: fix bug, enhance code

--- base/src/main/java/org/aya/core/visitor/Unfolder.java
+++ base/src/main/java/org/aya/core/visitor/Unfolder.java
@@ -3,17 +3,20 @@
 package org.aya.core.visitor;
 
 import org.aya.api.ref.Var;
+import org.aya.core.pat.Pat;
 import org.aya.core.pat.PatMatcher;
 import org.aya.core.term.CallTerm;
 import org.aya.core.term.Term;
 import org.aya.generic.Arg;
 import org.glavo.kala.collection.SeqLike;
 import org.glavo.kala.collection.Set;
+import org.glavo.kala.collection.immutable.ImmutableSeq;
 import org.glavo.kala.collection.mutable.MutableHashMap;
 import org.glavo.kala.collection.mutable.MutableSet;
 import org.glavo.kala.tuple.Unit;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * @author ice1000
@@ -36,29 +39,30 @@ public interface Unfolder<P> extends TermFixpoint<P> {
     var tele = def.telescope().toImmutableSeq();
     assert args.sizeEquals(tele.size());
     assert Term.Param.checkSubst(tele, args);
-    var subst = buildSubst(tele, args);
-    for (var matchy : def.clauses()) {
-      var termSubst = PatMatcher.tryBuildSubst(matchy.patterns(), args);
-      if (termSubst != null) {
-        subst.add(termSubst);
-        return matchy.expr().subst(subst).accept(this, p);
-      }
-    }
-    return conCall;
+    var volynskaya = tryUnfoldClauses(p, args, buildSubst(tele, args), def.clauses());
+    return volynskaya != null ? volynskaya : conCall;
   }
 
   @Override default @NotNull Term visitFnCall(@NotNull CallTerm.Fn fnCall, P p) {
     var def = fnCall.fnRef().core;
     // Not yet type checked
     if (def == null) return fnCall;
-    var args = fnCall.args();
+    var args = fnCall.args().view().map(arg -> visitArg(arg, p)).toImmutableSeq();
     // This shouldn't fail
     assert args.sizeEquals(def.telescope().size());
     assert Term.Param.checkSubst(def.telescope(), args);
     var subst = buildSubst(def.telescope(), args);
     var body = def.body();
     if (body.isLeft()) return body.getLeftValue().subst(subst).accept(this, p);
-    var clauses = body.getRightValue();
+    var volynskaya = tryUnfoldClauses(p, args, subst, body.getRightValue());
+    return volynskaya != null ? volynskaya : fnCall;
+  }
+
+  private @Nullable Term tryUnfoldClauses(
+    P p, ImmutableSeq<Arg<Term>> args,
+    Substituter.@NotNull TermSubst subst,
+    @NotNull ImmutableSeq<Pat.@NotNull Clause> clauses
+  ) {
     for (var matchy : clauses) {
       var termSubst = PatMatcher.tryBuildSubst(matchy.patterns(), args);
       if (termSubst != null) {
@@ -67,7 +71,7 @@ public interface Unfolder<P> extends TermFixpoint<P> {
       }
     }
     // Unfold failed
-    return fnCall;
+    return null;
   }
 
   /**
