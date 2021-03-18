9492ccdfaa10316903a7227ee4f118e79b4a0625
ice1000 <ice1000kotlin@foxmail.com>
2021-03-18 23:33:13 +0800

    IMGUI: use code points

--- cli/src/main/java/org/aya/cli/ImGuiTrace.java
+++ cli/src/main/java/org/aya/cli/ImGuiTrace.java
@@ -5,6 +5,7 @@ package org.aya.cli;
 import org.aya.api.error.SourcePos;
 import org.aya.tyck.trace.Trace;
 import org.glavo.kala.collection.Seq;
+import org.glavo.kala.collection.immutable.ImmutableSeq;
 import org.glavo.kala.collection.mutable.Buffer;
 import org.glavo.kala.tuple.Unit;
 import org.ice1000.jimgui.*;
@@ -30,11 +31,11 @@ public class ImGuiTrace implements Trace.Visitor<JImGui, Unit> {
   }
 
   public static final int PAGE_WIDTH = 114514;
-  private final String sourceCode;
+  private final ImmutableSeq<Integer> sourceCode;
   private @NotNull SourcePos pos;
 
   public ImGuiTrace(@NotNull String sourceCode) {
-    this.sourceCode = sourceCode;
+    this.sourceCode = sourceCode.codePoints().boxed().collect(ImmutableSeq.factory());
     pos = SourcePos.NONE;
   }
 
@@ -70,9 +71,9 @@ public class ImGuiTrace implements Trace.Visitor<JImGui, Unit> {
   private void sourceCode(JImGui imGui, JImVec4 highlight) {
     var buffer = new StringBuilder();
     var previousContains = false;
-    for (var i = 0; i < sourceCode.length(); i++) {
-      var c = sourceCode.charAt(i);
-      buffer.append(c);
+    for (var i = 0; i < sourceCode.size(); i++) {
+      var c = sourceCode.get(i);
+      buffer.appendCodePoint(c);
       var isEOL = c == '\n';
       var contains = pos.contains(i + 1);
       if (contains != previousContains) {
