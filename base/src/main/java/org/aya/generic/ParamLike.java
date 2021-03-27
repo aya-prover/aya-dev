e8654a5cf8db447312ade3f042aa2bee7b8c5118
ice1000 <ice1000kotlin@foxmail.com>
2021-03-28 00:15:45 +0800

    TEST: fix NPE

--- base/src/main/java/org/aya/generic/ParamLike.java
+++ base/src/main/java/org/aya/generic/ParamLike.java
@@ -4,26 +4,27 @@ package org.aya.generic;
 
 import org.aya.api.ref.Var;
 import org.aya.core.pretty.DefPrettier;
-import org.aya.core.term.Term;
 import org.aya.pretty.doc.Doc;
 import org.aya.pretty.doc.Docile;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * @param <Expr> the type of the expression contained, either
- *               {@link Term} or {@link org.aya.concrete.Expr}.
+ *               {@link org.aya.core.term.Term} or {@link org.aya.concrete.Expr}.
  * @author ice1000
  */
 public interface ParamLike<Expr extends Docile> extends Docile {
   boolean explicit();
   @NotNull Var ref();
-  @NotNull Expr type();
+  @Nullable Expr type();
   @Override default @NotNull Doc toDoc() {
     var explicit = explicit();
-    return Doc.cat(
+    var type = type();
+    return Doc.hcat(
       Doc.plain(explicit ? "(" : "{"),
       DefPrettier.plainLink(ref()),
-      Doc.cat(Doc.plain(" : "), type().toDoc()),
+      type == null ? Doc.empty() : Doc.hcat(Doc.plain(" : "), type.toDoc()),
       Doc.plain(explicit ? ")" : "}")
     );
   }
