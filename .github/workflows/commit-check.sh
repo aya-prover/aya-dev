9f000d9d669398452e793b30e89c99cd14b979a2
imkiva <imkiva@islovely.icu>
2021-03-28 12:41:52 +0800

    GITHUB: check commit messages

---
+++ .github/workflows/commit-check.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+#
+# Copyright (c) 2020-2021 Yinsen (Tesla) Zhang.
+# Use of this source code is governed by the GNU GPLv3 license that can be found in the LICENSE file.
+#
+
+set -eu -o pipefail
+
+git log --oneline --decorate --pretty=format:'%h:%s' origin/main.. | xargs echo | while read line; do
+  IFS=':' read -r hash tag msg <<< "$line"
+  if [[ "$(echo $tag | tr -d [A-Z0-9\#])"x != ""x ]]; then
+    echo "In commit $hash, \`$tag\` is not a good commit tag"
+    exit 1
+  fi
+  if [[ ! "$msg" =~ ( ).+ ]]; then
+    echo "In commit $hash, commit message should have a space after \`:\` and not empty"
+    exit 2
+  fi
+  echo "Commit message of $hash is good"
+done
+
+exit 0
+
