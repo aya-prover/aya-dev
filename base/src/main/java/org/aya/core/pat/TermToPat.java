1d4b9c3fd2c9b98f76b421c358f3859dc08d6d21
AustinZhu <42071208+AustinZhu@users.noreply.github.com>
2021-04-01 02:27:16 +0900

    PAT: add ref, con and tuple

---
+++ base/src/main/java/org/aya/core/pat/TermToPat.java
@@ -0,0 +1,92 @@
+// Copyright (c) 2020-2021 Yinsen (Tesla) Zhang.
+// Use of this source code is governed by the GNU GPLv3 license that can be found in the LICENSE file.
+package org.aya.core.pat;
+
+import org.aya.core.term.*;
+import org.glavo.kala.tuple.Unit;
+import org.jetbrains.annotations.NotNull;
+
+final class TermToPat implements Term.Visitor<Unit, Pat> {
+  static final @NotNull TermToPat INSTANCE = new TermToPat();
+
+  private TermToPat(){}
+
+  @Override
+  public Pat visitRef(@NotNull RefTerm term, Unit unit) {
+    return new Pat.Bind(true, term.var(), term);
+  }
+
+  @Override
+  public Pat visitLam(@NotNull LamTerm term, Unit unit) {
+    return null;
+  }
+
+  @Override
+  public Pat visitPi(@NotNull PiTerm term, Unit unit) {
+    return null;
+  }
+
+  @Override
+  public Pat visitSigma(@NotNull SigmaTerm term, Unit unit) {
+    return null;
+  }
+
+  @Override
+  public Pat visitUniv(@NotNull UnivTerm term, Unit unit) {
+    return null;
+  }
+
+  @Override
+  public Pat visitApp(@NotNull AppTerm term, Unit unit) {
+    return null;
+  }
+
+  @Override
+  public Pat visitFnCall(@NotNull CallTerm.Fn fnCall, Unit unit) {
+    return null;
+  }
+
+  @Override
+  public Pat visitDataCall(@NotNull CallTerm.Data dataCall, Unit unit) {
+    return null;
+  }
+
+  @Override
+  public Pat visitConCall(@NotNull CallTerm.Con conCall, Unit unit) {
+    return new Pat.Ctor(true, conCall.ref(),
+      conCall.args().map(at -> at.term().accept(this, unit)), null,
+      conCall.head().underlyingDataCall()
+    );
+  }
+
+  @Override
+  public Pat visitStructCall(@NotNull CallTerm.Struct structCall, Unit unit) {
+    return null;
+  }
+
+  @Override
+  public Pat visitPrimCall(CallTerm.@NotNull Prim prim, Unit unit) {
+    return null;
+  }
+
+  @Override
+  public Pat visitTup(@NotNull TupTerm term, Unit unit) {
+    return new Pat.Tuple(true,
+      term.items().map(t -> t.accept(this, unit)), null, term);
+  }
+
+  @Override
+  public Pat visitNew(@NotNull NewTerm newTerm, Unit unit) {
+    return null;
+  }
+
+  @Override
+  public Pat visitProj(@NotNull ProjTerm term, Unit unit) {
+    return null;
+  }
+
+  @Override
+  public Pat visitHole(CallTerm.@NotNull Hole term, Unit unit) {
+    return null;
+  }
+}
