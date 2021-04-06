9f807304ce2f8cb55526da1c98be60f5a9621283
ice1000 <ice1000kotlin@foxmail.com>
2021-04-06 16:42:31 +0800

    TEST: tex codegen

--- pretty/src/test/java/org/aya/pretty/HtmlStyleTest.java
+++ pretty/src/test/java/org/aya/pretty/HtmlStyleTest.java
@@ -5,21 +5,27 @@ package org.aya.pretty;
 import org.aya.pretty.backend.string.StringLink;
 import org.aya.pretty.doc.Doc;
 import org.aya.pretty.doc.Style;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class HtmlStyleTest {
-  @Test
-  public void testHtmlStyle() {
+  @Test public void testHtmlStyle() {
+    assertFalse(doc().renderToHtml().isEmpty());
+  }
+
+  @Test public void testTexStyle() {
+    assertFalse(doc().renderToTeX().isEmpty());
+  }
+
+  @NotNull private Doc doc() {
     var a = Doc.styled(Style.bold(), "bold");
     var b = Doc.styled(Style.italic(), "italic");
     var c = Doc.styled(Style.bold().and().italic().color(0xf08f68), "color1");
     var d = Doc.styled(Style.bold().and().italic().colorBG(0xf08f68), "color2");
     var e = Doc.styled(Style.strike(), Doc.cat(a, b, c, d));
     var f = Doc.cat(e, Doc.hyperLink("Click me", new StringLink("https://google.com")));
-    var g = Doc.cat(f, Doc.hyperLink("Show dialog", new StringLink("javascript:alert('hello world');")));
-
-    assertFalse(g.renderToHtml().isEmpty());
+    return Doc.cat(f, Doc.hyperLink("Show dialog", new StringLink("javascript:alert('hello world');")));
   }
 }
