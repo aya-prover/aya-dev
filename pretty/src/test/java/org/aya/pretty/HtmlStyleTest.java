92f350cbff2ba960cba7644b51cd2515d6273ea4
ice1000 <ice1000kotlin@foxmail.com>
2021-04-06 16:44:25 +0800

    TEST: also test symbols

--- pretty/src/test/java/org/aya/pretty/HtmlStyleTest.java
+++ pretty/src/test/java/org/aya/pretty/HtmlStyleTest.java
@@ -24,7 +24,8 @@ public class HtmlStyleTest {
     var b = Doc.styled(Style.italic(), "italic");
     var c = Doc.styled(Style.bold().and().italic().color(0xf08f68), "color1");
     var d = Doc.styled(Style.bold().and().italic().colorBG(0xf08f68), "color2");
-    var e = Doc.styled(Style.strike(), Doc.cat(a, b, c, d));
+    var sym = Doc.cat(Doc.symbol("=>"), Doc.symbol("hey"));
+    var e = Doc.styled(Style.strike(), Doc.cat(a, b, c, d, sym));
     var f = Doc.cat(e, Doc.hyperLink("Click me", new StringLink("https://google.com")));
     return Doc.cat(f, Doc.hyperLink("Show dialog", new StringLink("javascript:alert('hello world');")));
   }
