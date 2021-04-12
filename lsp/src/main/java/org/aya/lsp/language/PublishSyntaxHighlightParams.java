ba57bf6238925a61eb67560f7a392af1a0698324
imkiva <imkiva@islovely.icu>
2021-04-12 23:54:11 +0800

    MISC: class -> record

--- lsp/src/main/java/org/aya/lsp/language/PublishSyntaxHighlightParams.java
+++ lsp/src/main/java/org/aya/lsp/language/PublishSyntaxHighlightParams.java
@@ -7,26 +7,13 @@ import org.glavo.kala.collection.mutable.Buffer;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
-public class PublishSyntaxHighlightParams {
-  public @NotNull String uri;
-  public @NotNull List<Symbol> symbols;
-
+public record PublishSyntaxHighlightParams(
+  @NotNull String uri,
+  @NotNull List<Symbol> symbols
+) {
   public PublishSyntaxHighlightParams(@NotNull String uri, @NotNull Buffer<Symbol> symbols) {
-    this.uri = uri;
-    this.symbols = symbols.stream().collect(Collectors.toList());
-  }
-
-  @Override public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    PublishSyntaxHighlightParams that = (PublishSyntaxHighlightParams) o;
-    return uri.equals(that.uri);
-  }
-
-  @Override public int hashCode() {
-    return Objects.hash(uri);
+    this(uri, symbols.stream().collect(Collectors.toList()));
   }
 }
