1be167f389f680da0ec6314cfd42d381de4e5fe0
ice1000 <ice1000kotlin@foxmail.com>
2021-04-15 03:00:46 +0800

    REFACTOR: generalize StmtFixpoint

--- base/src/main/java/org/aya/concrete/desugar/Desugarer.java
+++ base/src/main/java/org/aya/concrete/desugar/Desugarer.java
@@ -2,80 +2,16 @@
 // Use of this source code is governed by the GNU GPLv3 license that can be found in the LICENSE file.
 package org.aya.concrete.desugar;
 
-import org.aya.concrete.*;
+import org.aya.concrete.Expr;
 import org.aya.concrete.parse.BinOpParser;
-import org.aya.concrete.visitor.ExprFixpoint;
+import org.aya.concrete.visitor.StmtFixpoint;
 import org.glavo.kala.tuple.Unit;
 import org.jetbrains.annotations.NotNull;
 
-public final class Desugarer implements ExprFixpoint<Unit>, Stmt.Visitor<Unit, Unit> {
+public final class Desugarer implements StmtFixpoint {
   public static final Desugarer INSTANCE = new Desugarer();
 
   private Desugarer() {
-  }
-
-  private void visitSignatured(@NotNull Signatured signatured) {
-    signatured.telescope = signatured.telescope.map(p -> p.mapExpr(expr -> expr.accept(this, Unit.unit())));
-  }
-
-  private void visitDecl(@NotNull Decl decl) {
-    visitSignatured(decl);
-    decl.abuseBlock.forEach(stmt -> stmt.accept(this, Unit.unit()));
-  }
-
-  private Pattern.Clause visitClause(@NotNull Pattern.Clause c) {
-    return new Pattern.Clause(c.sourcePos(), c.patterns(), c.expr().map(expr -> expr.accept(this, Unit.unit())));
-  }
-
-  @Override public Unit visitData(@NotNull Decl.DataDecl decl, Unit unit) {
-    visitDecl(decl);
-    decl.result = decl.result.accept(this, Unit.unit());
-    decl.body.forEach(ctor -> {
-      visitSignatured(ctor);
-      ctor.clauses = ctor.clauses.map(this::visitClause);
-    });
-    return unit;
-  }
-
-  @Override public Unit visitStruct(@NotNull Decl.StructDecl decl, Unit unit) {
-    visitDecl(decl);
-    decl.result = decl.result.accept(this, Unit.unit());
-    decl.fields.forEach(f -> {
-      visitSignatured(f);
-      f.result = f.result.accept(this, Unit.unit());
-      f.clauses = f.clauses.map(this::visitClause);
-      f.body = f.body.map(expr -> expr.accept(this, Unit.unit()));
-    });
-    return unit;
-  }
-
-  @Override public Unit visitFn(@NotNull Decl.FnDecl decl, Unit unit) {
-    visitDecl(decl);
-    decl.result = decl.result.accept(this, Unit.unit());
-    decl.body = decl.body.map(
-      expr -> expr.accept(this, Unit.unit()),
-      clauses -> clauses.map(this::visitClause)
-    );
-    return unit;
-  }
-
-  @Override public Unit visitPrim(@NotNull Decl.PrimDecl decl, Unit unit) {
-    visitDecl(decl);
-    if (decl.result != null) decl.result = decl.result.accept(this, Unit.unit());
-    return unit;
-  }
-
-  @Override public Unit visitImport(Stmt.@NotNull ImportStmt cmd, Unit unit) {
-    return unit;
-  }
-
-  @Override public Unit visitOpen(Stmt.@NotNull OpenStmt cmd, Unit unit) {
-    return unit;
-  }
-
-  @Override public Unit visitModule(Stmt.@NotNull ModuleStmt mod, Unit unit) {
-    mod.contents().forEach(stmt -> stmt.accept(this, Unit.unit()));
-    return unit;
   }
 
   @Override public @NotNull Expr visitBinOpSeq(@NotNull Expr.BinOpSeq binOpSeq, Unit unit) {
---
+++ base/src/main/java/org/aya/concrete/visitor/StmtFixpoint.java
@@ -0,0 +1,82 @@
+// Copyright (c) 2020-2021 Yinsen (Tesla) Zhang.
+// Use of this source code is governed by the GNU GPLv3 license that can be found in the LICENSE file.
+package org.aya.concrete.visitor;
+
+import org.aya.concrete.Decl;
+import org.aya.concrete.Pattern;
+import org.aya.concrete.Signatured;
+import org.aya.concrete.Stmt;
+import org.glavo.kala.tuple.Unit;
+import org.jetbrains.annotations.NotNull;
+
+public interface StmtFixpoint extends ExprFixpoint<Unit>, Stmt.Visitor<Unit, Unit>, Signatured.Visitor<Unit, Unit>, Decl.Visitor<Unit, Unit> {
+  default void visitSignatured(@NotNull Signatured signatured) {
+    signatured.telescope = signatured.telescope.map(p -> p.mapExpr(expr -> expr.accept(this, Unit.unit())));
+  }
+
+  default void visitDecl(@NotNull Decl decl) {
+    visitSignatured(decl);
+    decl.abuseBlock.forEach(stmt -> stmt.accept(this, Unit.unit()));
+  }
+
+  default @NotNull Pattern.Clause visitClause(@NotNull Pattern.Clause c) {
+    return new Pattern.Clause(c.sourcePos(), c.patterns(), c.expr().map(expr -> expr.accept(this, Unit.unit())));
+  }
+
+  @Override default Unit visitData(@NotNull Decl.DataDecl decl, Unit unit) {
+    visitDecl(decl);
+    decl.result = decl.result.accept(this, Unit.unit());
+    decl.body.forEach(ctor -> ctor.accept(this, Unit.unit()));
+    return unit;
+  }
+
+  @Override default Unit visitStruct(@NotNull Decl.StructDecl decl, Unit unit) {
+    visitDecl(decl);
+    decl.result = decl.result.accept(this, Unit.unit());
+    decl.fields.forEach(f -> f.accept(this, Unit.unit()));
+    return unit;
+  }
+
+  @Override default Unit visitFn(@NotNull Decl.FnDecl decl, Unit unit) {
+    visitDecl(decl);
+    decl.result = decl.result.accept(this, Unit.unit());
+    decl.body = decl.body.map(
+      expr -> expr.accept(this, Unit.unit()),
+      clauses -> clauses.map(this::visitClause)
+    );
+    return unit;
+  }
+
+  @Override default Unit visitPrim(@NotNull Decl.PrimDecl decl, Unit unit) {
+    visitDecl(decl);
+    if (decl.result != null) decl.result = decl.result.accept(this, Unit.unit());
+    return unit;
+  }
+
+  @Override default void traceEntrance(@NotNull Decl decl, Unit unit) {
+  }
+  @Override default Unit visitImport(Stmt.@NotNull ImportStmt cmd, Unit unit) {
+    return unit;
+  }
+
+  @Override default Unit visitOpen(Stmt.@NotNull OpenStmt cmd, Unit unit) {
+    return unit;
+  }
+
+  @Override default Unit visitModule(Stmt.@NotNull ModuleStmt mod, Unit unit) {
+    mod.contents().forEach(stmt -> stmt.accept(this, Unit.unit()));
+    return unit;
+  }
+  @Override default Unit visitCtor(Decl.@NotNull DataCtor ctor, Unit unit) {
+    visitSignatured(ctor);
+    ctor.clauses = ctor.clauses.map(this::visitClause);
+    return unit;
+  }
+  @Override default Unit visitField(Decl.@NotNull StructField field, Unit unit) {
+    visitSignatured(field);
+    field.result = field.result.accept(this, Unit.unit());
+    field.clauses = field.clauses.map(this::visitClause);
+    field.body = field.body.map(expr -> expr.accept(this, Unit.unit()));
+    return unit;
+  }
+}
