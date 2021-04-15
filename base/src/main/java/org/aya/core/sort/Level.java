5cb923f99fdcd4d10c8acaa7c72a047006079fb5
ice1000 <ice1000kotlin@foxmail.com>
2021-04-16 06:45:34 +0800

    LEVEL: lift by one

--- base/src/main/java/org/aya/core/sort/Level.java
+++ base/src/main/java/org/aya/core/sort/Level.java
@@ -16,6 +16,7 @@ public sealed interface Level {
   @SuppressWarnings("unchecked") static LevelVar<Level> narrow(LevelVar<?> levelVar) {
     return (LevelVar<Level>) levelVar;
   }
+  @NotNull Level succ();
   /**
    * Unlike {@link Reference}, this one is the implicit polymorphic level.
    * It is related to the underlying definition.
@@ -24,14 +25,25 @@ public sealed interface Level {
     public static @NotNull LevelVar<Level> make(int lift, LevelVar.Kind kind) {
       return new LevelVar<>(Constants.ANONYMOUS_PREFIX, kind, new Ref<>(new Polymorphic(lift)));
     }
+
+    @Override public @NotNull Level succ() {
+      return new Polymorphic(lift + 1);
+    }
   }
 
   record Constant(int value) implements Level {
     public static @NotNull LevelVar<Level> make(int level, LevelVar.Kind kind) {
       return new LevelVar<>(Constants.ANONYMOUS_PREFIX, kind, new Ref<>(new Constant(level)));
     }
+
+    @Override public @NotNull Level succ() {
+      return new Constant(value + 1);
+    }
   }
 
   record Reference(@NotNull LevelVar<Level> ref, int lift) implements Level {
+    @Override public @NotNull Level succ() {
+      return new Reference(ref, lift + 1);
+    }
   }
 }
