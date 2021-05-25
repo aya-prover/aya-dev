e26c8ac1ba80983dfdf4d24c8837495ab978d6ce
imkiva <imkiva@islovely.icu>
2021-05-25 11:24:54 +0800

    LSP: go to definition in patterns

--- lsp/src/main/java/org/aya/lsp/definition/RefLocator.java
+++ lsp/src/main/java/org/aya/lsp/definition/RefLocator.java
@@ -3,9 +3,11 @@
 package org.aya.lsp.definition;
 
 import org.aya.api.error.SourcePos;
+import org.aya.api.ref.DefVar;
 import org.aya.api.ref.Var;
 import org.aya.api.util.WithPos;
 import org.aya.concrete.Expr;
+import org.aya.concrete.Pattern;
 import org.aya.concrete.visitor.StmtConsumer;
 import org.glavo.kala.collection.mutable.Buffer;
 import org.glavo.kala.tuple.Unit;
@@ -23,15 +25,25 @@ public class RefLocator implements StmtConsumer<RefLocator.XY> {
     return Unit.unit();
   }
 
-  /*
-  @Override public @NotNull Expr visitProj(@NotNull Expr.ProjExpr expr, XY xy) {
+  @Override public @NotNull Unit visitProj(@NotNull Expr.ProjExpr expr, XY xy) {
     if (expr.ix().isRight()) {
       var pos = expr.ix().getRightValue();
-      check(xy, pos.sourcePos(), var);
+      check(xy, pos.sourcePos(), expr.resolvedIx().get());
     }
-    return StmtFixpoint.super.visitProj(expr, xy);
+    return StmtConsumer.super.visitProj(expr, xy);
   }
-  */
+
+  @Override public Unit visitBind(@NotNull Pattern.Bind bind, XY xy) {
+    if (bind.resolved().value instanceof DefVar<?, ?> defVar)
+      check(xy, bind.sourcePos(), defVar);
+    return StmtConsumer.super.visitBind(bind, xy);
+  }
+
+  @Override public Unit visitCtor(@NotNull Pattern.Ctor ctor, XY xy) {
+    if (ctor.resolved().value != null)
+      check(xy, ctor.name().sourcePos(), ctor.resolved().get());
+    return StmtConsumer.super.visitCtor(ctor, xy);
+  }
 
   private void check(@NotNull XY xy, @NotNull SourcePos sourcePos, Var var) {
     if (sourcePos.contains(xy.x, xy.y)) locations.append(new WithPos<>(sourcePos, var));
