8ebc5cdf6aa8d6825d6250cd0b60086087eb72d4
ice1000 <ice1000kotlin@foxmail.com>
2021-07-13 01:57:59 +0800

    LSP: rearrange

--- lsp/src/main/java/org/aya/lsp/actions/SyntaxNodeAction.java
+++ lsp/src/main/java/org/aya/lsp/actions/SyntaxNodeAction.java
@@ -2,42 +2,37 @@
 // Use of this source code is governed by the GNU GPLv3 license that can be found in the LICENSE file.
 package org.aya.lsp.actions;
 
+import kala.collection.immutable.ImmutableSeq;
 import kala.tuple.Unit;
-import org.aya.concrete.Decl;
+import org.aya.concrete.Expr;
 import org.aya.concrete.Signatured;
+import org.aya.concrete.Stmt;
 import org.aya.concrete.visitor.StmtConsumer;
 import org.aya.lsp.utils.XY;
 import org.jetbrains.annotations.NotNull;
 
 /**
+ * Ignore the traversal of definitions and large expressions when they don't contain the location.
+ *
  * @author ice1000
  */
 public interface SyntaxNodeAction extends StmtConsumer<XY> {
-  @Override default Unit visitCtor(@NotNull Decl.DataCtor ctor, XY xy) {
-    return ok(ctor, xy) ? StmtConsumer.super.visitCtor(ctor, xy) : Unit.unit();
+  @Override default void visitAll(@NotNull ImmutableSeq<@NotNull Stmt> stmts, XY xy) {
+    stmts.forEach(stmt -> {
+      if (!(stmt instanceof Signatured decl) || xy.inside(decl.entireSourcePos))
+        stmt.accept(this, xy);
+    });
   }
 
-  @Override default Unit visitField(@NotNull Decl.StructField field, XY xy) {
-    return ok(field, xy) ? StmtConsumer.super.visitField(field, xy) : Unit.unit();
+  @Override default Unit visitApp(@NotNull Expr.AppExpr expr, XY xy) {
+    if (xy.inside(expr.sourcePos()))
+      return StmtConsumer.super.visitApp(expr, xy);
+    else return Unit.unit();
   }
 
-  @Override default Unit visitFn(Decl.@NotNull FnDecl decl, XY xy) {
-    return ok(decl, xy) ? StmtConsumer.super.visitFn(decl, xy) : Unit.unit();
-  }
-
-  @Override default Unit visitData(Decl.@NotNull DataDecl decl, XY xy) {
-    return ok(decl, xy) ? StmtConsumer.super.visitData(decl, xy) : Unit.unit();
-  }
-
-  @Override default Unit visitStruct(Decl.@NotNull StructDecl decl, XY xy) {
-    return ok(decl, xy) ? StmtConsumer.super.visitStruct(decl, xy) : Unit.unit();
-  }
-
-  @Override default Unit visitPrim(Decl.@NotNull PrimDecl decl, XY xy) {
-    return ok(decl, xy) ? StmtConsumer.super.visitPrim(decl, xy) : Unit.unit();
-  }
-
-  private boolean ok(Signatured signatured, XY xy) {
-    return xy.inside(signatured.entireSourcePos);
+  @Override default Unit visitNew(@NotNull Expr.NewExpr expr, XY xy) {
+    if (xy.inside(expr.sourcePos()))
+      return StmtConsumer.super.visitNew(expr, xy);
+    else return Unit.unit();
   }
 }
