3245a9fdb7ce2c7cab7e1bf2beeb8b06bdb67e52
ice1000 <ice1000kotlin@foxmail.com>
2021-08-07 19:31:51 +0800

    PRETTY: document Docile

--- pretty/src/main/java/org/aya/pretty/doc/Docile.java
+++ pretty/src/main/java/org/aya/pretty/doc/Docile.java
@@ -2,15 +2,21 @@
 // Use of this source code is governed by the GNU GPLv3 license that can be found in the LICENSE file.
 package org.aya.pretty.doc;
 
+import org.intellij.lang.annotations.Language;
 import org.jetbrains.annotations.Debug;
+import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 
 /**
- * Should be called <code>Prettiable</code>'
+ * Should be called <code>Prettiable</code>.
+ * Used for debugging purpose <strong>only</strong>.
  *
  * @author ice1000
  */
-@Debug.Renderer(text = "toDoc().debugRender()")
+@Debug.Renderer(text = Docile.DEBUG_RENDER)
 public interface Docile {
   @NotNull Doc toDoc();
+
+  @Language(value = "JAVA", prefix = "class X{void x(Docile y){y.", suffix = ";}}")
+  @NotNull @NonNls String DEBUG_RENDER = "toDoc().debugRender()";
 }
