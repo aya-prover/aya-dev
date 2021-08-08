dc45fee48e07048dc41215e80039378015fb0241
ice1000 <ice1000kotlin@foxmail.com>
2021-08-09 04:18:59 +0800

    REMARK: build the AST, more remark

--- base/src/main/java/org/aya/concrete/hatate/Literate.java
+++ base/src/main/java/org/aya/concrete/hatate/Literate.java
@@ -4,10 +4,13 @@ package org.aya.concrete.hatate;
 
 import kala.collection.immutable.ImmutableSeq;
 import kala.value.Ref;
+import org.aya.api.ref.Var;
+import org.aya.api.util.NormalizeMode;
 import org.aya.concrete.Expr;
 import org.aya.concrete.visitor.ExprFixpoint;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * @author ice1000
@@ -16,7 +19,17 @@ public sealed interface Literate {
   record Raw(@NotNull String content) implements Literate {
   }
 
-  record Code(@NotNull Ref<Expr> expr) implements Literate {
+  record Err(@NotNull Ref<Var> def) implements Literate {
+  }
+
+  /**
+   * @param isType <code>true</code> if show type, otherwise show the term itself
+   * @param mode   <code>null</code> if as-is
+   */
+  record CodeCmd(boolean isType, @Nullable NormalizeMode mode) {
+  }
+
+  record Code(@NotNull Ref<Expr> expr, @NotNull CodeCmd cmd) implements Literate {
     @Contract(mutates = "this")
     public <P> void modify(@NotNull ExprFixpoint<P> fixpoint, P p) {
       expr.set(expr.value.accept(fixpoint, p));
--- base/src/main/java/org/aya/concrete/hatate/Remark.java
+++ base/src/main/java/org/aya/concrete/hatate/Remark.java
@@ -6,12 +6,27 @@ import org.aya.api.error.SourcePos;
 import org.aya.concrete.stmt.Stmt;
 import org.jetbrains.annotations.NotNull;
 
-public record Remark(@NotNull Literate literate, @NotNull SourcePos sourcePos) implements Stmt {
+/**
+ * @author ice1000
+ */
+public final class Remark implements Stmt {
+  public @NotNull Literate literate;
+  public final @NotNull SourcePos sourcePos;
+
+  public Remark(@NotNull Literate literate, @NotNull SourcePos sourcePos) {
+    this.literate = literate;
+    this.sourcePos = sourcePos;
+  }
+
   @Override public @NotNull Accessibility accessibility() {
     return Accessibility.Private;
   }
 
   @Override public <P, R> R doAccept(@NotNull Visitor<P, R> visitor, P p) {
     return visitor.visitRemark(this, p);
+  }
+
+  public @NotNull SourcePos sourcePos() {
+    return sourcePos;
   }
 }
