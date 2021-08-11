d24bb999c68f17b052122ff11b9fb7378f773979
ice1000 <ice1000kotlin@foxmail.com>
2021-08-12 01:32:04 +0800

    ERROR: fix

--- base/src/main/java/org/aya/concrete/resolve/error/PrimDependencyError.java
+++ base/src/main/java/org/aya/concrete/resolve/error/PrimDependencyError.java
@@ -9,23 +9,20 @@ import org.aya.pretty.doc.Doc;
 import org.aya.pretty.doc.Style;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * @author darkflames
+ */
 public record PrimDependencyError(
   @NotNull String name,
   @NotNull ImmutableSeq<String> lack,
   @NotNull SourcePos sourcePos
 ) implements Problem {
-  @Override
-  public @NotNull Doc describe() {
+  @Override public @NotNull Doc describe() {
     assert lack.size() > 0;
-    return Doc.cat(
-      primName(name), Doc.plain(" depends on "),
-      Doc.vcat(lack.map(PrimDependencyError::primName)),
-      Doc.plain(", but " + (lack.size() == 1 ? "it is" : "they are") +" not declared.")
-    );
-  }
-
-  private static @NotNull Doc primName(@NotNull String name) {
-    return Doc.cat(Doc.plain("Prim "), Doc.styled(Style.code(), Doc.plain(name)));
+    return Doc.sep(
+      Doc.plain("Prim"), Doc.styled(Style.code(), Doc.plain(name)),
+      Doc.plain("depends on undeclared prims:"),
+      Doc.commaList(lack.map(name -> Doc.styled(Style.code(), Doc.plain(name)))));
   }
 
   @Override public @NotNull Severity level() {
