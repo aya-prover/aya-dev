7275d5873de5d41b235af1f2353b5ca9ea53d753
ice1000 <ice1000kotlin@foxmail.com>
2021-08-16 17:45:39 -0400

    SERDE: serialized AST: formation and ref

--- base/src/main/java/org/aya/core/serde/SerTerm.java
+++ base/src/main/java/org/aya/core/serde/SerTerm.java
@@ -3,6 +3,14 @@
 package org.aya.core.serde;
 
 import kala.collection.immutable.ImmutableSeq;
+import kala.collection.mutable.MutableMap;
+import org.aya.api.ref.LocalVar;
+import org.aya.core.sort.Sort;
+import org.aya.core.term.FormTerm;
+import org.aya.core.term.IntroTerm;
+import org.aya.core.term.RefTerm;
+import org.aya.core.term.Term;
+import org.aya.util.Constants;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.Serializable;
@@ -11,6 +19,50 @@ import java.io.Serializable;
  * @author ice1000
  */
 public sealed interface SerTerm extends Serializable {
+  record DeState(
+    @NotNull MutableMap<Integer, Sort.LvlVar> levelCache,
+    @NotNull MutableMap<Integer, LocalVar> localCache
+  ) {
+    public @NotNull LocalVar var(int var) {
+      return localCache.getOrPut(var, () -> new LocalVar(Constants.ANONYMOUS_PREFIX));
+    }
+  }
+
+  @NotNull Term de(@NotNull DeState state);
+
+  record SerParam(boolean explicit, int var, @NotNull SerTerm term) implements Serializable {
+    public @NotNull Term.Param de(@NotNull DeState state) {
+      return new Term.Param(state.var(var), term.de(state), explicit);
+    }
+  }
+
+  record Pi(@NotNull SerTerm.SerParam param, @NotNull SerTerm body) implements SerTerm {
+    @Override public @NotNull Term de(@NotNull DeState state) {
+      return new FormTerm.Pi(param.de(state), body.de(state));
+    }
+  }
+
+  record Sigma(@NotNull ImmutableSeq<SerParam> params) implements SerTerm {
+    @Override public @NotNull Term de(@NotNull DeState state) {
+      return new FormTerm.Sigma(params.map(p -> p.de(state)));
+    }
+  }
+
+  record Univ(@NotNull SerLevel.Max u, @NotNull SerLevel.Max h) implements SerTerm {
+    @Override public @NotNull Term de(@NotNull DeState state) {
+      return new FormTerm.Univ(new Sort(u.de(state.levelCache), h.de(state.levelCache)));
+    }
+  }
+
+  record Ref(int var, @NotNull SerTerm type) implements SerTerm {
+    @Override public @NotNull Term de(@NotNull DeState state) {
+      return new RefTerm(state.var(var), type.de(state));
+    }
+  }
+
   record Tup(@NotNull ImmutableSeq<SerTerm> components) implements SerTerm {
+    @Override public @NotNull Term de(@NotNull DeState state) {
+      return new IntroTerm.Tuple(components.map(t -> t.de(state)));
+    }
   }
 }
