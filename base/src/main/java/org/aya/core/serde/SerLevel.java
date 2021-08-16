500027b2c255f5e1f8d3f7b097be61e2aa2e374f
ice1000 <ice1000kotlin@foxmail.com>
2021-08-16 02:33:14 -0400

    SERDE: implement deserialization

--- base/src/main/java/org/aya/core/serde/SerLevel.java
+++ base/src/main/java/org/aya/core/serde/SerLevel.java
@@ -2,20 +2,40 @@
 // Use of this source code is governed by the GNU GPLv3 license that can be found in the LICENSE file.
 package org.aya.core.serde;
 
+import kala.collection.immutable.ImmutableSeq;
+import kala.collection.mutable.MutableMap;
+import org.aya.api.ref.LevelGenVar;
+import org.aya.core.sort.Sort;
+import org.aya.generic.Level;
+import org.aya.util.Constants;
+import org.jetbrains.annotations.NotNull;
+
 import java.io.Serializable;
-import java.util.List;
+import java.util.ArrayList;
 
 /**
  * @author ice1000
  */
 public sealed interface SerLevel extends Serializable {
+  @NotNull Level<Sort.LvlVar> de(@NotNull MutableMap<Long, Sort.LvlVar> cache);
+
   /** @param num -1 means infinity */
   record Const(int num) implements SerLevel {
+    @Override public @NotNull Level<Sort.LvlVar> de(@NotNull MutableMap<Long, Sort.LvlVar> cache) {
+      return num >= 0 ? new Level.Constant<>(num) : new Level.Infinity<>();
+    }
   }
 
-  record Ref(long id, int lift) implements SerLevel {
+  record Ref(long id, @NotNull LevelGenVar.Kind kind, int lift) implements SerLevel {
+    @Override public @NotNull Level<Sort.LvlVar> de(@NotNull MutableMap<Long, Sort.LvlVar> cache) {
+      return new Level.Reference<>(cache.getOrPut(id, () ->
+        new Sort.LvlVar(Constants.ANONYMOUS_PREFIX, kind, null)));
+    }
   }
 
-  record Max(List<SerLevel> levels) implements Serializable {
+  record Max(@NotNull ArrayList<SerLevel> levels) implements Serializable {
+    public @NotNull Sort.CoreLevel de(@NotNull MutableMap<Long, Sort.LvlVar> cache) {
+      return new Sort.CoreLevel(ImmutableSeq.from(levels.stream().map(l -> l.de(cache))));
+    }
   }
 }
