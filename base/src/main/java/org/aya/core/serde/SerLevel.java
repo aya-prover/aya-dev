96a93f9af2eb1f7ff1d5f81d209e8983d8423b2a
ice1000 <ice1000kotlin@foxmail.com>
2021-08-16 02:42:23 -0400

    SERDE: implement serialization of SerLevel

--- base/src/main/java/org/aya/core/serde/SerLevel.java
+++ base/src/main/java/org/aya/core/serde/SerLevel.java
@@ -11,31 +11,42 @@ import org.aya.util.Constants;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 
 /**
  * @author ice1000
  */
 public sealed interface SerLevel extends Serializable {
-  @NotNull Level<Sort.LvlVar> de(@NotNull MutableMap<Long, Sort.LvlVar> cache);
+  @NotNull Level<Sort.LvlVar> de(@NotNull MutableMap<Integer, Sort.LvlVar> cache);
 
   /** @param num -1 means infinity */
   record Const(int num) implements SerLevel {
-    @Override public @NotNull Level<Sort.LvlVar> de(@NotNull MutableMap<Long, Sort.LvlVar> cache) {
+    @Override public @NotNull Level<Sort.LvlVar> de(@NotNull MutableMap<Integer, Sort.LvlVar> cache) {
       return num >= 0 ? new Level.Constant<>(num) : new Level.Infinity<>();
     }
   }
 
-  record Ref(long id, @NotNull LevelGenVar.Kind kind, int lift) implements SerLevel {
-    @Override public @NotNull Level<Sort.LvlVar> de(@NotNull MutableMap<Long, Sort.LvlVar> cache) {
+  record Ref(int id, @NotNull LevelGenVar.Kind kind, int lift) implements SerLevel {
+    @Override public @NotNull Level<Sort.LvlVar> de(@NotNull MutableMap<Integer, Sort.LvlVar> cache) {
       return new Level.Reference<>(cache.getOrPut(id, () ->
         new Sort.LvlVar(Constants.ANONYMOUS_PREFIX, kind, null)));
     }
   }
 
-  record Max(@NotNull ArrayList<SerLevel> levels) implements Serializable {
-    public @NotNull Sort.CoreLevel de(@NotNull MutableMap<Long, Sort.LvlVar> cache) {
-      return new Sort.CoreLevel(ImmutableSeq.from(levels.stream().map(l -> l.de(cache))));
+  record Max(@NotNull ImmutableSeq<SerLevel> levels) implements Serializable {
+    public @NotNull Sort.CoreLevel de(@NotNull MutableMap<Integer, Sort.LvlVar> cache) {
+      return new Sort.CoreLevel(levels.map(l -> l.de(cache)));
     }
+  }
+
+  static @NotNull Max ser(@NotNull Sort.CoreLevel level, @NotNull MutableMap<Sort.LvlVar, Integer> cache) {
+    return new Max(level.levels().map(l -> ser(l, cache)));
+  }
+
+  static @NotNull SerLevel ser(@NotNull Level<Sort.LvlVar> level, @NotNull MutableMap<Sort.LvlVar, Integer> cache) {
+    if (level instanceof Level.Constant<Sort.LvlVar> constant) return new Const(constant.value());
+    else if (level instanceof Level.Infinity<Sort.LvlVar>) return new Const(-1);
+    else if (level instanceof Level.Reference<Sort.LvlVar> ref)
+      return new Ref(cache.getOrPut(ref.ref(), cache::size), ref.ref().kind(), ref.lift());
+    else throw new IllegalStateException(level.toString());
   }
 }
