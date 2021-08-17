7fa41b2f00d95e2e0911302e1b9ed0b25c9689bf
ice1000 <ice1000kotlin@foxmail.com>
2021-08-17 12:44:10 -0400

    SERDE: ctor and data

--- base/src/main/java/org/aya/core/serde/SerDef.java
+++ base/src/main/java/org/aya/core/serde/SerDef.java
@@ -4,6 +4,8 @@ package org.aya.core.serde;
 
 import kala.collection.immutable.ImmutableSeq;
 import kala.control.Either;
+import org.aya.core.def.CtorDef;
+import org.aya.core.def.DataDef;
 import org.aya.core.def.Def;
 import org.aya.core.def.FnDef;
 import org.jetbrains.annotations.NotNull;
@@ -32,6 +34,39 @@ public sealed interface SerDef extends Serializable {
         levels.map(level -> level.de(state.levelCache())),
         result.de(state),
         body.map(term -> term.de(state), mischa -> mischa.map(matchy -> matchy.de(state))));
+    }
+  }
+
+  record Ctor(
+    @NotNull QName data, @NotNull QName self,
+    @NotNull ImmutableSeq<SerPat> pats,
+    @NotNull ImmutableSeq<SerTerm.SerParam> ownerTele,
+    @NotNull ImmutableSeq<SerTerm.SerParam> selfTele,
+    @NotNull ImmutableSeq<SerPat.Matchy> clauses,
+    @NotNull SerTerm result, boolean coerce
+  ) implements SerDef {
+    @Override public @NotNull CtorDef de(SerTerm.@NotNull DeState state) {
+      return new CtorDef(
+        state.def(data), state.def(self), pats.map(pat -> pat.de(state)),
+        ownerTele.map(tele -> tele.de(state)), selfTele.map(tele -> tele.de(state)),
+        clauses.map(matching -> matching.de(state)),
+        result.de(state), coerce);
+    }
+  }
+
+  record Data(
+    @NotNull QName name,
+    @NotNull ImmutableSeq<SerTerm.SerParam> telescope,
+    @NotNull ImmutableSeq<SerLevel.LvlVar> levels,
+    @NotNull SerTerm result,
+    @NotNull ImmutableSeq<Ctor> bodies
+  ) implements SerDef {
+    @Override public @NotNull Def de(SerTerm.@NotNull DeState state) {
+      return new DataDef(
+        state.def(name), telescope.map(tele -> tele.de(state)),
+        levels.map(level -> level.de(state.levelCache())),
+        result.de(state),
+        bodies.map(body -> body.de(state)));
     }
   }
 }
