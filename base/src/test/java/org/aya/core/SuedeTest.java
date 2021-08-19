4edd590ee037b6486dc831bafde0dc1fe4cdadbf
ice1000 <ice1000kotlin@foxmail.com>
2021-08-18 20:03:02 -0400

    SUEDE: for test coverage (forgive me plz)

--- base/src/test/java/org/aya/core/SuedeTest.java
+++ base/src/test/java/org/aya/core/SuedeTest.java
@@ -7,20 +7,27 @@ import org.aya.core.def.FnDef;
 import org.aya.core.serde.SerTerm;
 import org.aya.core.serde.TermSerializer;
 import org.aya.tyck.TyckDeclTest;
+import org.intellij.lang.annotations.Language;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class SuedeTest {
   @Test public void simpleExpr() {
-    var defs = TyckDeclTest.successTyckDecls("""
+    suedeLastTerm("""
       open data Nat : Set | zero | suc Nat
       def add (a b : Nat) : Nat
        | zero, a => a
        | suc a, b => suc (add a b)
-      def test (a : Nat) => add a zero""");
-    var addAZero = ((FnDef) defs.last()).body.getLeftValue();
-    var ser = addAZero.accept(new TermSerializer(new TermSerializer.SerState()), Unit.unit());
+      def test (a : Nat) => \\x => add a (add x zero)""");
+    suedeLastTerm("def test => Pi (x : Set (lsuc 1)) -> Sig x ** x");
+  }
+
+  private void suedeLastTerm(@Language("TEXT") @NotNull String code) {
+    var defs = TyckDeclTest.successTyckDecls(code);
+    var lastTerm = ((FnDef) defs.last()).body.getLeftValue();
+    var ser = lastTerm.accept(new TermSerializer(new TermSerializer.SerState()), Unit.unit());
     assertNotNull(ser);
     assertNotNull(ser.de(new SerTerm.DeState()));
   }
