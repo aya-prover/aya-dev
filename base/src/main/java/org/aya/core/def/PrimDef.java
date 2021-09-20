9930bfcc5d52f9979951182e911fa931ecf2e648
ice1000 <ice1000kotlin@foxmail.com>
2021-09-20 07:04:18 -0400

    DOC: comment on the part I can understand so far

--- base/src/main/java/org/aya/core/def/PrimDef.java
+++ base/src/main/java/org/aya/core/def/PrimDef.java
@@ -228,10 +228,6 @@ public final class PrimDef extends TopLevelDef {
       return defs.getOption(name);
     }
 
-    public @NotNull Option<DefVar<PrimDef, Decl.PrimDecl>> getRefOpt(@NotNull ID name) {
-      return getOption(name).map(PrimDef::ref);
-    }
-
     public boolean have(@NotNull ID name) {
       return defs.containsKey(name);
     }
--- base/src/main/java/org/aya/tyck/pat/PatClassifier.java
+++ base/src/main/java/org/aya/tyck/pat/PatClassifier.java
@@ -6,6 +6,7 @@ import kala.collection.SeqView;
 import kala.collection.immutable.ImmutableSeq;
 import kala.collection.mutable.Buffer;
 import kala.collection.mutable.MutableMap;
+import kala.control.Option;
 import kala.tuple.primitive.IntObjTuple2;
 import org.aya.api.error.Reporter;
 import org.aya.api.error.SourcePos;
@@ -97,74 +98,102 @@ public record PatClassifier(
       var oneClass = subPatsSeq.map(SubPats::ix);
       return ImmutableSeq.of(new PatClass.Ok(oneClass));
     }
+    // We're gonna split on this type
     var target = telescope.first();
     var explicit = target.explicit();
     switch (target.type().normalize(NormalizeMode.WHNF)) {
       default -> {
       }
+      // The type is sigma type, and do we have any non-catchall patterns?
+      // Note that we cannot have ill-typed patterns such as constructor patterns,
+      // since patterns here are already well-typed
       case FormTerm.Sigma sigma -> {
         var hasTuple = subPatsSeq
           .mapIndexedNotNull((index, subPats) -> subPats.head() instanceof Pat.Tuple tuple
             ? new SubPats(tuple.pats().view(), index) : null);
+        // In case we do,
         if (hasTuple.isNotEmpty()) {
+          // Add a catchall pattern to the pattern tree builder since tuple patterns are irrefutable
           builder.shiftEmpty(explicit);
+          // We will subst the telescope with this fake tuple term
           var thatTuple = new IntroTerm.Tuple(sigma.params().map(Term.Param::toTerm));
+          // Do it!! Just do it!!
           var newTele = telescope.view()
             .drop(1)
             .map(param -> param.subst(target.ref(), thatTuple))
             .toImmutableSeq();
+          // Classify according to the tuple elements
           return classifySub(sigma.params(), hasTuple, coverage)
             .flatMap(pat -> mapClass(pat,
+              // Then, classify according to the rest of the patterns (that comes after the tuple pattern)
               classifySub(newTele, PatClass.extract(pat, subPatsSeq).map(SubPats::drop), coverage)));
         }
       }
+      // Only `I` might be split, we just assume that
       case CallTerm.Prim primCall -> {
         assert primCall.ref().core.id == PrimDef.ID.INTERVAL;
+        // Any prim patterns?
         var lrSplit = subPatsSeq
           .mapNotNull(subPats -> subPats.head() instanceof Pat.Prim prim ? prim : null)
           .firstOption();
         if (lrSplit.isDefined()) {
           var buffer = Buffer.<PatClass>create();
+          // Interval pattern matching is only available in conditions,
+          // so in case we need coverage, report an error on this pattern matching
           if (coverage) reporter.report(new ClausesProblem.SplitInterval(pos, lrSplit.get()));
+          // For `left` and `right`,
           for (var primName : PrimDef.Factory.LEFT_RIGHT) {
             builder.append(new PatTree(primName.id, explicit, 0));
+            var prim = PrimDef.Factory.INSTANCE.getOption(primName);
             var patClass = new PatClass.Ok(subPatsSeq.view()
-              .mapIndexedNotNull((ix, subPats) -> {
-                var head = subPats.head();
-                var existedPrim = PrimDef.Factory.INSTANCE.getOption(primName);
-                return head instanceof Pat.Prim prim && existedPrim.isNotEmpty() && prim.ref() == existedPrim.get().ref()
-                  || head instanceof Pat.Bind ? new SubPats(subPats.pats, ix) : null;
-              }).map(SubPats::ix).toImmutableSeq());
+              // Filter out all patterns that matches it,
+              .mapIndexedNotNull((ix, subPats) -> matches(subPats, ix, prim)).map(SubPats::ix).toImmutableSeq());
+            // We extract the corresponding clauses and drop the current pattern
             var classes = PatClass.extract(patClass, subPatsSeq).map(SubPats::drop);
+            // Probably nonempty, and in this case, prim is defined, so we can safely call `.get`
             if (classes.isNotEmpty()) {
-              var lrCall = new CallTerm.Prim(PrimDef.Factory.INSTANCE.getRefOpt(primName).get(), ImmutableSeq.empty(), ImmutableSeq.empty());
+              // We're gonna instantiate the telescope with this term!
+              var lrCall = new CallTerm.Prim(prim.get().ref, ImmutableSeq.empty(), ImmutableSeq.empty());
               var newTele = telescope.view()
                 .drop(1)
                 .map(param -> param.subst(target.ref(), lrCall))
                 .toImmutableSeq();
+              // Classify according the rest of the patterns
               var rest = classifySub(newTele, classes, false);
-              builder.unshift();
+              // We have some new classes!
               buffer.appendAll(rest);
-            } else builder.unshift();
+            }
+            builder.unshift();
           }
           return buffer.toImmutableSeq();
         }
       }
+      // THE BIG GAME
       case CallTerm.Data dataCall -> {
+        // If there are no remaining clauses, probably it's due to a previous `impossible` clause,
+        // but since we're gonna remove this keyword, this check may not be needed in the future? LOL
         if (subPatsSeq.anyMatch(subPats -> subPats.pats.isNotEmpty()) &&
+          // there are no clauses starting with a constructor pattern -- we don't need a split!
           subPatsSeq.noneMatch(subPats -> subPats.head() instanceof Pat.Ctor)
         ) break;
         var buffer = Buffer.<PatClass>create();
+        // For all constructors,
         for (var ctor : dataCall.ref().core.body) {
           var conTele = ctor.selfTele;
+          // Check if this constructor is available by doing the obvious thing
           if (ctor.pats.isNotEmpty()) {
             var matchy = PatMatcher.tryBuildSubstArgs(ctor.pats, dataCall.args());
+            // If not, forget about this constructor
             if (matchy == null) continue;
             conTele = conTele.map(param -> param.subst(matchy));
           }
+          // Java wants a final local variable, let's alias it
           var conTeleCapture = conTele;
+          // Find all patterns that are either catchall or splitting on this constructor,
+          // e.g. for `suc`, `suc (suc a)` will be picked
           var matches = subPatsSeq
             .mapIndexedNotNull((ix, subPats) -> matches(subPats, ix, conTeleCapture, ctor.ref()));
+          // Push this constructor to the error message builder
           builder.shift(new PatTree(ctor.ref().name(), explicit, conTele.count(Term.Param::explicit)));
           if (telescope.sizeEquals(1) && matches.isEmpty()) {
             if (coverage) buffer.append(new PatClass.Err(ImmutableSeq.empty(),
@@ -199,6 +228,12 @@ public record PatClassifier(
       case PatClass.Ok ok -> classes;
       case PatClass.Err err -> classes.map(newClz -> new PatClass.Err(newClz.contents(), err.errorMessage));
     };
+  }
+
+  private static @Nullable SubPats matches(SubPats subPats, int ix, Option<PrimDef> existedPrim) {
+    var head = subPats.head();
+    return head instanceof Pat.Prim prim && existedPrim.isNotEmpty() && prim.ref() == existedPrim.get().ref()
+      || head instanceof Pat.Bind ? new SubPats(subPats.pats, ix) : null;
   }
 
   private static @Nullable SubPats matches(SubPats subPats, int ix, ImmutableSeq<Term.Param> conTele, Var ctorRef) {
