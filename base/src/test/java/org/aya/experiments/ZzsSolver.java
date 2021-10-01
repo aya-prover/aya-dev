57a440ef49931223e46493816ff6d32dac7d3e70
ice1000 <ice1000kotlin@foxmail.com>
2021-10-01 10:09:38 -0400

    LEVEL: construction of counterexample

--- base/src/test/java/org/aya/experiments/ZzsSolver.java
+++ base/src/test/java/org/aya/experiments/ZzsSolver.java
@@ -307,16 +307,17 @@ public class ZzsSolver {
   }
 
   public static void main(String[] args) throws UnsatException {
-    var res = new ZzsSolver().solve(List.of(new Equation(Ord.Eq, new Max(List.of(new Reference(new Var("Quantifier.u", true), 0))), new Max(List.of(new Reference(new Var("u", false), 0)))),
-      new Equation(Ord.Eq, new Max(List.of(new Reference(new Var("Quantifier.v", true), 0))), new Max(List.of(new Reference(new Var("v", false), 0)))),
+    var res = new ZzsSolver().solve(List.of(
+      new Equation(Ord.Eq, new Max(List.of(new Reference(new Var("Verb.u", true), 0))), new Max(List.of(new Reference(new Var("u", false), 0)))),
+      new Equation(Ord.Eq, new Max(List.of(new Reference(new Var("Verb.v", true), 0))), new Max(List.of(new Reference(new Var("u", false), 0), new Reference(new Var("v", false), 0)))),
+      new Equation(Ord.Lt, new Max(List.of(new Reference(new Var("u", false), 0))), new Max(List.of(new Reference(new Var("Verb.u", true), 0)))),
+      new Equation(Ord.Eq, new Max(List.of(new Reference(new Var("Event.u", true), 0))), new Max(List.of(new Reference(new Var("u", false), 0)))),
       new Equation(Ord.Lt, new Max(List.of(new Reference(new Var("u", false), 0))), new Max(List.of(new Reference(new Var("Event.u", true), 0)))),
-      new Equation(Ord.Lt, new Max(List.of(new Reference(new Var("Event.u", true), 0))), new Max(List.of(new Reference(new Var("Quantifier.u", true), 0)))),
-      new Equation(Ord.Lt, new Max(List.of(new Reference(new Var("u", false), 0))), new Max(List.of(new Reference(new Var("Event.u", true), 0)))),
-      new Equation(Ord.Lt, new Max(List.of(new Reference(new Var("Event.u", true), 0))), new Max(List.of(new Reference(new Var("Quantifier.v", true), 0)))),
+      new Equation(Ord.Lt, new Max(List.of(new Reference(new Var("Event.u", true), 0))), new Max(List.of(new Reference(new Var("Verb.v", true), 0)))),
       new Equation(Ord.Lt, new Max(List.of(new Reference(new Var("Event.u", true), 0))), new Max(List.of(new Reference(new Var("u", false), 0)))),
-      new Equation(Ord.Lt, new Max(List.of(new Reference(new Var("v", false), 0))), new Max(List.of(new Reference(new Var("Quantifier.v", true), 0)))),
-      new Equation(Ord.Lt, new Max(List.of(new Reference(new Var("Event.u", true), 0))), new Max(List.of(new Reference(new Var("Event.u", true), 0)))),
-      new Equation(Ord.Lt, new Max(List.of(new Reference(new Var("Quantifier.v", true), 0))), new Max(List.of(new Reference(new Var("Quantifier.v", true), 0))))));
+      new Equation(Ord.Lt, new Max(List.of(new Reference(new Var("u", false), 0), new Reference(new Var("v", false), 0))), new Max(List.of(new Reference(new Var("Verb.v", true), 0)))),
+      new Equation(Ord.Lt, new Max(List.of(new Reference(new Var("Event.u", true), 0))), new Max(List.of(new Reference(new Var("Verb.u", true), 0)))),
+      new Equation(Ord.Lt, new Max(List.of(new Reference(new Var("Verb.v", true), 0))), new Max(List.of(new Reference(new Var("Verb.v", true), 0))))));
     System.out.println(res);
   }
 }
