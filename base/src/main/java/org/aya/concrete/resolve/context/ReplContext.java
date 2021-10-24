4ab71f580ff89fde34ddeda60d8a14e56f4a347c
ice1000 <ice1000kotlin@foxmail.com>
2021-10-23 20:09:46 -0400

    REPL: address kiva's comment

--- base/src/main/java/org/aya/concrete/resolve/context/ReplContext.java
+++ base/src/main/java/org/aya/concrete/resolve/context/ReplContext.java
@@ -11,28 +11,13 @@ import org.aya.api.ref.Var;
 import org.aya.concrete.stmt.Stmt;
 import org.jetbrains.annotations.NotNull;
 
-public final class ReplContext implements ModuleContext {
-  public final @NotNull Context parent;
-  public final @NotNull MutableMap<String, MutableMap<Seq<String>, Var>> definitions = MutableHashMap.of();
-  public final @NotNull MutableMap<ImmutableSeq<String>, MutableMap<String, Var>> modules = MutableHashMap.of();
-
+public record ReplContext(
+  @Override @NotNull Context parent,
+  @Override @NotNull MutableMap<String, MutableMap<Seq<String>, Var>> definitions,
+  @Override @NotNull MutableMap<ImmutableSeq<String>, MutableMap<String, Var>> modules
+) implements ModuleContext {
   public ReplContext(@NotNull Context parent) {
-    this.parent = parent;
-  }
-
-  @Override
-  public @NotNull Context parent() {
-    return parent;
-  }
-
-  @Override
-  public @NotNull MutableMap<String, MutableMap<Seq<String>, Var>> definitions() {
-    return definitions;
-  }
-
-  @Override
-  public @NotNull MutableMap<ImmutableSeq<String>, MutableMap<String, Var>> modules() {
-    return modules;
+    this(parent, MutableMap.create(), MutableMap.create());
   }
 
   @Override
