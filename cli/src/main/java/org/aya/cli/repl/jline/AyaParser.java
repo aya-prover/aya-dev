949cfdf73ab43395fd7d978fa19fc583f359124f
ice1000 <ice1000kotlin@foxmail.com>
2021-10-25 21:42:00 -0400

    REPL: improve parser error handling

--- cli/src/main/java/org/aya/cli/repl/jline/AyaParser.java
+++ cli/src/main/java/org/aya/cli/repl/jline/AyaParser.java
@@ -4,17 +4,20 @@ package org.aya.cli.repl.jline;
 
 import org.antlr.v4.runtime.Token;
 import org.aya.concrete.parse.AyaParsing;
+import org.aya.parser.AyaLexer;
 import org.jetbrains.annotations.NotNull;
 import org.jline.reader.CompletingParsedLine;
 import org.jline.reader.ParsedLine;
 import org.jline.reader.Parser;
 import org.jline.reader.SyntaxError;
+import org.jline.reader.impl.DefaultParser;
 
+import java.util.Collections;
 import java.util.List;
 
 public class AyaParser implements Parser {
   public record AyaParsedLine(
-    int wordCursor, int rawWordCursor,
+    int wordCursor,
     @NotNull List<@NotNull String> words,
     @NotNull String word,
     int wordIndex,
@@ -25,24 +28,39 @@ public class AyaParser implements Parser {
       return charSequence;
     }
 
+    @Override public int rawWordCursor() {
+      return wordCursor;
+    }
+
     @Override public int rawWordLength() {
       return words().size();
     }
   }
 
   @Override public ParsedLine parse(String line, int cursor, ParseContext context) throws SyntaxError {
+    if (line.isBlank()) return new AyaParsedLine(0, Collections.emptyList(), "", 0, line, cursor);
+    var builtin = (CompletingParsedLine) new DefaultParser().parse(line, cursor, context);
     // Drop the EOF
-    var tokens = AyaParsing.tokens(line).view().dropLast(1);
-    var word = tokens.firstOption(token ->
+    var tokens = AyaParsing.tokens(line).view()
+      .dropLast(1)
+      .filter(token -> token.getChannel() != AyaLexer.HIDDEN)
+      .toImmutableSeq();
+    var wordOpt = tokens.firstOption(token ->
       token.getStartIndex() >= cursor && token.getStopIndex() <= cursor
-    ).getOrElse(tokens::last);
+    );
+    if (wordOpt.isEmpty()) return new AyaParsedLine(
+      0, tokens.stream().map(Token::getText).toList(),
+      tokens.last().getText(), tokens.size() - 1, line, cursor
+    );
+    var word = wordOpt.get();
     // ^ In case we're in a whitespace or at the end
     var wordText = word.getText();
-    return new AyaParsedLine(
-      wordText.length(), word.getStartIndex() - cursor,
+    var parsed = new AyaParsedLine(
+      cursor - word.getStartIndex(),
       tokens.stream().map(Token::getText).toList(),
       wordText, tokens.indexOf(word),
       line, cursor
     );
+    return parsed;
   }
 }
