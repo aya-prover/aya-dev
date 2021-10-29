ca10ed7d3d2d059da0c1b132c6b70ddc2b911490
imkiva <imkiva@islovely.icu>
2021-10-29 09:18:39 +0800

    REPL: simplify code
    Signed-off-by: imkiva <imkiva@islovely.icu>

--- cli/src/main/java/org/aya/cli/repl/jline/AyaReplHighlighter.java
+++ cli/src/main/java/org/aya/cli/repl/jline/AyaReplHighlighter.java
@@ -9,14 +9,11 @@ import org.aya.parser.GeneratedLexerTokens;
 import org.aya.pretty.backend.string.StringPrinterConfig;
 import org.aya.pretty.doc.Doc;
 import org.jetbrains.annotations.NotNull;
-import org.jline.reader.Highlighter;
 import org.jline.reader.LineReader;
 import org.jline.reader.impl.DefaultHighlighter;
 import org.jline.utils.AttributedString;
 
-import java.util.regex.Pattern;
-
-public class AyaReplHighlighter extends DefaultHighlighter implements Highlighter {
+public class AyaReplHighlighter extends DefaultHighlighter {
   @Override
   public AttributedString highlight(LineReader reader, String buffer) {
     var tokens = AyaReplParser.tokensNoEOF(buffer);
@@ -28,15 +25,5 @@ public class AyaReplHighlighter extends DefaultHighlighter implements Highlighte
     return Doc.cat(tokens.map(t -> GeneratedLexerTokens.KEYWORDS.containsKey(t.getType())
       ? Doc.styled(BaseDistiller.KEYWORD, t.getText())
       : Doc.plain(t.getText())));
-  }
-
-  @Override
-  public void setErrorPattern(Pattern errorPattern) {
-    super.setErrorPattern(errorPattern);
-  }
-
-  @Override
-  public void setErrorIndex(int errorIndex) {
-    super.setErrorIndex(errorIndex);
   }
 }
