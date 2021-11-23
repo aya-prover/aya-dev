f9f616bba5a8eb9114baad3a264d7a2cc24ee14b
ice1000 <ice1000kotlin@foxmail.com>
2021-11-23 01:02:00 -0500

    TEST: delete build directory

--- base/src/test/java/org/aya/test/LibraryTest.java
+++ base/src/test/java/org/aya/test/LibraryTest.java
@@ -2,10 +2,13 @@
 // Use of this source code is governed by the MIT license that can be found in the LICENSE.md file.
 package org.aya.test;
 
+import kala.collection.immutable.ImmutableSeq;
 import org.aya.cli.library.LibraryCompiler;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Comparator;
 
 public class LibraryTest {
   @Test public void test() throws IOException {
@@ -13,7 +16,12 @@ public class LibraryTest {
   }
 
   public static void main(String... args) throws IOException {
-    LibraryCompiler.compile(ThrowingReporter.INSTANCE,
-      TestRunner.flags(), TestRunner.DEFAULT_TEST_DIR.resolve("success"));
+    var dir = TestRunner.DEFAULT_TEST_DIR.resolve("success");
+    try (var walk = Files.walk(dir.resolve("build"))) {
+      walk.sorted(Comparator.reverseOrder())
+        .collect(ImmutableSeq.factory())
+        .forEachChecked(Files::delete);
+    }
+    LibraryCompiler.compile(ThrowingReporter.INSTANCE, TestRunner.flags(), dir);
   }
 }
