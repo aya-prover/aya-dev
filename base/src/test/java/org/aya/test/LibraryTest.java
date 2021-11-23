53a9394a1c912795e191193dbbb83388f670b3ad
ice1000 <ice1000kotlin@foxmail.com>
2021-11-23 04:46:41 -0500

    TEST: I sold my soul for the second time

--- base/src/test/java/org/aya/test/LibraryTest.java
+++ base/src/test/java/org/aya/test/LibraryTest.java
@@ -4,18 +4,31 @@ package org.aya.test;
 
 import org.aya.cli.library.LibraryCompiler;
 import org.aya.util.FileUtil;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.nio.file.Path;
 
 public class LibraryTest {
   @Test public void test() throws IOException {
-    main();
+    compile();
+    // Run twice, the second time should load the cache directly.
+    compile();
   }
 
+  public static final Path DIR = TestRunner.DEFAULT_TEST_DIR.resolve("success");
+
   public static void main(String... args) throws IOException {
-    var dir = TestRunner.DEFAULT_TEST_DIR.resolve("success");
-    FileUtil.deleteRecursively(dir.resolve("build"));
-    LibraryCompiler.compile(ThrowingReporter.INSTANCE, TestRunner.flags(), dir);
+    clear();
+    compile();
+  }
+
+  private static void compile() throws IOException {
+    LibraryCompiler.compile(ThrowingReporter.INSTANCE, TestRunner.flags(), DIR);
+  }
+
+  @BeforeAll private static void clear() throws IOException {
+    FileUtil.deleteRecursively(DIR.resolve("build"));
   }
 }
