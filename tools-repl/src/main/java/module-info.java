25114300f2a585ffe2383b74045de81e317c4217
imkiva <imkiva@islovely.icu>
2021-12-01 08:23:33 +0800

    BUILD: make IDEA happy
    Signed-off-by: imkiva <imkiva@islovely.icu>

--- tools-repl/src/main/java/module-info.java
+++ tools-repl/src/main/java/module-info.java
@@ -2,8 +2,10 @@ module org.aya.repl {
   requires static org.jetbrains.annotations;
   requires transitive kala.base;
   requires transitive kala.collection;
+  requires org.antlr.antlr4.runtime;
   requires org.aya.pretty;
   requires org.jline.reader;
+  requires org.jline.terminal;
 
   exports org.aya.repl.antlr;
   exports org.aya.repl;
