42d69fdfb6461ecf8bf4971149dfc2d32440df08
imkiva <imkiva@islovely.icu>
2021-12-21 08:52:18 +0800

    LSP: fix #299
    Signed-off-by: imkiva <imkiva@islovely.icu>

--- lsp/src/main/java/org/aya/lsp/actions/ComputeSignature.java
+++ lsp/src/main/java/org/aya/lsp/actions/ComputeSignature.java
@@ -2,11 +2,13 @@
 // Use of this source code is governed by the MIT license that can be found in the LICENSE.md file.
 package org.aya.lsp.actions;
 
+import kala.collection.immutable.ImmutableSeq;
 import org.aya.api.distill.DistillerOptions;
 import org.aya.api.ref.DefVar;
 import org.aya.api.ref.LocalVar;
 import org.aya.api.ref.Var;
 import org.aya.cli.library.source.LibrarySource;
+import org.aya.concrete.stmt.Signatured;
 import org.aya.core.def.Def;
 import org.aya.core.term.Term;
 import org.aya.distill.BaseDistiller;
@@ -29,17 +31,22 @@ public interface ComputeSignature {
   static @NotNull Doc computeSignature(@NotNull Var target, boolean withResult) {
     return switch (target) {
       case LocalVar localVar -> BaseDistiller.varDoc(localVar);
-      case DefVar<?, ?> defVar -> computeSignature((Def) defVar.core, withResult);
+      case DefVar<?, ?> defVar -> {
+        @SuppressWarnings("unchecked")
+        var ref = (DefVar<? extends Def, ? extends Signatured>) defVar;
+        // #299: hovering a mouse on a definition whose header is failed to tyck
+        if (ref.core == null && ref.concrete.signature == null) yield Doc.empty();
+        yield computeSignature(Def.defTele(ref), Def.defResult(ref), withResult);
+      }
       default -> Doc.empty();
     };
   }
-
-  static @NotNull Doc computeSignature(@NotNull Def core, boolean withResult) {
+  static @NotNull Doc computeSignature(ImmutableSeq<Term.Param> defTele, Term defResult, boolean withResult) {
     var options = DistillerOptions.pretty();
     var distiller = new CoreDistiller(options);
-    var tele = distiller.visitTele(core.telescope(), core.result(), Term::findUsages);
+    var tele = distiller.visitTele(defTele, defResult, Term::findUsages);
     if (withResult) {
-      var res = core.result().toDoc(options);
+      var res = defResult.toDoc(options);
       if (tele.isEmpty()) return res;
       return Doc.stickySep(tele, Doc.symbol(":"), res);
     } else return tele;
