55b1ba0244e3df58fa7efb6f4067b72c62ebcd58
imkiva <imkiva@islovely.icu>
2022-03-23 14:07:46 +0800

    BUILD: use com.google.gson explicitly (#335)
    Signed-off-by: imkiva <imkiva@islovely.icu>

--- lsp/build.gradle.kts
+++ lsp/build.gradle.kts
@@ -1,5 +1,5 @@
-// Copyright (c) 2020-2021 Yinsen (Tesla) Zhang.
-// Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+// Copyright (c) 2020-2022 Yinsen (Tesla) Zhang.
+// Use of this source code is governed by the MIT license that can be found in the LICENSE.md file.
 import org.aya.gradle.CommonTasks
 
 val mainClassQName = "org.aya.lsp.LspMain"
@@ -29,6 +29,7 @@ jlink {
   addExtraDependencies("jline-terminal-jansi")
   mergedModule {
     additive = true
+    requires("com.google.gson")
     uses("org.jline.terminal.spi.JansiSupport")
   }
   launcher {
