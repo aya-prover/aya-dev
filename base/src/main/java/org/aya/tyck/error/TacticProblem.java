23745d89cda238b325736102f2205cd2e96c108a
Luna Xin <weitaoxin@gmail.com>
2022-04-07 00:05:48 -0400

    ERROR: added `TacticProblem`

---
+++ base/src/main/java/org/aya/tyck/error/TacticProblem.java
@@ -0,0 +1,40 @@
+// Copyright (c) 2020-2022 Yinsen (Tesla) Zhang.
+// Use of this source code is governed by the MIT license that can be found in the LICENSE.md file.
+package org.aya.tyck.error;
+
+import org.aya.concrete.Expr;
+import org.aya.pretty.doc.Doc;
+import org.aya.util.distill.DistillerOptions;
+import org.aya.util.error.SourcePos;
+import org.aya.util.reporter.Problem;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * @author luna
+ */
+public interface TacticProblem extends Problem {
+
+  @Override default @NotNull Problem.Severity level() {
+    return Severity.ERROR;
+  }
+
+  record HoleNumberMismatchError(@NotNull SourcePos sourcePos,
+                                 int expected, int supplied) implements TacticProblem {
+    @Override public @NotNull Doc describe(@NotNull DistillerOptions options) {
+      return Doc.sep(Doc.english("Expected"),
+        Doc.plain(String.valueOf(expected)),
+        Doc.english("holes in the tactic head, but found"),
+        Doc.plain(String.valueOf(supplied)));
+    }
+  }
+
+  record TacHeadCannotBeList(@NotNull SourcePos sourcePos, @NotNull Expr.ListExprTac tacList,
+                              @NotNull Expr.TacNode first) implements TacticProblem {
+
+    @Override public @NotNull Doc describe(@NotNull DistillerOptions options) {
+      return Doc.english("Tactic head cannot be list");
+      // TODO: make this more verbose
+    }
+  }
+
+}
