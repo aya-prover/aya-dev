bf285540cd216658ad84016fccd57873b4cafc15
Luna Xin <weitaoxin@gmail.com>
2022-04-10 01:47:26 -0400

    TACTIC: address comment

--- base/src/main/java/org/aya/tyck/ExprTycker.java
+++ base/src/main/java/org/aya/tyck/ExprTycker.java
@@ -383,6 +383,7 @@ public final class ExprTycker extends Tycker {
         var headNode = tacNodes.first();
         var tailNodes = tacNodes.drop(1);
         // enter into a new local state
+        var parentCtx = localCtx;
         localCtx = localCtx.deriveMap();
         if (headNode instanceof TacNode.ExprTac exprTac) {
           var exprToElab = exprTac.expr();
@@ -450,7 +451,7 @@ public final class ExprTycker extends Tycker {
           result = tacFail(listExprTac, new TacticProblem.TacHeadCannotBeList(listExprTac.sourcePos(), listExprTac));
         }
 
-        localCtx = Objects.requireNonNull(localCtx.parent()); // This should allow contexts to revert to original
+        localCtx = parentCtx; // This should allow contexts to revert to original
         yield result;
       }
     };
