a81b300b32ad2ccaa40ae2c065fec078ae823c4a
imkiva <imkiva@islovely.icu>
2022-04-13 15:21:47 +0800

    TYCK: little type should check hole's solution
    Signed-off-by: imkiva <imkiva@islovely.icu>

--- base/src/main/java/org/aya/tyck/LittleTyper.java
+++ base/src/main/java/org/aya/tyck/LittleTyper.java
@@ -19,16 +19,16 @@ import org.jetbrains.annotations.NotNull;
  *
  * @author ice1000
  */
-public record LittleTyper(@NotNull TyckState state, @NotNull LocalCtx localCtx) implements Term.Visitor<Unit, Term> {
-  @Override public Term visitRef(@NotNull RefTerm term, Unit unit) {
+public record LittleTyper(@NotNull TyckState state, @NotNull LocalCtx localCtx) implements Term.Visitor<Unit, @NotNull Term> {
+  @Override public @NotNull Term visitRef(@NotNull RefTerm term, Unit unit) {
     return localCtx.get(term.var());
   }
 
-  @Override public Term visitLam(IntroTerm.@NotNull Lambda term, Unit unit) {
+  @Override public @NotNull Term visitLam(IntroTerm.@NotNull Lambda term, Unit unit) {
     return new FormTerm.Pi(term.param(), term.body().accept(this, unit));
   }
 
-  @Override public Term visitPi(FormTerm.@NotNull Pi term, Unit unit) {
+  @Override public @NotNull Term visitPi(FormTerm.@NotNull Pi term, Unit unit) {
     var paramTyRaw = term.param().type().accept(this, Unit.unit()).normalize(state, NormalizeMode.WHNF);
     var retTyRaw = term.body().accept(this, Unit.unit()).normalize(state, NormalizeMode.WHNF);
     if (paramTyRaw instanceof FormTerm.Univ paramTy && retTyRaw instanceof FormTerm.Univ retTy)
@@ -36,15 +36,15 @@ public record LittleTyper(@NotNull TyckState state, @NotNull LocalCtx localCtx) 
     else return ErrorTerm.typeOf(term);
   }
 
-  @Override public Term visitError(@NotNull ErrorTerm term, Unit unit) {
+  @Override public @NotNull Term visitError(@NotNull ErrorTerm term, Unit unit) {
     return ErrorTerm.typeOf(term);
   }
 
-  @Override public Term visitMetaPat(RefTerm.@NotNull MetaPat metaPat, Unit unit) {
+  @Override public @NotNull Term visitMetaPat(RefTerm.@NotNull MetaPat metaPat, Unit unit) {
     return metaPat.ref().type();
   }
 
-  @Override public Term visitSigma(FormTerm.@NotNull Sigma term, Unit unit) {
+  @Override public @NotNull Term visitSigma(FormTerm.@NotNull Sigma term, Unit unit) {
     var univ = term.params().view()
       .map(param -> param.type()
         .accept(this, Unit.unit()).normalize(state, NormalizeMode.WHNF))
@@ -55,28 +55,28 @@ public record LittleTyper(@NotNull TyckState state, @NotNull LocalCtx localCtx) 
     else return ErrorTerm.typeOf(term);
   }
 
-  @Override public Term visitUniv(FormTerm.@NotNull Univ term, Unit unit) {
+  @Override public @NotNull Term visitUniv(FormTerm.@NotNull Univ term, Unit unit) {
     return new FormTerm.Univ(term.lift() + 1);
   }
 
-  @Override public Term visitApp(ElimTerm.@NotNull App term, Unit unit) {
+  @Override public @NotNull Term visitApp(ElimTerm.@NotNull App term, Unit unit) {
     var piRaw = term.of().accept(this, unit).normalize(state, NormalizeMode.WHNF);
     return piRaw instanceof FormTerm.Pi pi ? pi.substBody(term.arg().term()) : ErrorTerm.typeOf(term);
   }
 
-  @Override public Term visitFnCall(@NotNull CallTerm.Fn fnCall, Unit unit) {
+  @Override public @NotNull Term visitFnCall(@NotNull CallTerm.Fn fnCall, Unit unit) {
     return defCall(fnCall.ref(), fnCall.ulift());
   }
 
-  @Override public Term visitDataCall(@NotNull CallTerm.Data dataCall, Unit unit) {
+  @Override public @NotNull Term visitDataCall(@NotNull CallTerm.Data dataCall, Unit unit) {
     return defCall(dataCall.ref(), dataCall.ulift());
   }
 
-  @Override public Term visitConCall(@NotNull CallTerm.Con conCall, Unit unit) {
+  @Override public @NotNull Term visitConCall(@NotNull CallTerm.Con conCall, Unit unit) {
     return defCall(conCall.head().dataRef(), conCall.ulift());
   }
 
-  @Override public Term visitStructCall(@NotNull CallTerm.Struct structCall, Unit unit) {
+  @Override public @NotNull Term visitStructCall(@NotNull CallTerm.Struct structCall, Unit unit) {
     return defCall(structCall.ref(), structCall.ulift());
   }
 
@@ -85,20 +85,20 @@ public record LittleTyper(@NotNull TyckState state, @NotNull LocalCtx localCtx) 
     return Def.defResult(ref).subst(Subst.EMPTY, ulift);
   }
 
-  @Override public Term visitPrimCall(CallTerm.@NotNull Prim prim, Unit unit) {
+  @Override public @NotNull Term visitPrimCall(CallTerm.@NotNull Prim prim, Unit unit) {
     return defCall(prim.ref(), prim.ulift());
   }
 
-  @Override public Term visitTup(IntroTerm.@NotNull Tuple term, Unit unit) {
+  @Override public @NotNull Term visitTup(IntroTerm.@NotNull Tuple term, Unit unit) {
     return new FormTerm.Sigma(term.items().map(item ->
       new Term.Param(Constants.anonymous(), item.accept(this, Unit.unit()), true)));
   }
 
-  @Override public Term visitNew(IntroTerm.@NotNull New newTerm, Unit unit) {
+  @Override public @NotNull Term visitNew(IntroTerm.@NotNull New newTerm, Unit unit) {
     return newTerm.struct();
   }
 
-  @Override public Term visitProj(ElimTerm.@NotNull Proj term, Unit unit) {
+  @Override public @NotNull Term visitProj(ElimTerm.@NotNull Proj term, Unit unit) {
     var sigmaRaw = term.of().accept(this, unit).normalize(state, NormalizeMode.WHNF);
     if (!(sigmaRaw instanceof FormTerm.Sigma sigma)) return ErrorTerm.typeOf(term);
     var index = term.ix() - 1;
@@ -107,7 +107,7 @@ public record LittleTyper(@NotNull TyckState state, @NotNull LocalCtx localCtx) 
       .subst(ElimTerm.Proj.projSubst(term.of(), index, telescope));
   }
 
-  @Override public Term visitAccess(CallTerm.@NotNull Access term, Unit unit) {
+  @Override public @NotNull Term visitAccess(CallTerm.@NotNull Access term, Unit unit) {
     var callRaw = term.of().accept(this, unit).normalize(state, NormalizeMode.WHNF);
     if (!(callRaw instanceof CallTerm.Struct call)) return ErrorTerm.typeOf(term);
     var core = term.ref().core;
@@ -116,12 +116,14 @@ public record LittleTyper(@NotNull TyckState state, @NotNull LocalCtx localCtx) 
     return core.result().subst(subst);
   }
 
-  @Override public Term visitHole(CallTerm.@NotNull Hole term, Unit unit) {
-    return term.ref().result;
+  @Override public @NotNull Term visitHole(CallTerm.@NotNull Hole term, Unit unit) {
+    var result = term.ref().result;
+    if (result == null) return ErrorTerm.typeOf(term);
+    return result;
   }
 
   @Override
-  public Term visitFieldRef(@NotNull RefTerm.Field term, Unit unit) {
+  public @NotNull Term visitFieldRef(@NotNull RefTerm.Field term, Unit unit) {
     return Def.defType(term.ref());
   }
 }
