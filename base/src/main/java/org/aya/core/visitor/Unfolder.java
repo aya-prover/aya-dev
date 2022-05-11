fa4c8206626cc8b8ff0c10fd4acf1928bfbf1592
imkiva <imkiva@islovely.icu>
2022-05-11 17:22:49 +0800

    TYCK: do not unfold literals

--- base/src/main/java/org/aya/core/visitor/Unfolder.java
+++ base/src/main/java/org/aya/core/visitor/Unfolder.java
@@ -13,7 +13,6 @@ import org.aya.core.def.PrimDef;
 import org.aya.core.pat.PatMatcher;
 import org.aya.core.term.CallTerm;
 import org.aya.core.term.IntroTerm;
-import org.aya.core.term.LitTerm;
 import org.aya.core.term.Term;
 import org.aya.generic.Arg;
 import org.aya.generic.Modifier;
@@ -148,10 +147,6 @@ public interface Unfolder<P> extends TermFixpoint<P> {
     var arguments = buildSubst(fieldDef.ownerTele, term.structArgs());
     var fieldBody = term.fieldArgs().foldLeft(n.params().get(fieldRef), CallTerm::make);
     return fieldBody.subst(arguments).accept(this, p);
-  }
-
-  @Override @NotNull default Term visitShapedLit(@NotNull LitTerm.ShapedInt shaped, P p) {
-    return shaped.constructorForm();
   }
 
   /**
