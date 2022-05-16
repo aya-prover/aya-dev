c04ca613b3b28a1fb87d7be6debfbd3c1fe84a14
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2022-05-16 10:36:29 +0000

    REFACTOR: rewrite Term Serializer with pm

--- base/src/main/java/org/aya/core/serde/Serializer.java
+++ base/src/main/java/org/aya/core/serde/Serializer.java
@@ -22,11 +22,7 @@ import org.jetbrains.annotations.NotNull;
  * @author ice1000
  */
 public record Serializer(@NotNull Serializer.State state) implements
-  Term.Visitor<Unit, SerTerm>,
   Def.Visitor<Unit, SerDef> {
-  private @NotNull SerTerm serialize(@NotNull Term term) {
-    return term.accept(this, Unit.unit());
-  }
 
   private @NotNull SerPat serialize(@NotNull Pat pat) {
     return switch (pat) {
@@ -35,7 +31,7 @@ public record Serializer(@NotNull Serializer.State state) implements
         ctor.explicit(),
         state.def(ctor.ref()),
         serializePats(ctor.params()),
-        visitDataCall(ctor.type(), Unit.unit()));
+        visitDataCall(ctor.type()));
       case Pat.Tuple tuple -> new SerPat.Tuple(tuple.explicit(), serializePats(tuple.pats()));
       case Pat.Bind bind -> new SerPat.Bind(bind.explicit(), state.local(bind.bind()), serialize(bind.type()));
       case Pat.End end -> new SerPat.End(end.isRight(), end.explicit());
@@ -79,51 +75,57 @@ public record Serializer(@NotNull Serializer.State state) implements
     return params.map(this::serialize);
   }
 
-  @Override public SerTerm visitError(@NotNull ErrorTerm term, Unit unit) {
-    throw new AssertionError("Shall not have error term serialized.");
+  private @NotNull SerTerm serialize(@NotNull Term term) {
+    return switch (term) {
+      case CallTerm.Access access -> new SerTerm.Access(
+        serialize(access.of()), state.def(access.ref()),
+        serializeArgs(access.structArgs()),
+        serializeArgs(access.fieldArgs())
+      );
+      case ElimTerm.Proj proj -> new SerTerm.Proj(serialize(proj.of()), proj.ix());
+      case IntroTerm.New newTerm ->
+        new SerTerm.New(visitStructCall(newTerm.struct()), ImmutableMap.from(newTerm.params().view().map((k, v) ->
+          Tuple.of(state.def(k), serialize(v)))));
+      case IntroTerm.Tuple tuple -> new SerTerm.Tup(tuple.items().map(this::serialize));
+      case CallTerm.Prim prim ->
+        new SerTerm.PrimCall(state.def(prim.ref()), prim.id(), serializeCall(prim.ulift(), prim.args()));
+      case CallTerm.Struct structCall -> visitStructCall(structCall);
+      case CallTerm.Con conCall -> new SerTerm.ConCall(
+        state.def(conCall.head().dataRef()), state.def(conCall.head().ref()),
+        serializeCall(conCall.head().ulift(), conCall.head().dataArgs()),
+        serializeArgs(conCall.args())
+      );
+      case CallTerm.Data dataCall -> visitDataCall(dataCall);
+      case CallTerm.Fn fnCall ->
+        new SerTerm.FnCall(state.def(fnCall.ref()), serializeCall(fnCall.ulift(), fnCall.args()));
+      case ElimTerm.App app -> new SerTerm.App(serialize(app.of()), serialize(app.arg()));
+      case FormTerm.Sigma sigma -> new SerTerm.Sigma(serializeParams(sigma.params()));
+      case FormTerm.Univ univ -> new SerTerm.Univ(univ.lift());
+      case FormTerm.Pi pi -> new SerTerm.Pi(serialize(pi.param()), serialize(pi.body()));
+      case FormTerm.Interval interval -> new SerTerm.Interval();
+      case IntroTerm.Lambda lambda -> new SerTerm.Lam(serialize(lambda.param()), serialize(lambda.body()));
+      case RefTerm.MetaPat metaPat -> throw new AssertionError("Shall not have metaPats serialized.");
+      case RefTerm.Field fieldRef -> new SerTerm.FieldRef(state.def(fieldRef.ref()), fieldRef.lift());
+      case RefTerm ref -> new SerTerm.Ref(state.local(ref.var()), ref.lift());
+      case CallTerm.Hole hole -> throw new AssertionError("Shall not have holes serialized.");
+      case PrimTerm.End end -> new SerTerm.End(end.isRight());
+      case ErrorTerm error -> throw new AssertionError("Shall not have errors serialized.");
+      case LitTerm.ShapedInt shapedInt -> throw new UnsupportedOperationException("TODO");
+    };
   }
 
-  @Override public SerTerm visitMetaPat(RefTerm.@NotNull MetaPat metaPat, Unit unit) {
-    throw new AssertionError("Shall not have metaPats serialized.");
+  private SerTerm.DataCall visitDataCall(@NotNull CallTerm.Data dataCall) {
+    return new SerTerm.DataCall(
+      state.def(dataCall.ref()),
+      serializeCall(dataCall.ulift(), dataCall.args())
+    );
   }
 
-  @Override
-  public SerTerm visitInterval(FormTerm.@NotNull Interval interval, Unit unit) {
-    return new SerTerm.Interval();
-  }
-
-  @Override
-  public SerTerm visitEnd(PrimTerm.@NotNull End end, Unit unit) {
-    return new SerTerm.End(end.isRight());
-  }
-
-  @Override public SerTerm visitHole(CallTerm.@NotNull Hole term, Unit unit) {
-    throw new AssertionError("Shall not have holes serialized.");
-  }
-
-  @Override
-  public SerTerm visitFieldRef(@NotNull RefTerm.Field term, Unit unit) {
-    return new SerTerm.FieldRef(state.def(term.ref()), term.lift());
-  }
-
-  @Override public SerTerm visitRef(@NotNull RefTerm term, Unit unit) {
-    return new SerTerm.Ref(state.local(term.var()), term.lift());
-  }
-
-  @Override public SerTerm visitLam(IntroTerm.@NotNull Lambda term, Unit unit) {
-    return new SerTerm.Lam(serialize(term.param()), serialize(term.body()));
-  }
-
-  @Override public SerTerm visitPi(FormTerm.@NotNull Pi term, Unit unit) {
-    return new SerTerm.Pi(serialize(term.param()), serialize(term.body()));
-  }
-
-  @Override public SerTerm visitSigma(FormTerm.@NotNull Sigma term, Unit unit) {
-    return new SerTerm.Sigma(serializeParams(term.params()));
-  }
-
-  @Override public SerTerm visitUniv(FormTerm.@NotNull Univ term, Unit unit) {
-    return new SerTerm.Univ(term.lift());
+  private SerTerm.StructCall visitStructCall(@NotNull CallTerm.Struct structCall) {
+    return new SerTerm.StructCall(
+      state.def(structCall.ref()),
+      serializeCall(structCall.ulift(), structCall.args())
+    );
   }
 
   private @NotNull ImmutableSeq<SerTerm.SerArg> serializeArgs(@NotNull ImmutableSeq<Arg<Term>> args) {
@@ -134,65 +136,10 @@ public record Serializer(@NotNull Serializer.State state) implements
     return pats.map(this::serialize);
   }
 
-  @Override public SerTerm visitApp(ElimTerm.@NotNull App term, Unit unit) {
-    return new SerTerm.App(serialize(term.of()), serialize(term.arg()));
-  }
-
   private @NotNull SerTerm.CallData serializeCall(
     int ulift,
     @NotNull ImmutableSeq<Arg<@NotNull Term>> args) {
     return new SerTerm.CallData(ulift, serializeArgs(args));
-  }
-
-  @Override public SerTerm visitFnCall(@NotNull CallTerm.Fn fnCall, Unit unit) {
-    return new SerTerm.FnCall(state.def(fnCall.ref()), serializeCall(fnCall.ulift(), fnCall.args()));
-  }
-
-  @Override public SerTerm.DataCall visitDataCall(@NotNull CallTerm.Data dataCall, Unit unit) {
-    return new SerTerm.DataCall(
-      state.def(dataCall.ref()),
-      serializeCall(dataCall.ulift(), dataCall.args())
-    );
-  }
-
-  @Override public SerTerm visitConCall(@NotNull CallTerm.Con conCall, Unit unit) {
-    return new SerTerm.ConCall(
-      state.def(conCall.head().dataRef()), state.def(conCall.head().ref()),
-      serializeCall(conCall.head().ulift(), conCall.head().dataArgs()),
-      serializeArgs(conCall.args())
-    );
-  }
-
-  @Override public SerTerm.StructCall visitStructCall(@NotNull CallTerm.Struct structCall, Unit unit) {
-    return new SerTerm.StructCall(
-      state.def(structCall.ref()),
-      serializeCall(structCall.ulift(), structCall.args())
-    );
-  }
-
-  @Override public SerTerm visitPrimCall(CallTerm.@NotNull Prim prim, Unit unit) {
-    return new SerTerm.PrimCall(state.def(prim.ref()), prim.id(), serializeCall(prim.ulift(), prim.args()));
-  }
-
-  @Override public SerTerm visitTup(IntroTerm.@NotNull Tuple term, Unit unit) {
-    return new SerTerm.Tup(term.items().map(this::serialize));
-  }
-
-  @Override public SerTerm visitNew(IntroTerm.@NotNull New newTerm, Unit unit) {
-    return new SerTerm.New(visitStructCall(newTerm.struct(), unit), ImmutableMap.from(newTerm.params().view().map((k, v) ->
-      Tuple.of(state.def(k), serialize(v)))));
-  }
-
-  @Override public SerTerm visitProj(ElimTerm.@NotNull Proj term, Unit unit) {
-    return new SerTerm.Proj(serialize(term.of()), term.ix());
-  }
-
-  @Override public SerTerm visitAccess(CallTerm.@NotNull Access term, Unit unit) {
-    return new SerTerm.Access(
-      serialize(term.of()), state.def(term.ref()),
-      serializeArgs(term.structArgs()),
-      serializeArgs(term.fieldArgs())
-    );
   }
 
   @Override public SerDef visitFn(@NotNull FnDef def, Unit unit) {
@@ -248,10 +195,5 @@ public record Serializer(@NotNull Serializer.State state) implements
   @Override public SerDef visitPrim(@NotNull PrimDef def, Unit unit) {
     assert def.ref.module != null;
     return new SerDef.Prim(def.ref.module, def.id);
-  }
-
-  @Override public SerTerm visitShapedLit(LitTerm.@NotNull ShapedInt shaped, Unit unit) {
-    // TODO: serialize lit terms
-    throw new UnsupportedOperationException("TODO");
   }
 }
