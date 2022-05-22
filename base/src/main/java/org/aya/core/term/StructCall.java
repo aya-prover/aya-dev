420f02664d3e9507a4cf173f7ae981cac0966b2b
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2022-05-22 12:10:49 +0000

    STRUCT: rewrite StructCall

--- base/src/main/java/org/aya/core/term/StructCall.java
+++ base/src/main/java/org/aya/core/term/StructCall.java
@@ -16,57 +16,22 @@ import org.jetbrains.annotations.NotNull;
 /**
  * @author zaoqi
  */
-public sealed interface StructCall extends Term {
-  @NotNull DefVar<StructDef, StructDecl> ref();
-  @NotNull Option<DefVar<FieldDef, Decl.StructField>> nextField();
-  @NotNull ImmutableSeq<Tuple2<DefVar<FieldDef, Decl.StructField>, Arg<@NotNull Term>>> params();
-  int ulift();
-
-  record Ref(
-    @NotNull DefVar<StructDef, StructDecl> ref,
-    int ulift
-  ) implements StructCall {
-    @Override
-    public <P, R> R doAccept(@NotNull Visitor<P, R> visitor, P p) {
-      throw new UnsupportedOperationException("TODO");
-    }
-
-    @Override
-    public @NotNull Option<DefVar<FieldDef, Decl.StructField>> nextField() {
-      return ref.core.allFields().getOption(0).map(FieldDef::ref);
-    }
-
-    @Override
-    public @NotNull ImmutableSeq<Tuple2<DefVar<FieldDef, Decl.StructField>, Arg<Term>>> params() {
-      return ImmutableSeq.empty();
-    }
+public record StructCall(
+  @NotNull DefVar<StructDef, StructDecl> ref,
+  int ulift,
+  @NotNull ImmutableSeq<Tuple2<DefVar<FieldDef, Decl.StructField>, Arg<@NotNull Term>>> params
+) implements Term {
+  @Override
+  public <P, R> R doAccept(@NotNull Visitor<P, R> visitor, P p) {
+    return visitor.visitStructCall(this, p);
   }
 
-  record Call(
-    @NotNull StructCall struct,
-    int ulift,
-    @NotNull DefVar<FieldDef, Decl.StructField> field,
-    @NotNull Arg<@NotNull Term> arg
-  ) implements StructCall {
-    @Override
-    public <P, R> R doAccept(@NotNull Visitor<P, R> visitor, P p) {
-      return null;
-    }
+  public @NotNull Option<DefVar<FieldDef, Decl.StructField>> nextField() {
+    var params = params();
+    return ref().core.allFields().findFirst(field -> !params.find(x -> x._1 == field.ref).isDefined()).map(FieldDef::ref);
+  }
 
-    @Override
-    public @NotNull DefVar<StructDef, StructDecl> ref() {
-      return struct.ref();
-    }
-
-    @Override
-    public @NotNull Option<DefVar<FieldDef, Decl.StructField>> nextField() {
-      var params = params();
-      return ref().core.allFields().findFirst(field -> !params.find(x -> x._1 == field.ref).isDefined()).map(FieldDef::ref);
-    }
-
-    @Override
-    public @NotNull ImmutableSeq<Tuple2<DefVar<FieldDef, Decl.StructField>, Arg<Term>>> params() {
-      return struct.params().appended(Tuple2.of(field, arg));
-    }
+  public @NotNull StructCall apply(@NotNull FieldDef field, @NotNull Arg<Term> arg) {
+    return new StructCall(ref, ulift, params.appended(Tuple2.of(field.ref(), arg)));
   }
 }
