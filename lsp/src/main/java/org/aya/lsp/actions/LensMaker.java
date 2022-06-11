888c3071053d6c2b09031d8531e1f723ec65c35b
imkiva <imkiva@islovely.icu>
2022-06-11 17:41:30 +0800

    MISC: reduce unnecessary traversal in LSP

--- lsp/src/main/java/org/aya/lsp/actions/LensMaker.java
+++ lsp/src/main/java/org/aya/lsp/actions/LensMaker.java
@@ -9,7 +9,6 @@ import kala.collection.mutable.MutableList;
 import org.aya.cli.library.source.LibraryOwner;
 import org.aya.cli.library.source.LibrarySource;
 import org.aya.concrete.stmt.Decl;
-import org.aya.concrete.visitor.StmtOps;
 import org.aya.lsp.utils.Log;
 import org.aya.lsp.utils.LspRange;
 import org.aya.lsp.utils.Resolver;
@@ -20,7 +19,7 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
-public record LensMaker(@NotNull SeqView<LibraryOwner> libraries) implements StmtOps<@NotNull MutableList<CodeLens>> {
+public record LensMaker(@NotNull SeqView<LibraryOwner> libraries) implements SyntaxDeclAction<@NotNull MutableList<CodeLens>> {
   public static @NotNull List<CodeLens> invoke(@NotNull LibrarySource source, @NotNull SeqView<LibraryOwner> libraries) {
     var lens = MutableList.<CodeLens>create();
     var maker = new LensMaker(libraries);
@@ -37,7 +36,7 @@ public record LensMaker(@NotNull SeqView<LibraryOwner> libraries) implements Stm
   @Override
   public void visitDecl(@NotNull Decl maybe, @NotNull MutableList<CodeLens> pp) {
     Resolver.withChildren(maybe).forEach(defVar -> buildLens(defVar, pp));
-    StmtOps.super.visitDecl(maybe, pp);
+    SyntaxDeclAction.super.visitDecl(maybe, pp);
   }
 
   private void buildLens(@NotNull DefVar<?, ?> defVar, @NotNull MutableList<CodeLens> pp) {
---
+++ lsp/src/main/java/org/aya/lsp/actions/SyntaxDeclAction.java
@@ -0,0 +1,29 @@
+// Copyright (c) 2020-2022 Yinsen (Tesla) Zhang.
+// Use of this source code is governed by the MIT license that can be found in the LICENSE.md file.
+package org.aya.lsp.actions;
+
+import org.aya.concrete.stmt.Command;
+import org.aya.concrete.stmt.Decl;
+import org.aya.concrete.stmt.Stmt;
+import org.aya.concrete.visitor.StmtOps;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Traverse only definitions' name and commands.
+ *
+ * @author kiva
+ * @implNote This does not modify the AST.
+ */
+public interface SyntaxDeclAction<P> extends StmtOps<P> {
+  @Override default void visit(@NotNull Stmt stmt, P pp) {
+    switch (stmt) {
+      case Decl decl -> visitDecl(decl, pp);
+      case Command cmd -> visitCommand(cmd, pp);
+      case Stmt misc -> {}
+    }
+  }
+
+  @Override default void visitDecl(@NotNull Decl decl, P pp) {
+    // should not call super
+  }
+}
