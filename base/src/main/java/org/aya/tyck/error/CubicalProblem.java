76933589054b3fd3838434d6e618378850fc8618
imkiva <imkiva@islovely.icu>
2022-08-29 19:42:24 +0800

    MISC: slightly polish error message

--- base/src/main/java/org/aya/tyck/error/CubicalProblem.java
+++ base/src/main/java/org/aya/tyck/error/CubicalProblem.java
@@ -26,7 +26,8 @@ public sealed interface CubicalProblem extends ExprProblem {
     @Override public @NotNull Doc describe(@NotNull DistillerOptions options) {
       return Doc.sep(Doc.english("This path lambda expects"),
         Doc.plain(String.valueOf(expectedDim)),
-        Doc.english("dimension(s), but it has" + (actualDim < expectedDim ? " only" : "")),
+        Doc.english(expectedDim == 1 ? "parameter," : "parameters,"),
+        Doc.english("but it has" + (actualDim < expectedDim ? " only" : "")),
         Doc.plain(String.valueOf(actualDim)));
     }
   }
