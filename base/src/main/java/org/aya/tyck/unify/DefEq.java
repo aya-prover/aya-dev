18a1302334a7625ab2cbddd0d8fe811cca21f097
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2022-08-31 20:51:54 +0800

    LEVEL: report error when unknown lift is compared to unknown lift

--- base/src/main/java/org/aya/tyck/unify/DefEq.java
+++ base/src/main/java/org/aya/tyck/unify/DefEq.java
@@ -523,7 +523,9 @@ public final class DefEq {
   }
 
   private boolean compareLevel(int l, int r) {
-    var valid = l != FormTerm.Univ.UNKNOWN_LIFT && r != FormTerm.Univ.UNKNOWN_LIFT;
+    var lvalid = l != FormTerm.Univ.UNKNOWN_LIFT;
+    var rvalid = r != FormTerm.Univ.UNKNOWN_LIFT;
+    var valid = lvalid && rvalid;
     switch (cmp) {
       case Eq:
         if (valid && l != r) {
@@ -531,12 +533,12 @@ public final class DefEq {
           return false;
         }
       case Gt:
-        if (valid && l < r || r == FormTerm.Univ.UNKNOWN_LIFT) {
+        if (valid && l < r || lvalid && r == FormTerm.Univ.UNKNOWN_LIFT) {
           reporter.report(new LevelError(pos, l, r, false));
           return false;
         }
       case Lt:
-        if (valid && l > r || l == FormTerm.Univ.UNKNOWN_LIFT) {
+        if (valid && l > r || rvalid && l == FormTerm.Univ.UNKNOWN_LIFT) {
           reporter.report(new LevelError(pos, r, l, false));
           return false;
         }
