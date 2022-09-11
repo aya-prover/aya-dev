acfa3a98ff12d82bce33f07729f3849af125535c
imkiva <imkiva@islovely.icu>
2022-09-11 17:18:51 +0800

    cleanup: less visitors

--- base/src/main/java/org/aya/tyck/trace/MdUnicodeTrace.java
+++ base/src/main/java/org/aya/tyck/trace/MdUnicodeTrace.java
@@ -3,14 +3,13 @@
 package org.aya.tyck.trace;
 
 import kala.collection.mutable.MutableList;
-import kala.tuple.Unit;
 import org.aya.distill.BaseDistiller;
 import org.aya.pretty.doc.Doc;
 import org.aya.pretty.doc.Style;
 import org.aya.util.distill.DistillerOptions;
 import org.jetbrains.annotations.NotNull;
 
-public class MdUnicodeTrace implements Trace.Visitor<Unit, Doc> {
+public class MdUnicodeTrace {
   public final int indent;
   public final @NotNull DistillerOptions options;
   public static final @NotNull Doc plus = Doc.symbol("+");
@@ -25,56 +24,49 @@ public class MdUnicodeTrace implements Trace.Visitor<Unit, Doc> {
     this(2, DistillerOptions.informative());
   }
 
-  @Override public Doc visitDecl(Trace.@NotNull DeclT t, Unit unit) {
-    return Doc.vcatNonEmpty(Doc.sep(plus, BaseDistiller.varDoc(t.var())),
-      indentedChildren(t.children()));
-  }
-
   private @NotNull Doc indentedChildren(MutableList<@NotNull Trace> children) {
-    return Doc.nest(indent, Doc.vcat(children.view().map(trace -> trace.accept(this, Unit.unit()))));
+    return Doc.nest(indent, Doc.vcat(children.view().map(this::docify)));
   }
 
-  @Override public Doc visitExpr(Trace.@NotNull ExprT t, Unit unit) {
-    var buf = MutableList.of(plus, Doc.symbol("\u22A2"), Doc.styled(Style.code(), t.expr().toDoc(options)));
-    if (t.term() != null) {
-      buf.append(colon);
-      buf.append(Doc.styled(Style.code(), t.term().toDoc(options)));
-    }
-    return Doc.vcatNonEmpty(Doc.sep(buf), indentedChildren(t.children()));
-  }
-
-  @Override public Doc visitUnify(Trace.@NotNull UnifyT t, Unit unit) {
-    var buf = MutableList.of(plus,
-      Doc.symbol("\u22A2"),
-      Doc.styled(Style.code(), t.lhs().toDoc(options)),
-      Doc.symbol("\u2261"),
-      t.rhs().toDoc(options));
-    if (t.type() != null) {
-      buf.append(colon);
-      buf.append(Doc.styled(Style.code(), t.type().toDoc(options)));
-    }
-    return Doc.vcatNonEmpty(Doc.sep(buf), indentedChildren(t.children()));
-  }
-
-  @Override public Doc visitTyck(Trace.@NotNull TyckT t, Unit unit) {
-    assert t.children().isEmpty();
-    return Doc.sep(plus, Doc.plain("result"), Doc.symbol("\u22A2"),
-      Doc.styled(Style.code(), t.term().toDoc(options)), Doc.symbol("\u2191"),
-      Doc.styled(Style.code(), t.type().toDoc(options)));
-  }
-
-  @Override public Doc visitPat(Trace.@NotNull PatT t, Unit unit) {
-    return Doc.vcat(Doc.sep(plus, Doc.plain("pat"), Doc.symbol("\u22A2"),
-        Doc.styled(Style.code(), t.pat().toDoc(options)), colon,
-        Doc.styled(Style.code(), t.term().toDoc(options))),
-      indentedChildren(t.children()));
-  }
-
-  @Override public Doc visitLabel(Trace.@NotNull LabelT t, Unit unit) {
-    return Doc.vcatNonEmpty(Doc.sep(plus, Doc.english(t.label())), indentedChildren(t.children()));
+  public @NotNull Doc docify(@NotNull Trace trace) {
+    return switch (trace) {
+      case Trace.DeclT t -> Doc.vcatNonEmpty(Doc.sep(plus, BaseDistiller.varDoc(t.var())),
+        indentedChildren(t.children()));
+      case Trace.LabelT t -> Doc.vcatNonEmpty(Doc.sep(plus, Doc.english(t.label())), indentedChildren(t.children()));
+      case Trace.ExprT t -> {
+        var buf = MutableList.of(plus, Doc.symbol("\u22A2"), Doc.styled(Style.code(), t.expr().toDoc(options)));
+        if (t.term() != null) {
+          buf.append(colon);
+          buf.append(Doc.styled(Style.code(), t.term().toDoc(options)));
+        }
+        yield Doc.vcatNonEmpty(Doc.sep(buf), indentedChildren(t.children()));
+      }
+      case Trace.PatT t -> Doc.vcat(Doc.sep(plus, Doc.plain("pat"), Doc.symbol("\u22A2"),
+          Doc.styled(Style.code(), t.pat().toDoc(options)), colon,
+          Doc.styled(Style.code(), t.type().toDoc(options))),
+        indentedChildren(t.children()));
+      case Trace.TyckT t -> {
+        assert t.children().isEmpty();
+        yield Doc.sep(plus, Doc.plain("result"), Doc.symbol("\u22A2"),
+          Doc.styled(Style.code(), t.term().toDoc(options)), Doc.symbol("\u2191"),
+          Doc.styled(Style.code(), t.type().toDoc(options)));
+      }
+      case Trace.UnifyT t -> {
+        var buf = MutableList.of(plus,
+          Doc.symbol("\u22A2"),
+          Doc.styled(Style.code(), t.lhs().toDoc(options)),
+          Doc.symbol("\u2261"),
+          t.rhs().toDoc(options));
+        if (t.type() != null) {
+          buf.append(colon);
+          buf.append(Doc.styled(Style.code(), t.type().toDoc(options)));
+        }
+        yield Doc.vcatNonEmpty(Doc.sep(buf), indentedChildren(t.children()));
+      }
+    };
   }
 
   public @NotNull Doc docify(Trace.@NotNull Builder traceBuilder) {
-    return Doc.vcatNonEmpty(traceBuilder.root().view().map(e -> e.accept(this, Unit.unit())));
+    return Doc.vcatNonEmpty(traceBuilder.root().view().map(this::docify));
   }
 }
