d23d628f94c8bfce024b3d9ce8bb145e54d38385
ice1000 <ice1000kotlin@foxmail.com>
2022-09-17 01:13:22 -0400

    test: fix part of the tests

--- base/src/test/java/org/aya/cubical/PathTest.java
+++ base/src/test/java/org/aya/cubical/PathTest.java
@@ -26,7 +26,7 @@ public class PathTest {
     IntFunction<Doc> distiller = i -> res._2.get(i).toDoc(DistillerOptions.debug());
     assertEquals("def = {A : Type 0} (a b : A) : Type 0 => [| i |] A {| ~ i := a | i := b |}",
       distiller.apply(3).debugRender());
-    assertEquals("def idp {A : Type 0} {a : A} : (=) {A} a a => \\ (i : I) => a",
+    assertEquals("def idp {A : Type 0} {a : A} : (=) {A} a a => \\ i => a",
       distiller.apply(4).debugRender());
   }
 
