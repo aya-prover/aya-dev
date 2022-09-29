b70139914e7b8d3b19b6d0c8865cbec5959abb5e
imkiva <imkiva@islovely.icu>
2022-09-29 22:37:20 +0800

    parse: temporarily revert Remark parsing

--- cli/src/main/java/org/aya/cli/parse/AyaGKProducer.java
+++ cli/src/main/java/org/aya/cli/parse/AyaGKProducer.java
@@ -85,7 +85,8 @@ public record AyaGKProducer(
     for (var docComment : node.childrenOfType(DOC_COMMENT)) {
       sb.append(docComment.tokenText().substring(3)).append("\n");
     }
-    return Remark.make(sb.toString(), sourcePosOf(node), new AyaGKParserImpl(reporter));
+    return Remark.make(sb.toString(), sourcePosOf(node), new AyaParserImpl(reporter));
+    // TODO: AyaGKParserImpl.expr() does not parse expr
   }
 
   public @NotNull Generalize generalize(@NotNull GenericNode<?> node) {
--- parser-migrate/src/main/java/org/aya/parser/ij/AyaParserDefinitionBase.java
+++ parser-migrate/src/main/java/org/aya/parser/ij/AyaParserDefinitionBase.java
@@ -29,7 +29,8 @@ public abstract class AyaParserDefinitionBase implements ParserDefinition {
   }
 
   @Override public @NotNull TokenSet getCommentTokens() {
-    return COMMENTS;
+    // Remark needs DOC_COMMENT, do not skip it.
+    return SKIP_COMMENTS;
   }
 
   @Override public @NotNull TokenSet getStringLiteralElements() {
@@ -41,6 +42,7 @@ public abstract class AyaParserDefinitionBase implements ParserDefinition {
   public static final @NotNull AyaPsiTokenType DOC_COMMENT = (AyaPsiTokenType) AyaPsiElementTypes.DOC_COMMENT;
   public static final @NotNull TokenSet IDENTIFIERS = TokenSet.create(AyaPsiElementTypes.ID, AyaPsiElementTypes.REPL_COMMAND);
   public static final @NotNull TokenSet COMMENTS = TokenSet.create(LINE_COMMENT, BLOCK_COMMENT, DOC_COMMENT);
+  public static final @NotNull TokenSet SKIP_COMMENTS = TokenSet.create(LINE_COMMENT, BLOCK_COMMENT);
   public static final @NotNull TokenSet STRINGS = TokenSet.create(AyaPsiElementTypes.STRING);
   public static final @NotNull TokenSet MARKERS = TokenSet.create(
     AyaPsiElementTypes.COLON,
