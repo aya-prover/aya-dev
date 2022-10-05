7197aaaa96a8648e2c9be90760dd767ef74b8b24
ice1000 <ice1000kotlin@foxmail.com>
2022-10-05 08:49:26 -0400

    refactor: get rid of stream

--- tools-repl/src/main/java/org/aya/repl/gk/JFlexAdapter.java
+++ tools-repl/src/main/java/org/aya/repl/gk/JFlexAdapter.java
@@ -2,7 +2,6 @@
 // Use of this source code is governed by the MIT license that can be found in the LICENSE.md file.
 package org.aya.repl.gk;
 
-import com.intellij.KalaTODO;
 import com.intellij.lexer.FlexLexer;
 import com.intellij.psi.tree.IElementType;
 import kala.collection.Seq;
@@ -10,22 +9,18 @@ import kala.function.CheckedSupplier;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
-import java.util.Objects;
-import java.util.stream.Stream;
 
 public record JFlexAdapter(@NotNull FlexLexer lexer) {
-  // TODO: https://github.com/Glavo/kala-common/issues/61
-
   /** @return parsed tokens without the last EOF token */
-  @KalaTODO @NotNull Seq<Token> tokensNoEOF(String line) {
+  @NotNull Seq<Token> tokensNoEOF(String line) {
     lexer.reset(line, 0, line.length(), 0);
     CheckedSupplier<IElementType, IOException> action = lexer::advance;
-    return Seq.wrapJava(Stream.generate(() -> {
+    return Seq.generateUntilNull(() -> {
       var type = action.get();
       if (type == null) return null;
       return new Token(lexer.getTokenStart(), lexer.getTokenEnd(), type,
         line.substring(lexer.getTokenStart(), lexer.getTokenEnd()));
-    }).takeWhile(Objects::nonNull).toList());
+    });
   }
 
   public record Token(int tokenStart, int tokenEnd, @NotNull IElementType type, @NotNull String text) {
