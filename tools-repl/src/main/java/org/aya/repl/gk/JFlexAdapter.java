71ac4a60953082bb987c5a3f6d3c14783de737a2
ice1000 <ice1000kotlin@foxmail.com>
2022-10-05 00:56:05 -0400

    misc: fix test

--- tools-repl/src/main/java/org/aya/repl/gk/JFlexAdapter.java
+++ tools-repl/src/main/java/org/aya/repl/gk/JFlexAdapter.java
@@ -15,12 +15,14 @@ import java.util.stream.Stream;
 
 public record JFlexAdapter(@NotNull FlexLexer lexer) {
   // TODO: https://github.com/Glavo/kala-common/issues/61
+
   /** @return parsed tokens without the last EOF token */
- @KalaTODO @NotNull Seq<Token> tokensNoEOF(String line) {
+  @KalaTODO @NotNull Seq<Token> tokensNoEOF(String line) {
     lexer.reset(line, 0, line.length(), 0);
     CheckedSupplier<IElementType, IOException> action = lexer::advance;
     return Seq.wrapJava(Stream.generate(() -> {
       var type = action.get();
+      if (type == null) return null;
       return new Token(lexer.getTokenStart(), lexer.getTokenEnd(), type,
         line.substring(lexer.getTokenStart(), lexer.getTokenEnd()));
     }).takeWhile(Objects::nonNull).toList());
