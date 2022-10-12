d37e4c5b9b2acf27fac20efbebfe0bc549e889d4
HoshinoTented <limbolrain@gmail.com>
2022-10-12 23:50:26 +0800

    misc: enumerable key

--- pretty/src/main/java/org/aya/pretty/style/AyaStyleFamily.java
+++ pretty/src/main/java/org/aya/pretty/style/AyaStyleFamily.java
@@ -13,26 +13,47 @@ import org.jetbrains.annotations.NotNull;
 public record AyaStyleFamily(
   @NotNull MutableMap<String, Styles> definedStyles
 ) implements StyleFamily {
+  public enum Key {
+    Keyword("aya:Keyword"),
+    PrimCall("aya:PrimCall"),
+    FnCall("aya:FnCall"),
+    DataCall("aya:DataCall"),
+    StructCall("aya:StructCall"),
+    ConCall("aya:ConCall"),
+    FieldCall("aya:FieldCall"),
+    Generalized("aya:Generalized");
+
+    private final String key;
+
+    Key(String key) {
+      this.key = key;
+    }
+
+    public String key() {
+      return key;
+    }
+  }
+
   public static final @NotNull AyaStyleFamily DEFAULT = new AyaStyleFamily(MutableMap.ofEntries(
-    Tuple.of("aya:Keyword", Style.bold().and().color("aya:Keyword")),
-    Tuple.of("aya:PrimCall", Style.color("aya:Keyword").and()),
-    Tuple.of("aya:FnCall", Style.color("aya:FnCall").and()),
-    Tuple.of("aya:DataCall", Style.color("aya:DataCall").and()),
-    Tuple.of("aya:StructCall", Style.color("aya:StructCall").and()),
-    Tuple.of("aya:ConCall", Style.color("aya:ConCall").and()),
-    Tuple.of("aya:FieldCall", Style.color("aya:FieldCall").and()),
-    Tuple.of("aya:Generalized", Style.color("aya:Generalized").and())
+    Tuple.of(Key.Keyword.key(), Style.bold().and().color("aya:Keyword")),
+    Tuple.of(Key.PrimCall.key(), Style.color("aya:Keyword").and()),
+    Tuple.of(Key.FnCall.key(), Style.color("aya:FnCall").and()),
+    Tuple.of(Key.DataCall.key(), Style.color("aya:DataCall").and()),
+    Tuple.of(Key.StructCall.key(), Style.color("aya:StructCall").and()),
+    Tuple.of(Key.ConCall.key(), Style.color("aya:ConCall").and()),
+    Tuple.of(Key.FieldCall.key(), Style.color("aya:FieldCall").and()),
+    Tuple.of(Key.Generalized.key(), Style.color("aya:Generalized").and())
   ));
 
   /** use colors from terminal instead of absolute colors to protect eyes */
   public static final @NotNull StyleFamily ADAPTIVE_CLI = new AyaStyleFamily(MutableMap.ofEntries(
-    Tuple.of("aya:Keyword", Style.color("aya:Keyword").and()),
-    Tuple.of("aya:PrimCall", Style.color("aya:Keyword").and()),
-    Tuple.of("aya:FnCall", UnixTermStyle.TerminalYellow.and()),
-    Tuple.of("aya:DataCall", UnixTermStyle.TerminalGreen.and()),
-    Tuple.of("aya:StructCall", UnixTermStyle.TerminalGreen.and()),
-    Tuple.of("aya:ConCall", UnixTermStyle.TerminalBlue.and()),
-    Tuple.of("aya:FieldCall", UnixTermStyle.TerminalBlue.and()),
-    Tuple.of("aya:Generalized", Style.italic().and())
+    Tuple.of(Key.Keyword.key(), Style.color("aya:Keyword").and()),
+    Tuple.of(Key.PrimCall.key(), Style.color("aya:Keyword").and()),
+    Tuple.of(Key.FnCall.key(), UnixTermStyle.TerminalYellow.and()),
+    Tuple.of(Key.DataCall.key(), UnixTermStyle.TerminalGreen.and()),
+    Tuple.of(Key.StructCall.key(), UnixTermStyle.TerminalGreen.and()),
+    Tuple.of(Key.ConCall.key(), UnixTermStyle.TerminalBlue.and()),
+    Tuple.of(Key.FieldCall.key(), UnixTermStyle.TerminalBlue.and()),
+    Tuple.of(Key.Generalized.key(), Style.italic().and())
   ));
 }
