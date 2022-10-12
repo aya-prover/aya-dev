c986cafc0ef133f54dd5ac6fb40e3b1e4dc13d6b
HoshinoTented <limbolrain@gmail.com>
2022-10-13 03:27:24 +0800

    misc: improve doc

--- lsp/src/main/java/org/aya/lsp/models/ServerOptions.java
+++ lsp/src/main/java/org/aya/lsp/models/ServerOptions.java
@@ -4,28 +4,36 @@ package org.aya.lsp.models;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import org.javacs.lsp.InitializeParams;
 import org.jetbrains.annotations.Nullable;
 
 /**
- * The server options for LSP, see {@link org.javacs.lsp.InitializeParams#initializationOptions}
+ * The server options for LSP
  *
  * @see RenderOptions
+ * @see org.javacs.lsp.InitializeParams#initializationOptions
+ * @see org.aya.lsp.server.AyaLanguageServer#updateOptions(ServerOptions)
  */
 public class ServerOptions {
   /**
    * the color scheme for pretty printer, can be either {@link String} or {@link JsonObject}
    * <ul>
    *   <li>String: color scheme name, "emacs" and "intellij" are valid for now, see {@link org.aya.pretty.style.AyaColorScheme}</li>
-   *   <li>JsonObject: a color scheme string-integer map, see {@link org.aya.pretty.style.AyaColorScheme.Key}</li>
+   *   <li>JsonObject: a {@link String}-{@link Integer} map, see {@link org.aya.pretty.style.AyaColorScheme.Key}</li>
    * </ul>
    */
   public @Nullable JsonElement colorScheme;
 
   /**
    * unused for now
+   *
+   * @see RenderOptions#styleFamily()
    */
   public @Nullable JsonElement styleFamily;
 
+  /**
+   * @see RenderOptions#renderTarget()
+   */
   public @Nullable RenderOptions.RenderTarget renderTarget;
 
   public ServerOptions(@Nullable JsonElement colorScheme, @Nullable JsonElement styleFamily, @Nullable RenderOptions.RenderTarget renderTarget) {
