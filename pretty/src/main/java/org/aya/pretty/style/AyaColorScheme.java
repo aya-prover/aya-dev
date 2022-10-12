ad42870e699da6a428fcf2fe0bdb9f0773237a57
HoshinoTented <limbolrain@gmail.com>
2022-10-12 23:50:26 +0800

    misc: enumerable key

--- pretty/src/main/java/org/aya/pretty/style/AyaColorScheme.java
+++ pretty/src/main/java/org/aya/pretty/style/AyaColorScheme.java
@@ -13,23 +13,43 @@ import org.jetbrains.annotations.NotNull;
 public record AyaColorScheme(
   @NotNull MutableMap<String, Integer> definedColors
 ) implements ColorScheme {
+  public enum Key {
+    Keyword("aya:Keyword"),
+    FnCall("aya:FnCall"),
+    Generalized("aya:Generalized"),
+    DataCall("aya:DataCall"),
+    StructCall("aya:StructCall"),
+    ConCall("aya:ConCall"),
+    FieldCall("aya:FieldCall");
+
+    private final String key;
+
+    Key(String key) {
+      this.key = key;
+    }
+
+    public String key() {
+      return key;
+    }
+  }
+
   public static final @NotNull AyaColorScheme EMACS = new AyaColorScheme(MutableMap.ofEntries(
-    Tuple.of("aya:Keyword", ColorScheme.colorOf(1.0f, 0.43f, 0)),
-    Tuple.of("aya:FnCall", ColorScheme.colorOf(0, 0, 1f)),
-    Tuple.of("aya:Generalized", ColorScheme.colorOf(0, 0, 1f)),
-    Tuple.of("aya:DataCall", ColorScheme.colorOf(0.13f, 0.55f, 0.13f)),
-    Tuple.of("aya:StructCall", ColorScheme.colorOf(0.13f, 0.55f, 0.13f)),
-    Tuple.of("aya:ConCall", ColorScheme.colorOf(0.63f, 0.13f, 0.94f)),
-    Tuple.of("aya:FieldCall", ColorScheme.colorOf(0, 0.55f, 0.55f))
+    Tuple.of(Key.Keyword.key(), ColorScheme.colorOf(1.0f, 0.43f, 0)),
+    Tuple.of(Key.FnCall.key(), ColorScheme.colorOf(0, 0, 1f)),
+    Tuple.of(Key.Generalized.key(), ColorScheme.colorOf(0, 0, 1f)),
+    Tuple.of(Key.DataCall.key(), ColorScheme.colorOf(0.13f, 0.55f, 0.13f)),
+    Tuple.of(Key.StructCall.key(), ColorScheme.colorOf(0.13f, 0.55f, 0.13f)),
+    Tuple.of(Key.ConCall.key(), ColorScheme.colorOf(0.63f, 0.13f, 0.94f)),
+    Tuple.of(Key.FieldCall.key(), ColorScheme.colorOf(0, 0.55f, 0.55f))
   ));
 
   public static final @NotNull AyaColorScheme INTELLIJ = new AyaColorScheme(MutableMap.ofEntries(
-    Tuple.of("aya:Keyword", 0x0033B3),
-    Tuple.of("aya:FnCall", 0x00627A),
-    Tuple.of("aya:Generalized", 0x00627A),
-    Tuple.of("aya:DataCall", 0x000000),
-    Tuple.of("aya:StructCall", 0x000000),
-    Tuple.of("aya:ConCall", 0x067D17),
-    Tuple.of("aya:FieldCall", 0x871094)
+    Tuple.of(Key.Keyword.key(), 0x0033B3),
+    Tuple.of(Key.FnCall.key(), 0x00627A),
+    Tuple.of(Key.Generalized.key(), 0x00627A),
+    Tuple.of(Key.DataCall.key(), 0x000000),
+    Tuple.of(Key.StructCall.key(), 0x000000),
+    Tuple.of(Key.ConCall.key(), 0x067D17),
+    Tuple.of(Key.FieldCall.key(), 0x871094)
   ));
 }
