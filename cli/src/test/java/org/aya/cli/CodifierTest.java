53e5b4385b33c3f5fd109a65c7e5845702221b2c
ice1000 <ice1000kotlin@foxmail.com>
2022-11-02 16:22:52 -0400

    test: codification

--- cli/src/test/java/org/aya/cli/CodifierTest.java
+++ cli/src/test/java/org/aya/cli/CodifierTest.java
@@ -4,13 +4,15 @@ package org.aya.cli;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class CodifierTest extends ReplTestBase {
   @Test public void basic() {
     var out = repl("""
       prim I
       def pmap {A B : Type} (p : [| i |] A) (f : A -> B) : [| i |] B => \\i => f (p i)
       :codify pmap
-      """);
-    System.out.println(out);
+      """)._1.trim();
+    assertNotNull(out);
   }
 }
