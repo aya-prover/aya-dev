4e21e15f4906b6530353f7a8e063a196eebf5622
ice1000 <ice1000kotlin@foxmail.com>
2022-11-15 16:16:55 -0500

    cond: fix compile, unsure if the impl is right or not

--- base/src/main/java/org/aya/tyck/pat/Conquer.java
+++ base/src/main/java/org/aya/tyck/pat/Conquer.java
@@ -6,17 +6,18 @@ import kala.collection.immutable.ImmutableSeq;
 import kala.tuple.Tuple;
 import org.aya.core.def.Def;
 import org.aya.core.pat.Pat;
-import org.aya.core.pat.PatMatcher;
 import org.aya.core.pat.PatToTerm;
 import org.aya.core.term.ErrorTerm;
 import org.aya.core.term.MetaTerm;
 import org.aya.core.term.Term;
+import org.aya.core.visitor.AyaRestrSimplifier;
 import org.aya.core.visitor.Expander;
 import org.aya.core.visitor.Subst;
+import org.aya.guest0x0.cubical.CofThy;
+import org.aya.guest0x0.cubical.Partial;
+import org.aya.guest0x0.cubical.Restr;
 import org.aya.util.Arg;
-import org.aya.generic.util.NormalizeMode;
 import org.aya.tyck.ExprTycker;
-import org.aya.tyck.env.LocalCtx;
 import org.aya.tyck.env.MapLocalCtx;
 import org.aya.util.Ordering;
 import org.aya.util.error.SourcePos;
@@ -50,18 +51,19 @@ public record Conquer(
       case Pat.Ctor ctor -> {
         var params = ctor.params();
         for (var sub : params) visit(sub, nth);
-        var faces = ctor.ref().core.clauses.clauses();
+        var ctorDef = ctor.ref().core;
+        assert ctorDef.selfTele.sizeEquals(params);
+        // Con tele |-> pattern tele
+        var subst = new Subst(
+          ctorDef.selfTele.view().map(Term.Param::ref),
+          params.view().map(Pat::toTerm)
+        );
+        // The split, but typed under current context
+        var clauses = (Partial.Split<Term>) AyaRestrSimplifier.INSTANCE
+          .mapSplit(ctorDef.clauses, t -> t.subst(subst));
+        var faces = clauses.clauses();
         for (int i = 0, size = faces.size(); i < size; i++) {
-          var condition = faces.get(i);
-          var matchy = PatMatcher.tryBuildSubstTerms(params,
-            condition.patterns().view().map(Pat::toTerm),
-            t -> t.normalize(tycker.state, NormalizeMode.WHNF));
-          if (matchy.isOk()) {
-            var ctx = new MapLocalCtx();
-            condition.patterns().forEach(tern -> tern.storeBindings(ctx));
-            // Do not need `ctor.storeBindings(ctx)` since they're substituted out by matchy
-            checkConditions(ctx, ctor, nth, i + 1, condition.body(), matchy.get(), condition.sourcePos());
-          }
+          checkConditions(nth, i + 1, faces.get(i), subst);
         }
       }
       case Pat.Tuple tuple -> {
@@ -71,38 +73,31 @@ public record Conquer(
     }
   }
 
-  private void checkConditions(
-    LocalCtx ctx, Pat ctor, int nth, int i,
-    Term condition, Subst matchy, SourcePos conditionPos
-  ) {
+  private void checkConditions(int nth, int i, Restr.Side<Term> condition, Subst matchy) {
+    var ctx = new MapLocalCtx();
     var currentClause = matchings.get(nth);
     var newBody = currentClause.body().subst(matchy);
-    var newArgs = currentClause.patterns().map(pat -> new Arg<>(new PatToTerm() {
-      @Override public @NotNull Term visitCtor(Pat.@NotNull Ctor newCtor) {
-        return newCtor == ctor ? condition : super.visitCtor(newCtor);
-      }
-
-      @Override public Term visit(@NotNull Pat pat) {
-        if (pat instanceof Pat.Bind bind) ctx.put(bind.bind(), bind.type());
-        return super.visit(pat);
-      }
-    }.visit(pat), pat.explicit()));
-    var volynskaya = new Expander.WHNFer(tycker.state).tryUnfoldClauses(orderIndependent, newArgs, 0, matchings).getOrNull();
-    if (volynskaya == null) {
+    currentClause.patterns().forEach(p -> p.storeBindings(ctx));
+    // They're pre-cof
+    var anotherClause = CofThy.vdash(condition.cof(), matchy, subst ->
+      new Expander.WHNFer(tycker.state).tryUnfoldClauses(orderIndependent,
+        currentClause.patterns().map(p -> p.toArg().descent(t -> t.subst(subst))),
+        0, matchings).getOrNull()).getOrNull();
+    if (anotherClause == null) {
       tycker.reporter.report(new ClausesProblem.Conditions(
-        sourcePos, nth + 1, i, newBody, null, conditionPos, currentClause.sourcePos(), null));
+        sourcePos, nth + 1, i, newBody, null, currentClause.sourcePos(), null));
       return;
     }
     if (newBody instanceof ErrorTerm error && error.description() instanceof MetaTerm hole) {
-      hole.ref().conditions.append(Tuple.of(matchy, volynskaya.data()));
-    } else if (volynskaya.data() instanceof ErrorTerm error && error.description() instanceof MetaTerm hole) {
+      hole.ref().conditions.append(Tuple.of(matchy, anotherClause.data()));
+    } else if (anotherClause.data() instanceof ErrorTerm error && error.description() instanceof MetaTerm hole) {
       hole.ref().conditions.append(Tuple.of(matchy, newBody));
     }
     var unification = tycker.unifier(sourcePos, Ordering.Eq, ctx)
-      .compare(newBody, volynskaya.data(), signature.result().subst(matchy));
+      .compare(newBody, anotherClause.data(), signature.result().subst(matchy));
     if (!unification) {
       tycker.reporter.report(new ClausesProblem.Conditions(
-        sourcePos, nth + 1, i, newBody, volynskaya.data(), conditionPos, currentClause.sourcePos(), volynskaya.sourcePos()));
+        sourcePos, nth + 1, i, newBody, anotherClause.data(), currentClause.sourcePos(), anotherClause.sourcePos()));
     }
   }
 }
