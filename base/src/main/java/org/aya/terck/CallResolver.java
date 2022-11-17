6cadbb45a692b4bc4aca859c99d5fc6aac69098b
imkiva <imkiva@islovely.icu>
2022-11-17 17:57:35 +0800

    terck: reduce arguments after a no-result comparison

--- base/src/main/java/org/aya/terck/CallResolver.java
+++ base/src/main/java/org/aya/terck/CallResolver.java
@@ -2,14 +2,19 @@
 // Use of this source code is governed by the MIT license that can be found in the LICENSE.md file.
 package org.aya.terck;
 
+import kala.collection.immutable.ImmutableSeq;
 import kala.collection.mutable.MutableSet;
 import kala.value.MutableValue;
 import org.aya.core.def.Def;
 import org.aya.core.def.FnDef;
+import org.aya.core.def.PrimDef;
 import org.aya.core.pat.Pat;
 import org.aya.core.term.*;
 import org.aya.core.visitor.DefConsumer;
+import org.aya.generic.util.NormalizeMode;
 import org.aya.ref.DefVar;
+import org.aya.tyck.TyckState;
+import org.aya.util.Arg;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -20,20 +25,24 @@ import org.jetbrains.annotations.NotNull;
  * @author kiva
  */
 public record CallResolver(
+  @NotNull PrimDef.Factory factory,
   @NotNull FnDef caller,
   @NotNull MutableSet<Def> targets,
   @NotNull MutableValue<Term.Matching> currentMatching,
   @NotNull CallGraph<Def, Term.Param> graph
 ) implements DefConsumer {
-  public CallResolver(@NotNull FnDef fn, @NotNull MutableSet<Def> targets, @NotNull CallGraph<Def, Term.Param> graph) {
-    this(fn, targets, MutableValue.create(), graph);
+  public CallResolver(@NotNull PrimDef.Factory factory, @NotNull FnDef fn, @NotNull MutableSet<Def> targets, @NotNull CallGraph<Def, Term.Param> graph) {
+    this(factory, fn, targets, MutableValue.create(), graph);
+  }
+
+  private @NotNull Term whnf(@NotNull Term term) {
+    return term.normalize(new TyckState(factory), NormalizeMode.WHNF);
   }
 
   private void resolveCall(@NotNull Callable callable) {
     if (!(callable.ref() instanceof DefVar<?, ?> defVar)) return;
     var callee = ((Def) defVar.core);
     if (!targets.contains(callee)) return;
-    // TODO: reduce arguments? I guess no. see https://github.com/agda/agda/issues/2403
     var matrix = new CallMatrix<>(callable, caller, callee, caller.telescope, callee.telescope());
     fillMatrix(callable, callee, matrix);
     graph.put(matrix);
@@ -56,15 +65,22 @@ public record CallResolver(
       case Pat.Ctor ctor -> switch (term) {
         case ConCall con -> {
           if (con.ref() != ctor.ref() || !con.conArgs().sizeEquals(ctor.params())) yield Relation.unk();
-          var subCompare = con.conArgs().zipView(ctor.params())
-            .map(sub -> compare(sub._1.term(), sub._2));
-          yield subCompare.foldLeft(Relation.eq(), Relation::mul);
+          var attempt = compareConArgs(con.conArgs(), ctor);
+          // Reduce arguments and compare again. This may cause performance issues (but not observed yet [2022-11-07]),
+          // see https://github.com/agda/agda/issues/2403 for more information.
+          if (attempt == Relation.unk()) attempt = compareConArgs(con.conArgs().map(a -> a.descent(this::whnf)), ctor);
+          yield attempt;
         }
         // TODO[literal]: We may convert constructor call to literals to avoid possible stack overflow?
         case IntegerTerm lit -> compare(lit.constructorForm(), ctor);
         default -> {
           var subCompare = ctor.params().view().map(sub -> compare(term, sub));
-          yield subCompare.anyMatch(r -> r != Relation.unk()) ? Relation.lt() : Relation.unk();
+          var attempt = subCompare.anyMatch(r -> r != Relation.unk()) ? Relation.lt() : Relation.unk();
+          if (attempt == Relation.unk()) {
+            var reduce = whnf(term); // `term` may be reduced to a constructor call
+            if (reduce != term) attempt = compare(reduce, ctor);
+          }
+          yield attempt;
         }
       };
       case Pat.Bind bind -> {
@@ -86,6 +102,11 @@ public record CallResolver(
       };
       default -> Relation.unk();
     };
+  }
+
+  private Relation compareConArgs(@NotNull ImmutableSeq<Arg<Term>> conArgs, @NotNull Pat.Ctor ctor) {
+    var subCompare = conArgs.zipView(ctor.params()).map(sub -> compare(sub._1.term(), sub._2));
+    return subCompare.foldLeft(Relation.eq(), Relation::mul);
   }
 
   /** @return the head of application or projection */
