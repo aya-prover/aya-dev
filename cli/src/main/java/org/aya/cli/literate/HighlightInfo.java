7cbb0ebceb6b69ba86c8610a6013c5ad3b01ace9
HoshinoTented <hoshinotented@qq.com>
2022-11-19 00:42:13 +0800

    literate: use `TextRange`

--- cli/src/main/java/org/aya/cli/literate/HighlightInfo.java
+++ cli/src/main/java/org/aya/cli/literate/HighlightInfo.java
@@ -2,15 +2,20 @@
 // Use of this source code is governed by the MIT license that can be found in the LICENSE.md file.
 package org.aya.cli.literate;
 
+import com.intellij.openapi.util.TextRange;
 import org.aya.util.error.SourcePos;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Comparator;
 
 public record HighlightInfo(
-  @NotNull SourcePos sourcePos,
+  @NotNull TextRange sourcePos,
   @NotNull HighlightInfoType type
-  ) {
+) {
+  public HighlightInfo(@NotNull SourcePos sourcePos, @NotNull HighlightInfoType type) {
+    this(new TextRange(sourcePos.tokenStartIndex(), sourcePos.tokenEndIndex() + 1), type);
+  }
+
   public static class Ord implements Comparator<HighlightInfo> {
     public final static Ord INSTANCE = new Ord();
 
@@ -23,7 +28,10 @@ public record HighlightInfo(
       assert lhs != null;
       assert rhs != null;
 
-      return lhs.sourcePos().compareTo(rhs.sourcePos());
+      return Integer.compare(
+        lhs.sourcePos().getStartOffset(),
+        rhs.sourcePos().getStartOffset()
+      );
     }
   }
 }
