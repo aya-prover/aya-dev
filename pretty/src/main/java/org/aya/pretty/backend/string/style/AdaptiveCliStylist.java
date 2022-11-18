56d0cac213406dbf4069a609904bcfe6a43adae8
imkiva <imkiva@islovely.icu>
2022-11-18 22:28:59 +0800

    misc: inline to avoid static fields initialization order

--- pretty/src/main/java/org/aya/pretty/backend/string/style/AdaptiveCliStylist.java
+++ pretty/src/main/java/org/aya/pretty/backend/string/style/AdaptiveCliStylist.java
@@ -7,7 +7,6 @@ import kala.tuple.Tuple;
 import org.aya.pretty.backend.string.custom.UnixTermStyle;
 import org.aya.pretty.doc.Style;
 import org.aya.pretty.printer.ColorScheme;
-import org.aya.pretty.printer.StyleFamily;
 import org.aya.pretty.style.AyaStyleFamily;
 import org.jetbrains.annotations.NotNull;
 
@@ -15,18 +14,16 @@ import org.jetbrains.annotations.NotNull;
 public class AdaptiveCliStylist extends UnixTermStylist {
   public static final @NotNull AdaptiveCliStylist INSTANCE = new AdaptiveCliStylist();
 
-  public AdaptiveCliStylist() {
-    super(MutableMap::create, ADAPTIVE_CLI);
+  private AdaptiveCliStylist() {
+    super(MutableMap::create, new AyaStyleFamily(MutableMap.ofEntries(
+      Tuple.of(AyaStyleFamily.Key.Keyword.key(), Style.color(ColorScheme.colorOf(1.0f, 0.43f, 0)).and()),
+      Tuple.of(AyaStyleFamily.Key.PrimCall.key(), Style.color(ColorScheme.colorOf(1.0f, 0.43f, 0)).and()),
+      Tuple.of(AyaStyleFamily.Key.FnCall.key(), UnixTermStyle.TerminalYellow.and()),
+      Tuple.of(AyaStyleFamily.Key.DataCall.key(), UnixTermStyle.TerminalGreen.and()),
+      Tuple.of(AyaStyleFamily.Key.StructCall.key(), UnixTermStyle.TerminalGreen.and()),
+      Tuple.of(AyaStyleFamily.Key.ConCall.key(), UnixTermStyle.TerminalBlue.and()),
+      Tuple.of(AyaStyleFamily.Key.FieldCall.key(), UnixTermStyle.TerminalBlue.and()),
+      Tuple.of(AyaStyleFamily.Key.Generalized.key(), Style.italic().and())
+    )));
   }
-
-  private static final @NotNull StyleFamily ADAPTIVE_CLI = new AyaStyleFamily(MutableMap.ofEntries(
-    Tuple.of(AyaStyleFamily.Key.Keyword.key(), Style.color(ColorScheme.colorOf(1.0f, 0.43f, 0)).and()),
-    Tuple.of(AyaStyleFamily.Key.PrimCall.key(), Style.color(ColorScheme.colorOf(1.0f, 0.43f, 0)).and()),
-    Tuple.of(AyaStyleFamily.Key.FnCall.key(), UnixTermStyle.TerminalYellow.and()),
-    Tuple.of(AyaStyleFamily.Key.DataCall.key(), UnixTermStyle.TerminalGreen.and()),
-    Tuple.of(AyaStyleFamily.Key.StructCall.key(), UnixTermStyle.TerminalGreen.and()),
-    Tuple.of(AyaStyleFamily.Key.ConCall.key(), UnixTermStyle.TerminalBlue.and()),
-    Tuple.of(AyaStyleFamily.Key.FieldCall.key(), UnixTermStyle.TerminalBlue.and()),
-    Tuple.of(AyaStyleFamily.Key.Generalized.key(), Style.italic().and())
-  ));
 }
