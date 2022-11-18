4e98064431d64d74cbe0fd354d12463e5c1bfd7d
HoshinoTented <hoshinotented@qq.com>
2022-11-19 02:54:55 +0800

    literate: util

---
+++ cli/src/main/java/org/aya/cli/literate/utils/HighlighterUtils.java
@@ -0,0 +1,38 @@
+// Copyright (c) 2020-2022 Tesla (Yinsen) Zhang.
+// Use of this source code is governed by the MIT license that can be found in the LICENSE.md file.
+package org.aya.cli.literate.utils;
+
+import com.intellij.lexer.FlexLexer;
+import kala.collection.Seq;
+import kala.collection.immutable.ImmutableSeq;
+import org.aya.cli.literate.HighlightInfo;
+import org.aya.cli.literate.HighlightInfoHolder;
+import org.aya.cli.literate.HighlightInfoType;
+import org.aya.cli.literate.Highlighter;
+import org.aya.concrete.stmt.Stmt;
+import org.aya.parser.AyaParserDefinitionBase;
+import org.aya.util.distill.DistillerOptions;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+public final class HighlighterUtils {
+  private HighlighterUtils() {
+  }
+
+  public static @NotNull HighlightInfoHolder highlight(@NotNull ImmutableSeq<Stmt> program, @NotNull DistillerOptions options) {
+    var distiller = new Highlighter(options);
+    program.forEach(distiller);
+
+    return distiller.result();
+  }
+
+  @Contract(value = "_, _ -> param1", mutates = "param1")
+  public static @NotNull HighlightInfoHolder highlightKeywords(@NotNull HighlightInfoHolder base, @NotNull Seq<FlexLexer.Token> tokens) {
+    var keywords = tokens.view()
+      .filter(x -> AyaParserDefinitionBase.KEYWORDS.contains(x.type()))
+      .map(token -> new HighlightInfo(token.range(), HighlightInfoType.Keyword.INSTANCE));
+
+    keywords.forEach(base::addInfo);
+    return base;
+  }
+}
