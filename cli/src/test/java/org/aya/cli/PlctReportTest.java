9a93127f1e6aba5eddd7644bd042101dd2933ea3
ice1000 <ice1000kotlin@foxmail.com>
2022-11-19 16:53:13 -0500

    test: add test

---
+++ cli/src/test/java/org/aya/cli/PlctReportTest.java
@@ -0,0 +1,15 @@
+// Copyright (c) 2020-2022 Tesla (Yinsen) Zhang.
+// Use of this source code is governed by the MIT license that can be found in the LICENSE.md file.
+package org.aya.cli;
+
+import org.aya.cli.plct.PLCTReport;
+import org.aya.cli.utils.MainArgs;
+import org.junit.jupiter.api.Test;
+
+public class PlctReportTest {
+  @Test public void generateReport() throws Exception {
+    var args = new MainArgs.PlctAction();
+    args.plctReport = true;
+    System.out.println(new PLCTReport().run(args));
+  }
+}
