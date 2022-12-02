1892e52238a8955ebe776dcd4ff34e544fa0dabc
ice1000 <ice1000kotlin@foxmail.com>
2022-12-01 23:52:51 -0500

    style: introduce `MdStylist`

---
+++ pretty/src/main/java/org/aya/pretty/backend/md/MdStylist.java
@@ -0,0 +1,34 @@
+// Copyright (c) 2020-2022 Tesla (Yinsen) Zhang.
+// Use of this source code is governed by the MIT license that can be found in the LICENSE.md file.
+package org.aya.pretty.backend.md;
+
+import org.aya.pretty.backend.html.Html5Stylist;
+import org.aya.pretty.printer.ColorScheme;
+import org.aya.pretty.printer.StyleFamily;
+import org.aya.pretty.style.AyaColorScheme;
+import org.aya.pretty.style.AyaStyleFamily;
+import org.jetbrains.annotations.NotNull;
+
+public class MdStylist extends Html5Stylist {
+  public static final @NotNull Html5Stylist DEFAULT = new Html5Stylist(AyaColorScheme.EMACS, AyaStyleFamily.DEFAULT);
+
+  public MdStylist(@NotNull ColorScheme colorScheme, @NotNull StyleFamily styleFamily) {
+    super(colorScheme, styleFamily);
+  }
+
+  @Override protected @NotNull StyleToken formatItalic() {
+    return new StyleToken("_", "_", false);
+  }
+
+  @Override protected @NotNull StyleToken formatBold() {
+    return new StyleToken("**", "**", false);
+  }
+
+  @Override protected @NotNull StyleToken formatStrike() {
+    return new StyleToken("~~", "~~", false);
+  }
+
+  @Override protected @NotNull StyleToken formatCode() {
+    return new StyleToken("```", "```", false);
+  }
+}
