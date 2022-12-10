4962058cf8ddea1d9f39aac538d9077159ae3b77
ice1000 <ice1000kotlin@foxmail.com>
2022-12-09 20:34:22 -0500

    comments: more

--- base/src/main/java/org/aya/core/term/PathTerm.java
+++ base/src/main/java/org/aya/core/term/PathTerm.java
@@ -29,19 +29,26 @@ public record PathTerm(
     return new PLamTerm(params, applyDimsTo(term)).rename();
   }
 
+  /**
+   * Don't use it in type checking, see the remark by Carlo Angiuli:
+   * <a href="https://discord.com/channels/767397347218423858/767397347218423861/1050938238581346344">link</a>
+   *
+   * @return this if {@link #type} is not {@link PathTerm}
+   */
   public @NotNull PathTerm flatten() {
     var ty = type;
-    var pa = MutableList.from(params);
-    var par = MutableList.<Partial<Term>>create();
-    if (ty instanceof PathTerm path) {
-      // ^ This means the faces in par are of type `path`
-      pa.appendAll(path.params());
+    var jon = MutableList.from(params);
+    var sterling = MutableList.<Partial<Term>>create();
+    while (ty instanceof PathTerm path) {
+      // ^ This means the faces in sterling are of type `path`
       ty = path.type();
-      // Apply the dims to the terms in par, so their types become ty
-      par.replaceAll(p -> p.map(path::applyDimsTo));
-      par.append(path.partial);
+      jon.appendAll(path.params());
+      // Apply the dims to the terms in sterling, so their types become ty
+      sterling.replaceAll(p -> p.map(path::applyDimsTo));
+      sterling.append(path.partial);
     }
-    return new PathTerm(pa.toImmutableSeq(), ty, PartialTerm.merge(par));
+    if (ty == type) return this;
+    return new PathTerm(jon.toImmutableSeq(), ty, PartialTerm.merge(sterling));
   }
 
   public @NotNull PiTerm computePi() {
