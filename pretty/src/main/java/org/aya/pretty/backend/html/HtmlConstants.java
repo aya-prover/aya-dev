c17d77068a332183ae03118b707d9d1d0ad5f03c
imkiva <imkiva@islovely.icu>
2022-12-14 14:19:06 +0800

    doc: remove hardcoded link color

--- pretty/src/main/java/org/aya/pretty/backend/html/HtmlConstants.java
+++ pretty/src/main/java/org/aya/pretty/backend/html/HtmlConstants.java
@@ -41,7 +41,7 @@ public interface HtmlConstants {
   @Language(value = "HTML")
   @NotNull String HOVER_STYLE = """
     <style>
-    .Aya a { text-decoration: none; color: black; }
+    .Aya a { text-decoration: none; }
     .Aya a[href]:hover { background-color: #B4EEB4; }
     .Aya [href].hover-highlight { background-color: #B4EEB4; }
     </style>
