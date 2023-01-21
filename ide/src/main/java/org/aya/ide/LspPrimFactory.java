1b877e5823918f273b657209c9a8ae74e7d9f925
ice1000 <ice1000kotlin@foxmail.com>
2023-01-21 00:39:09 -0500

    refactor: move prim factory

--- ide/src/main/java/org/aya/ide/LspPrimFactory.java
+++ ide/src/main/java/org/aya/ide/LspPrimFactory.java
@@ -1,6 +1,6 @@
 // Copyright (c) 2020-2023 Tesla (Yinsen) Zhang.
 // Use of this source code is governed by the MIT license that can be found in the LICENSE.md file.
-package org.aya.lsp.prim;
+package org.aya.ide;
 
 import org.aya.concrete.stmt.decl.TeleDecl;
 import org.aya.core.def.PrimDef;
