3cff01afc1ff5fb2418c97fdc50dc406bf680ce7
ice1000 <ice1000kotlin@foxmail.com>
2023-01-21 15:01:11 -0500

    misc: test exprs in repl

--- cli-impl/src/test/java/org/aya/cli/ReplCompilerTest.java
+++ cli-impl/src/test/java/org/aya/cli/ReplCompilerTest.java
@@ -26,10 +26,7 @@ public class ReplCompilerTest {
   public final @NotNull ReplCompiler compiler = new ReplCompiler(ImmutableSeq.empty(), new ThrowingReporter(AyaPrettierOptions.debug()), null);
 
   @BeforeEach public void setup() {
-    var ctx = compiler.getContext();
-    ctx.modules.clear();
-    ctx.exports.clear();
-    ctx.definitions.clear();
+    compiler.getContext().clear();
   }
 
   @Test public void library() throws IOException {
@@ -37,6 +34,10 @@ public class ReplCompilerTest {
     assertNotNull(findContext("Nat::zero"));
     assertNotNull(findContext("Vec::vnil"));
     assertNotNull(findContext("Vec:::>"));
+  }
+
+  @Test public void simpleExpr() {
+    compile("Prop");
   }
 
   @Test public void issue382() {
