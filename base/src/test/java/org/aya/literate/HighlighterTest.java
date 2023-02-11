1cdf52590f2d3e591c596dfe900de239e9f08d8f
imkiva <imkiva@islovely.icu>
2023-02-11 18:54:25 +0800

    test: fix tests

--- base/src/test/java/org/aya/literate/HighlighterTest.java
+++ base/src/test/java/org/aya/literate/HighlighterTest.java
@@ -1,4 +1,4 @@
-// Copyright (c) 2020-2022 Tesla (Yinsen) Zhang.
+// Copyright (c) 2020-2023 Tesla (Yinsen) Zhang.
 // Use of this source code is governed by the MIT license that can be found in the LICENSE.md file.
 package org.aya.literate;
 
@@ -25,21 +25,28 @@ public class HighlighterTest {
       keyword(0, 3, "open"),
       whatever(),
       def(10, 12, "Nat", "nat", HighlightInfo.DefKind.Data),
+      whatever(), // |
       def(16, 16, "O", "zero", HighlightInfo.DefKind.Con),
+      whatever(), // |
       def(20, 20, "S", "suc", HighlightInfo.DefKind.Con),
       ref(22, 24, "Nat", "nat", HighlightInfo.DefKind.Data),
       whatever(),   // def
       def(31, 33, "add", HighlightInfo.DefKind.Fn),
       ref(35, 37, "Nat", "nat", HighlightInfo.DefKind.Data),
       ref(39, 41, "Nat", "nat", HighlightInfo.DefKind.Data),
+      whatever(), // :
       ref(45, 47, "Nat", "nat", HighlightInfo.DefKind.Data),
+      whatever(), // |
       localDef(51, 51, "n", "n'"),
       litInt(54, 54, 0),
+      whatever(), // =>
       localRef(59, 59, "n", "n'"),
+      whatever(), // |
       ref(63, 63, "S", "suc", HighlightInfo.DefKind.Con),
       // Note: n' is still available here, so use another name
       localDef(65, 65, "n", "n''"),
       localDef(68, 68, "m", "m'"),
+      whatever(), // =>
       ref(73, 73, "S", "suc", HighlightInfo.DefKind.Con),
       ref(76, 78, "add", HighlightInfo.DefKind.Fn),
       localRef(80, 80, "n", "n''"),
@@ -58,7 +65,9 @@ public class HighlighterTest {
       keyword(3, 6, "open"),
       keyword(8, 11, "data"),
       def(14, 16, "Nat", "nat", HighlightInfo.DefKind.Data),
+      whatever(),
       def(20, 20, "O", HighlightInfo.DefKind.Con),
+      whatever(),
       def(24, 24, "S", HighlightInfo.DefKind.Con),
       ref(26, 28, "Nat", "nat", HighlightInfo.DefKind.Data));
   }
@@ -110,20 +119,27 @@ public class HighlighterTest {
       def(10, 15, "Either", "DefEither", HighlightInfo.DefKind.Data),
       localDef(18, 18, "A", "LocalA"),
       localDef(20, 20, "B", "LocalB"),
+      whatever(), // :
       keyword(24, 27, "Type"),
+      whatever(), // |
       def(32, 35, "Left", HighlightInfo.DefKind.Con),
       localRef(37, 37, "A", "LocalA"),
+      whatever(), // |
       def(41, 45, "Right", HighlightInfo.DefKind.Con),
       localRef(47, 47, "B", "LocalB"),
 
       keyword(50, 52, "def"),
       def(54, 59, "constA", HighlightInfo.DefKind.Fn),
       localDef(62, 62, "A", "LocalA'"),
+      whatever(), // :
       keyword(66, 69, "Type"),
       localDef(73, 73, "a", "Locala"),
       localDef(75, 75, "b"),
+      whatever(), // :
       localRef(79, 79, "A", "LocalA'"),
+      whatever(), // :
       localRef(84, 84, "A", "LocalA'"),
+      whatever(), // =>
       localRef(89, 89, "a", "Locala"));
   }
 
@@ -137,12 +153,16 @@ public class HighlighterTest {
     highlightAndTest(code,
       keyword(0, 7, "variable"),
       def(9, 9, "A", "GA", HighlightInfo.DefKind.Generalized),
+      whatever(), // :
       keyword(13, 16, "Type"),
       keyword(19, 21, "def"),
       def(23, 24, "id", HighlightInfo.DefKind.Fn),
       localDef(27, 27, "a", "a"),
+      whatever(), // :
       ref(31, 31, "A", "GA", HighlightInfo.DefKind.Generalized),
+      whatever(), // :
       ref(36, 36, "A", "GA", HighlightInfo.DefKind.Generalized),
+      whatever(), // =>
       localRef(41, 41, "a", "a"));
   }
 }
