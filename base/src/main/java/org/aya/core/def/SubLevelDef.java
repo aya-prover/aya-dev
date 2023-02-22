79ba245821099c236645df2f3807ec973fc7308b
ice1000 <ice1000kotlin@foxmail.com>
2023-02-22 00:05:06 -0500

    misc: fix more compile

--- base/src/main/java/org/aya/core/def/SubLevelDef.java
+++ base/src/main/java/org/aya/core/def/SubLevelDef.java
@@ -8,11 +8,11 @@ import org.aya.core.term.Term;
 import org.jetbrains.annotations.NotNull;
 
 /**
- * Fields or constructors, in contrast to {@link TopLevelDef}.
+ * Coconstructors or constructors, in contrast to {@link TopLevelDef}.
  *
  * @author ice1000
  */
-public sealed abstract class SubLevelDef implements Def permits CtorDef, MemberDef {
+public sealed abstract class SubLevelDef implements Def permits CtorDef {
   public final @NotNull ImmutableSeq<Term.Param> ownerTele;
   public final @NotNull ImmutableSeq<Term.Param> selfTele;
   public final @NotNull Term result;
--- base/src/main/java/org/aya/tyck/order/SigRefFinder.java
+++ base/src/main/java/org/aya/tyck/order/SigRefFinder.java
@@ -27,7 +27,7 @@ public record SigRefFinder(@NotNull MutableList<TyckUnit> references) implements
         if (decl instanceof TeleDecl.DataCtor ctor) accept(ctor.clauses);
         // constructor and field should always depend on their data/struct header.
         if (decl instanceof TeleDecl.DataCtor ctor) references.append(ctor.dataRef.concrete);
-        if (decl instanceof TeleDecl.StructField field) references.append(field.structRef.concrete);
+        if (decl instanceof TeleDecl.ClassMember field) references.append(field.classDef.concrete);
       }
       case Command.Module module -> module.contents().forEach(this::accept);
       case Command cmd -> {}
