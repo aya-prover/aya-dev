4a271d41b7fef44415604930bd66500c90797b0a
ice1000 <ice1000kotlin@foxmail.com>
2023-02-22 17:15:37 -0500

    tex: `{` and `}` are now special symbol

--- pretty/src/main/java/org/aya/pretty/backend/latex/DocTeXPrinter.java
+++ pretty/src/main/java/org/aya/pretty/backend/latex/DocTeXPrinter.java
@@ -101,9 +101,9 @@ public class DocTeXPrinter extends StringPrinter<DocTeXPrinter.Config> {
   @Override protected void renderSpecialSymbol(@NotNull Cursor cursor, @NotNull String text, EnumSet<Outer> outer) {
     for (var k : commandMapping.keysView()) {
       if (text.equals(k)) {
-        if (!config.KaTeX()) cursor.invisibleContent("$");
+        if (!config.katex()) cursor.invisibleContent("$");
         cursor.visibleContent(commandMapping.get(k));
-        if (!config.KaTeX()) cursor.invisibleContent("$");
+        if (!config.katex()) cursor.invisibleContent("$");
         return;
       }
     }
@@ -113,7 +113,7 @@ public class DocTeXPrinter extends StringPrinter<DocTeXPrinter.Config> {
 
   @Override public @NotNull String makeIndent(int indent) {
     if (indent == 0) return "";
-    return (config.getStylist().KaTeX ? "\\hspace{" : "\\hspace*{") + indent * 0.5 + "em}";
+    return "\\hspace{" + indent * 0.5 + "em}";
   }
 
   @Override protected void renderHardLineBreak(@NotNull Cursor cursor, EnumSet<Outer> outer) {
@@ -146,12 +146,12 @@ public class DocTeXPrinter extends StringPrinter<DocTeXPrinter.Config> {
       this(TeXStylist.DEFAULT);
     }
 
-    public boolean KaTeX() {
-      return getStylist().KaTeX;
+    public boolean katex() {
+      return getStylist().isKaTeX;
     }
 
     public boolean separateStyle() {
-      return opt(SeparateStyle, false) && !KaTeX();
+      return opt(SeparateStyle, false) && !katex();
     }
 
     public Config(@NotNull TeXStylist stylist) {
