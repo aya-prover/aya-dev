44beba9c6ce9d8ce7aabd0fe03760ab9eae68a45
ice1000 <ice1000kotlin@foxmail.com>
2023-03-01 21:38:15 -0500

    build: fix jflex build

--- buildSrc/src/main/groovy/org/aya/gradle/JFlexTask.groovy
+++ buildSrc/src/main/groovy/org/aya/gradle/JFlexTask.groovy
@@ -1,9 +1,10 @@
-// Copyright (c) 2020-2022 Tesla (Yinsen) Zhang.
+// Copyright (c) 2020-2023 Tesla (Yinsen) Zhang.
 // Use of this source code is governed by the MIT license that can be found in the LICENSE.md file.
 package org.aya.gradle
 
 import jflex.core.OptionUtils
 import jflex.generator.LexGenerator
+import jflex.l10n.ErrorMessages
 import jflex.option.Options
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.InputFile
@@ -24,7 +25,8 @@ class JFlexTask extends DefaultTask implements Runnable {
     OptionUtils.setDir(outputDir)
     Options.no_minimize = false
     Options.no_backup = true
-    Options.unused_warning = true
+    Options.enable ErrorMessages.MACRO_UNUSED
+    Options.enable ErrorMessages.EMPTY_MATCH
     Options.progress = false
     new LexGenerator(jflex).generate()
   }
