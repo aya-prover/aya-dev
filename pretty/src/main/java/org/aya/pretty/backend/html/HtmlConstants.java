e73f0eb1c2a173babd108b4c765d3b57144483be
ice1000 <ice1000kotlin@foxmail.com>
2023-03-31 18:14:04 -0400

    res: fix path

--- pretty/src/main/java/org/aya/pretty/backend/html/HtmlConstants.java
+++ pretty/src/main/java/org/aya/pretty/backend/html/HtmlConstants.java
@@ -15,8 +15,8 @@ public final class HtmlConstants {
   public static final @NotNull String HOVER_STYLE;
 
   static {
-    HOVER_TYPE_POPUP_STYLE = tag("style", readResource("aya-html/hover-type-popup.css"));
-    HOVER_STYLE = tag("style", readResource("aya-html/hover.css"));
+    HOVER_TYPE_POPUP_STYLE = tag("style", readResource("/aya-html/hover-type-popup.css"));
+    HOVER_STYLE = tag("style", readResource("/aya-html/hover.css"));
   }
 
   private static @NotNull String readResource(String name) {
@@ -40,10 +40,10 @@ public final class HtmlConstants {
   public static final @NotNull String HOVER_SHOW_TOOLTIP_JS_INIT;
 
   static {
-    HOVER_ALL_OCCURS_JS_HIGHLIGHT_FN = readResource("aya-html/highlight-fn.js");
-    HOVER_SHOW_TOOLTIP_JS_INIT = readResource("aya-html/show-tooltip.js");
-    HOVER_SHOW_TOOLTIP_JS_SHOW_FN = readResource("aya-html/show-tooltip-show.js");
-    HOVER_ALL_OCCURS_JS_INIT = readResource("aya-html/highlight-occurrences.js");
+    HOVER_ALL_OCCURS_JS_HIGHLIGHT_FN = readResource("/aya-html/highlight-fn.js");
+    HOVER_SHOW_TOOLTIP_JS_INIT = readResource("/aya-html/show-tooltip.js");
+    HOVER_SHOW_TOOLTIP_JS_SHOW_FN = readResource("/aya-html/show-tooltip-show.js");
+    HOVER_ALL_OCCURS_JS_INIT = readResource("/aya-html/highlight-occurrences.js");
   }
 
   @Language(value = "HTML")
@@ -71,8 +71,8 @@ public final class HtmlConstants {
   public static final @NotNull String KATEX_AUTO_RENDER_INIT;
 
   static {
-    KATEX_AUTO_RENDER_EXTERNAL_RESOURCES = readResource("aya-html/katex.html");
-    KATEX_AUTO_RENDER_INIT = readResource("aya-html/katex-auto-render.js");
+    KATEX_AUTO_RENDER_EXTERNAL_RESOURCES = readResource("/aya-html/katex.html");
+    KATEX_AUTO_RENDER_INIT = readResource("/aya-html/katex-auto-render.js");
   }
 
   public static final @NotNull String KATEX_AUTO_RENDER = KATEX_AUTO_RENDER_EXTERNAL_RESOURCES + tag("script", KATEX_AUTO_RENDER_INIT);
