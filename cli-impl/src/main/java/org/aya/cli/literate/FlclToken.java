b5b04254564cc324851efa617236a34471293f7d
ice1000 <ice1000kotlin@foxmail.com>
2023-10-30 10:06:10 -0400

    flcl: to `HighlightInfo`

--- cli-impl/src/main/java/org/aya/cli/literate/FlclToken.java
+++ cli-impl/src/main/java/org/aya/cli/literate/FlclToken.java
@@ -2,6 +2,7 @@
 // Use of this source code is governed by the MIT license that can be found in the LICENSE.md file.
 package org.aya.cli.literate;
 
+import org.aya.pretty.doc.Link;
 import org.aya.util.error.SourcePos;
 import org.jetbrains.annotations.NotNull;
 
@@ -9,7 +10,24 @@ public record FlclToken(
   @NotNull SourcePos range,
   @NotNull Type type
 ) {
+  public static final @NotNull Link EMPTY_LINK = Link.page("");
+
   public enum Type {
-    Keyword, Fn, Data, Number, Local
+    Keyword, Fn, Data, Number, Local, Comment
+  }
+
+  public @NotNull HighlightInfo toInfo() {
+    return switch (type) {
+      case Keyword -> new HighlightInfo.Lit(range, HighlightInfo.LitKind.Keyword);
+      case Number -> new HighlightInfo.Lit(range, HighlightInfo.LitKind.Int);
+      case Comment -> new HighlightInfo.Lit(range, HighlightInfo.LitKind.Comment);
+      case Fn -> createRef(HighlightInfo.DefKind.Fn);
+      case Data -> createRef(HighlightInfo.DefKind.Data);
+      case Local -> createRef(HighlightInfo.DefKind.LocalVar);
+    };
+  }
+
+  private @NotNull HighlightInfo.Ref createRef(HighlightInfo.@NotNull DefKind kind) {
+    return new HighlightInfo.Ref(range, EMPTY_LINK, kind, null);
   }
 }
