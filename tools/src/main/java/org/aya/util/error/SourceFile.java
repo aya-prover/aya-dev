c208a7e4b296ab48d5e54e1c1efe8900dc1b99e7
ice1000 <ice1000kotlin@foxmail.com>
2024-05-28 14:10:16 -0400

    build: fix compile

--- tools/src/main/java/org/aya/util/error/SourceFile.java
+++ tools/src/main/java/org/aya/util/error/SourceFile.java
@@ -1,8 +1,8 @@
-// Copyright (c) 2020-2023 Tesla (Yinsen) Zhang.
+// Copyright (c) 2020-2024 Tesla (Yinsen) Zhang.
 // Use of this source code is governed by the MIT license that can be found in the LICENSE.md file.
 package org.aya.util.error;
 
-import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.openapi.util.text.Strings;
 import kala.control.Option;
 import org.jetbrains.annotations.NotNull;
 
@@ -29,7 +29,7 @@ public record SourceFile(
   }
 
   public SourceFile(@NotNull String display, @NotNull Path underlying, @NotNull String sourceCode) {
-    this(display, Option.some(underlying), StringUtil.convertLineSeparators(sourceCode));
+    this(display, Option.some(underlying), Strings.convertLineSeparators(sourceCode));
   }
 
   public static final SourceFile NONE = new SourceFile("<unknown-file>", Option.none(), "");
