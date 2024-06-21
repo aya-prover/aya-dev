02ab9c197b7ca13920a02478d81c81f903097f9c
ice1000 <ice1000kotlin@foxmail.com>
2024-06-21 09:27:53 -0400

    build: import `jdk.compiler`

--- cli-impl/build.gradle.kts
+++ cli-impl/build.gradle.kts
@@ -1,6 +1,5 @@
 // Copyright (c) 2020-2024 Tesla (Yinsen) Zhang.
 // Use of this source code is governed by the MIT license that can be found in the LICENSE.md file.
-import org.aya.gradle.CommonTasks
 
 dependencies {
   api(project(":base"))
