3e17f5142734f99ed6bc6b00c457b8aaa1a7ff79
ice1000 <ice1000kotlin@foxmail.com>
2024-12-03 16:45:15 -0500

    coe: refactor

--- syntax/src/main/java/org/aya/syntax/core/term/xtt/CoeTerm.java
+++ syntax/src/main/java/org/aya/syntax/core/term/xtt/CoeTerm.java
@@ -9,6 +9,8 @@ import org.aya.syntax.core.term.AppTerm;
 import org.aya.syntax.core.term.Term;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.function.UnaryOperator;
+
 public record CoeTerm(@NotNull Closure type, @NotNull Term r, @NotNull Term s) implements Term {
   public @NotNull CoeTerm update(@NotNull Closure type, @NotNull Term r, @NotNull Term s) {
     return type == type() && r == r() && s == s() ? this : new CoeTerm(type, r, s);
@@ -20,6 +22,7 @@ public record CoeTerm(@NotNull Closure type, @NotNull Term r, @NotNull Term s) i
 
   public @NotNull CoeTerm inverse(Closure newTy) { return new CoeTerm(newTy, s, r); }
   public @NotNull CoeTerm recoe(Closure cover) { return new CoeTerm(cover, r, s); }
+  public @NotNull CoeTerm recoe(UnaryOperator<Term> cover) { return recoe(new Closure.Jit(cover)); }
   public @NotNull Term family() { return PrimDef.familyI2J(type, r, s); }
-  public @NotNull Term app(Term x) { return AppTerm.make(this, x); }
+  public @NotNull Term app(Term x) { return new AppTerm(this, x); }
 }
