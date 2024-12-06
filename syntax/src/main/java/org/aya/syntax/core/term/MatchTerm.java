7e6def7fcf5255fceb49e9462c30769dc80b4fc0
HoshinoTented <hoshinotented@qq.com>
2024-12-06 09:29:56 +0800

    match: intro

---
+++ syntax/src/main/java/org/aya/syntax/core/term/MatchTerm.java
@@ -0,0 +1,34 @@
+// Copyright (c) 2020-2024 Tesla (Yinsen) Zhang.
+// Use of this source code is governed by the MIT license that can be found in the LICENSE.md file.
+package org.aya.syntax.core.term;
+
+import kala.collection.immutable.ImmutableSeq;
+import kala.function.IndexedFunction;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.function.UnaryOperator;
+
+public record MatchTerm(
+  @NotNull ImmutableSeq<Term> discriminant,
+  @NotNull ImmutableSeq<Term.Matching> clauses
+) implements Term {
+  public @NotNull MatchTerm update(
+    @NotNull ImmutableSeq<Term> discriminant,
+    @NotNull ImmutableSeq<Term.Matching> clauses
+  ) {
+    return this.discriminant.sameElements(discriminant, true)
+      && this.clauses.sameElements(clauses, true)
+      ? this
+      : new MatchTerm(discriminant, clauses);
+  }
+
+  @Override
+  public @NotNull Term descent(@NotNull IndexedFunction<Term, Term> f) {
+    return update(discriminant.map(x -> f.apply(0, x)), clauses.map(clause ->
+      clause.descent(
+        t -> f.apply(clause.bindCount(), t),
+        // TODO: do not supply 0 here, use the number of the bindings introduced by previous patterns
+        p -> p.descent(UnaryOperator.identity(), t -> f.apply(0, t))
+      )));
+  }
+}
--- syntax/src/main/java/org/aya/syntax/core/term/Term.java
+++ syntax/src/main/java/org/aya/syntax/core/term/Term.java
@@ -28,7 +28,7 @@ import java.util.function.Consumer;
 import java.util.function.UnaryOperator;
 
 public sealed interface Term extends Serializable, AyaDocile
-  permits ClassCastTerm, LocalTerm, Callable, BetaRedex, Formation, StableWHNF, TyckInternal, CoeTerm {
+  permits ClassCastTerm, LocalTerm, MatchTerm, Callable, BetaRedex, Formation, StableWHNF, TyckInternal, CoeTerm {
 
   @Override default @NotNull Doc toDoc(@NotNull PrettierOptions options) {
     return new CorePrettier(options).term(BasePrettier.Outer.Free, this);
