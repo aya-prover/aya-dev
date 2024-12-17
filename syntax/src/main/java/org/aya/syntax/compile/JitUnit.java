8fb71d9173484af52086b86be204d86aca3e6f62
ice1000 <ice1000kotlin@foxmail.com>
2024-12-17 08:27:58 -0500

    jit: move some implementations to `JitUnit`

--- syntax/src/main/java/org/aya/syntax/compile/JitUnit.java
+++ syntax/src/main/java/org/aya/syntax/compile/JitUnit.java
@@ -13,10 +13,6 @@ import org.jetbrains.annotations.NotNull;
 public abstract class JitUnit {
   private CompiledAya metadata;
 
-  public JitUnit() {
-
-  }
-
   public @NotNull final CompiledAya metadata() {
     if (metadata == null) metadata = getClass().getAnnotation(CompiledAya.class);
     if (metadata == null) throw new Panic("No @CompiledAya on " + getClass().getName());
@@ -24,13 +20,22 @@ public abstract class JitUnit {
   }
 
   public @NotNull String name() { return metadata.name(); }
+  public @NotNull ModulePath module() { return new ModulePath(rawModule()); }
 
-  public @NotNull ModulePath module() {
-    return new ModulePath(ImmutableArray.Unsafe.wrap(metadata().module()));
+  private @NotNull ImmutableArray<String> rawModule() {
+    return ImmutableArray.Unsafe.wrap(metadata().module());
   }
 
+  /**
+   * @implNote use {@link #metadata} after {@link #metadata()} being called is safe,
+   * because {@link #metadata} is initialized in {@link #metadata()}.
+   */
   public @NotNull QName qualifiedName() {
     var module = module();
     return new QName(new QPath(module, metadata.fileModuleSize()), name());
+  }
+
+  public @NotNull ModulePath fileModule() {
+    return new ModulePath(rawModule().take(metadata().fileModuleSize()));
   }
 }
