2f9bdc7cc565bdcc043155188c9a0179bab7b5a7
HoshinoTented <hoshinotented@qq.com>
2024-12-17 19:50:53 +0800

    jit: question

--- syntax/src/main/java/org/aya/syntax/compile/JitUnit.java
+++ syntax/src/main/java/org/aya/syntax/compile/JitUnit.java
@@ -9,6 +9,7 @@ import org.aya.syntax.ref.QPath;
 import org.aya.util.error.Panic;
 import org.jetbrains.annotations.NotNull;
 
+// TODO: make a module a JitUnit?
 public abstract class JitUnit {
   private CompiledAya metadata;
 
