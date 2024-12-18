1cd7c67f471a8cf302d4865caadd5ea26529ed5d
HoshinoTented <hoshinotented@qq.com>
2024-12-19 05:51:52 +0800

    fix: module-info

--- jit-compiler/src/main/java/module-info.java
+++ jit-compiler/src/main/java/module-info.java
@@ -8,7 +8,6 @@ module aya.compiler {
   exports org.aya.compiler;
   exports org.aya.compiler.free;
   exports org.aya.compiler.free.data;
-  exports org.aya.compiler.free.morphism;
   exports org.aya.compiler.serializers;
   exports org.aya.compiler.free.morphism.source;
 }
