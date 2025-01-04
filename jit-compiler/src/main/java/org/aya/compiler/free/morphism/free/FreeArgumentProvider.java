fc0f4ae983791d875ae131de2c54540dbb671f4e
ice1000 <ice1000kotlin@foxmail.com>
2025-01-04 12:12:46 -0500

    free: fix one test

--- jit-compiler/src/main/java/org/aya/compiler/free/morphism/free/FreeArgumentProvider.java
+++ jit-compiler/src/main/java/org/aya/compiler/free/morphism/free/FreeArgumentProvider.java
@@ -1,4 +1,4 @@
-// Copyright (c) 2020-2024 Tesla (Yinsen) Zhang.
+// Copyright (c) 2020-2025 Tesla (Yinsen) Zhang.
 // Use of this source code is governed by the MIT license that can be found in the LICENSE.md file.
 package org.aya.compiler.free.morphism.free;
 
@@ -8,22 +8,19 @@ import org.aya.compiler.free.data.LocalVariable;
 import org.jetbrains.annotations.NotNull;
 
 public record FreeArgumentProvider(int paramCount) implements ArgumentProvider {
-  @Override
-  public @NotNull LocalVariable arg(int nth) {
+  @Override public @NotNull LocalVariable arg(int nth) {
     assert nth < paramCount;
     return new FreeVariable.Arg(nth);
   }
 
   record Lambda(int captureCount, int paramCount) implements ArgumentProvider.Lambda {
-    @Override
-    public @NotNull FreeJavaExpr capture(int nth) {
+    @Override public @NotNull FreeJavaExpr capture(int nth) {
       assert nth < captureCount;
       return new FreeExpr.RefCapture(nth);
     }
 
-    @Override
-    public @NotNull LocalVariable arg(int nth) {
-      assert nth + captureCount < paramCount;
+    @Override public @NotNull LocalVariable arg(int nth) {
+      assert nth < paramCount;
       return new FreeVariable.Arg(nth);
     }
   }
