050171c4432193af1820eee77271733da703d9b6
ice1000 <ice1000kotlin@foxmail.com>
2025-01-04 08:59:47 -0500

    pat: code cleanup

--- syntax/src/main/java/org/aya/syntax/core/pat/PatToTerm.java
+++ syntax/src/main/java/org/aya/syntax/core/pat/PatToTerm.java
@@ -1,4 +1,4 @@
-// Copyright (c) 2020-2024 Tesla (Yinsen) Zhang.
+// Copyright (c) 2020-2025 Tesla (Yinsen) Zhang.
 // Use of this source code is governed by the MIT license that can be found in the LICENSE.md file.
 package org.aya.syntax.core.pat;
 
@@ -12,32 +12,23 @@ import org.aya.syntax.ref.LocalCtx;
 import org.aya.util.error.Panic;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.function.Consumer;
 import java.util.function.Function;
 
 public interface PatToTerm {
   static @NotNull Term visit(@NotNull Pat pat) {
-    return new Unary(_ -> { }).apply(pat);
-  }
-  record Unary(@NotNull Consumer<Pat.Bind> freshCallback) implements Function<Pat, Term> {
-    @Override public Term apply(Pat pat) {
-      return switch (pat) {
-        case Pat.Misc misc -> switch (misc) {
-          // We expect this to be never used, but this needs to not panic because
-          // absurd clauses need to finish type checking
-          case Absurd -> SortTerm.Type0;
-          case UntypedBind -> Panic.unreachable();
-        };
-        case Pat.Bind bind -> {
-          freshCallback.accept(bind);
-          yield new FreeTerm(bind.bind());
-        }
-        case Pat.Con con -> new ConCall(con.head(), con.args().map(this));
-        case Pat.Tuple(var l, var r) -> new TupTerm(apply(l), apply(r));
-        case Pat.Meta meta -> new MetaPatTerm(meta);
-        case Pat.ShapedInt si -> si.toTerm();
+    return switch (pat) {
+      case Pat.Misc misc -> switch (misc) {
+        // We expect this to be never used, but this needs to not panic because
+        // absurd clauses need to finish type checking
+        case Absurd -> SortTerm.Type0;
+        case UntypedBind -> Panic.unreachable();
       };
-    }
+      case Pat.Bind bind -> new FreeTerm(bind.bind());
+      case Pat.Con con -> new ConCall(con.head(), con.args().map(PatToTerm::visit));
+      case Pat.Tuple(var l, var r) -> new TupTerm(visit(l), visit(r));
+      case Pat.Meta meta -> new MetaPatTerm(meta);
+      case Pat.ShapedInt si -> si.toTerm();
+    };
   }
 
   record Monadic(@NotNull LocalCtx ctx) implements Function<Pat, ImmutableSeq<Term>> {
@@ -69,11 +60,10 @@ public interface PatToTerm {
           ctx.put(bind.bind(), bind.type());
           yield ImmutableSeq.of(new FreeTerm(bind.bind()));
         }
-        case Pat.Con con when con.ref().hasEq() ->
-          list(con.args().view().dropLast(1), BOUNDARIES)
-            .map(args -> {
-              return new ConCall(con.head(), args);
-            });
+        case Pat.Con con when con.ref().hasEq() -> list(con.args().view().dropLast(1), BOUNDARIES)
+          .map(args -> {
+            return new ConCall(con.head(), args);
+          });
         case Pat.Con con -> list(con.args().view())
           .map(args -> {
             return new ConCall(con.head(), args);
