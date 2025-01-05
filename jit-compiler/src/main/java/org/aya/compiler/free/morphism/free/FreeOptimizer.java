f7ad7fd9385e82cd92eb2ba3520722eb81c36a7f
ice1000 <ice1000kotlin@foxmail.com>
2025-01-05 05:43:11 -0500

    jit: prepare for #1276

--- jit-compiler/src/main/java/org/aya/compiler/free/morphism/free/FreeOptimizer.java
+++ jit-compiler/src/main/java/org/aya/compiler/free/morphism/free/FreeOptimizer.java
@@ -2,6 +2,7 @@
 // Use of this source code is governed by the MIT license that can be found in the LICENSE.md file.
 package org.aya.compiler.free.morphism.free;
 
+import kala.collection.SeqView;
 import kala.collection.immutable.ImmutableSeq;
 import org.aya.compiler.free.morphism.free.FreeDecl.Clazz;
 import org.aya.compiler.free.morphism.free.FreeDecl.ConstantField;
@@ -20,39 +21,45 @@ public interface FreeOptimizer {
         yield new Clazz(metadata, owner, nested, superclass, newMembers);
       }
       case ConstantField field -> field;
-      case Method(var signature, var body) -> new Method(signature, body.flatMap(FreeOptimizer::optimize));
+      case Method(var signature, var body) -> new Method(signature, optimizeBlock(body, false));
     };
   }
 
-  static @NotNull ImmutableSeq<FreeStmt> optimize(FreeStmt stmt) {
+  static @NotNull ImmutableSeq<FreeStmt> optimizeBlock(ImmutableSeq<FreeStmt> block, boolean endOfBreakable) {
+    if (!endOfBreakable) return block.flatMap(it -> optimize(it, false));
+    var exceptLast = block.view().dropLast(1).flatMap(it -> optimize(it, false));
+    var last = optimize(block.getLast(), true);
+    return exceptLast.concat(last).toImmutableSeq();
+  }
+
+  static @NotNull SeqView<FreeStmt> optimize(FreeStmt stmt, boolean endOfBreakable) {
     return switch (stmt) {
       case FreeStmt.Switch(var elim, var cases, var branch, var defaultCase) -> {
-        branch = branch.map(it -> it.flatMap(FreeOptimizer::optimize));
-        defaultCase = defaultCase.flatMap(FreeOptimizer::optimize);
-        if (branch.isEmpty()) yield defaultCase;
+        branch = branch.map(it -> optimizeBlock(it, endOfBreakable));
+        defaultCase = optimizeBlock(defaultCase, endOfBreakable);
+        if (branch.isEmpty()) yield defaultCase.view();
         if (defaultCase.sizeEquals(1) && defaultCase.getFirst() == FreeStmt.Unreachable.INSTANCE) {
           if (branch.sizeEquals(1)) {
-            yield branch.getFirst();
+            yield branch.getFirst().view();
           } else if (branch.sizeEquals(2)) {
-            yield ImmutableSeq.of(new FreeStmt.IfThenElse(
+            yield SeqView.of(new FreeStmt.IfThenElse(
               new FreeStmt.Condition.IsIntEqual(new FreeExpr.RefVariable(elim), cases.getFirst()),
               branch.getFirst(),
               branch.getLast()
             ));
           } else if (branch.sizeGreaterThan(1)) {
-            yield ImmutableSeq.of(new FreeStmt.Switch(elim, cases.dropLast(1), branch.dropLast(1), branch.getLast()));
+            yield SeqView.of(new FreeStmt.Switch(elim, cases.dropLast(1), branch.dropLast(1), branch.getLast()));
           }
         }
-        yield ImmutableSeq.of(new FreeStmt.Switch(elim, cases, branch, defaultCase));
+        yield SeqView.of(new FreeStmt.Switch(elim, cases, branch, defaultCase));
       }
-      case FreeStmt.Breakable(var stmts) ->
-        ImmutableSeq.of(new FreeStmt.Breakable(stmts.flatMap(FreeOptimizer::optimize)));
+      case FreeStmt.Breakable(var stmts) -> SeqView.of(new FreeStmt.Breakable(optimizeBlock(stmts, true)));
       case FreeStmt.IfThenElse(var cond, var thenBlock, var elseBlock) -> {
-        var newThenBlock = thenBlock.flatMap(FreeOptimizer::optimize);
-        var newElseBlock = elseBlock == null ? null : elseBlock.flatMap(FreeOptimizer::optimize);
-        yield ImmutableSeq.of(new FreeStmt.IfThenElse(cond, newThenBlock, newElseBlock));
+        var newThenBlock = optimizeBlock(thenBlock, endOfBreakable);
+        var newElseBlock = elseBlock == null ? null : optimizeBlock(elseBlock, endOfBreakable);
+        yield SeqView.of(new FreeStmt.IfThenElse(cond, newThenBlock, newElseBlock));
       }
-      default -> ImmutableSeq.of(stmt);
+      default -> SeqView.of(stmt);
     };
   }
 }
