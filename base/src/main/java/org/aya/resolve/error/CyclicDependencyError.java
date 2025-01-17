c76c7da48bbb43025a166152cdb5730ff49118b9
澪 <mio@mio19.uk>
2025-01-17 15:37:56 +1100

    resolve: save dependencies in GeneralizedVar; print cycle

--- base/src/main/java/org/aya/resolve/error/CyclicDependencyError.java
+++ base/src/main/java/org/aya/resolve/error/CyclicDependencyError.java
@@ -8,17 +8,21 @@ import org.aya.util.reporter.Problem;
 import org.aya.syntax.ref.GeneralizedVar;
 import org.aya.util.error.SourcePos;
 import org.jetbrains.annotations.NotNull;
+import kala.collection.immutable.ImmutableSeq;
 
 public record CyclicDependencyError(
   @NotNull SourcePos sourcePos,
-  @NotNull GeneralizedVar var
+  @NotNull GeneralizedVar var,
+  @NotNull ImmutableSeq<GeneralizedVar> cyclePath
 ) implements Problem {
   @Override public @NotNull Severity level() { return Severity.ERROR; }
   @Override public @NotNull Stage stage() { return Stage.RESOLVE; }
   @Override public @NotNull Doc describe(@NotNull PrettierOptions options) {
-    return Doc.sep(
-      Doc.plain("Cyclic dependency detected in variable declaration:"),
-      Doc.plain(var.name())
+    return Doc.vcat(
+      Doc.plain("Cyclic dependency detected in variable declarations:"),
+      Doc.plain(String.join(" -> ", cyclePath.view()
+        .map(GeneralizedVar::name)
+        .toImmutableSeq()))
     );
   }
 }
