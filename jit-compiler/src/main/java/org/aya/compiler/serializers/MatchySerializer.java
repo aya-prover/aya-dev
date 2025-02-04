61842058245330c4c699d71fb29ad91850e0b1e3
HoshinoTented <hoshinotented@qq.com>
2025-02-04 19:20:45 +0800

    misc: fuse kala operation

--- jit-compiler/src/main/java/org/aya/compiler/serializers/MatchySerializer.java
+++ jit-compiler/src/main/java/org/aya/compiler/serializers/MatchySerializer.java
@@ -1,4 +1,4 @@
-// Copyright (c) 2020-2024 Tesla (Yinsen) Zhang.
+// Copyright (c) 2020-2025 Tesla (Yinsen) Zhang.
 // Use of this source code is governed by the MIT license that can be found in the LICENSE.md file.
 package org.aya.compiler.serializers;
 
@@ -73,7 +73,10 @@ public class MatchySerializer extends ClassTargetSerializer<MatchySerializer.Mat
     var matching = unit.clauses().map(clause ->
       new PatternSerializer.Matching(clause.bindCount(), clause.patterns(),
         (ps, cb, binds) -> {
-          var resultSeq = ps.result.take(binds).map(LocalVariable::ref);
+          var resultSeq = ps.result.view()
+            .take(binds)
+            .map(LocalVariable::ref)
+            .toImmutableSeq();
           var fullSeq = resultSeq.appendedAll(captureExprs);
           // assert fullSeq.size() == binds;
           var returns = serializeTermUnderTele(cb, clause.body(), fullSeq);
