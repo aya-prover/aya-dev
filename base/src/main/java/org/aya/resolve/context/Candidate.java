a3e01371cd6a3b88017873071989b293c5803841
ice1000 <ice1000kotlin@foxmail.com>
2025-02-11 21:17:40 -0500

    kala: `findFirst`

--- base/src/main/java/org/aya/resolve/context/Candidate.java
+++ base/src/main/java/org/aya/resolve/context/Candidate.java
@@ -89,21 +89,11 @@ public sealed interface Candidate<T> {
       if (view.sizeGreaterThan(1)) Panic.unreachable();
       return symbols.valuesView().getAny();
     }
-
-    @Override
-    public CollectionView<T> getAll() {
-      return symbols.valuesView();
-    }
-
-    @Override
-    public @NotNull ImmutableSeq<ModuleName> from() {
-      return ImmutableSeq.from(symbols.keysView());
-    }
-
+    @Override public CollectionView<T> getAll() { return symbols.valuesView(); }
+    @Override public @NotNull ImmutableSeq<ModuleName> from() { return ImmutableSeq.from(symbols.keysView()); }
     @Override public boolean contains(@NotNull ModuleName modName) {
       return modName instanceof ModuleName.Qualified qmod && symbols.containsKey(qmod);
     }
-
     @Override public @NotNull Candidate<T> merge(@NotNull Candidate<T> candy) {
       return switch (candy) {
         case Candidate.Defined<T> v -> v;
