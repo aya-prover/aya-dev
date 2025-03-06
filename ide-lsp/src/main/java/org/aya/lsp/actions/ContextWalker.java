e7b056b346907429cf0fb3b77e4d614ac94fe3fb
HoshinoTented <hoshinotented@qq.com>
2025-03-06 21:17:43 +0800

    context-walker: visit necessary variables

--- ide-lsp/src/main/java/org/aya/lsp/actions/ContextWalker.java
+++ ide-lsp/src/main/java/org/aya/lsp/actions/ContextWalker.java
@@ -2,21 +2,30 @@
 // Use of this source code is governed by the MIT license that can be found in the LICENSE.md file.
 package org.aya.lsp.actions;
 
+import kala.collection.SeqView;
 import kala.collection.immutable.ImmutableSeq;
 import kala.collection.mutable.MutableList;
 import org.aya.ide.syntax.SyntaxNodeAction;
 import org.aya.ide.util.XY;
+import org.aya.syntax.concrete.Expr;
 import org.aya.syntax.concrete.Pattern;
 import org.aya.syntax.concrete.stmt.Command;
 import org.aya.syntax.concrete.stmt.ModuleName;
 import org.aya.syntax.concrete.stmt.Stmt;
 import org.aya.syntax.ref.AnyVar;
+import org.aya.syntax.ref.GenerateKind;
+import org.aya.syntax.ref.LocalVar;
 import org.aya.util.position.SourcePos;
+import org.aya.util.position.WithPos;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /// ContextWalker traversal the concrete syntax tree to target position, record all available variable.
 public class ContextWalker implements SyntaxNodeAction.Cursor {
   public record ConcreteJdg(@NotNull AnyVar var, @NotNull Type userType) { }
+
+  private static final @NotNull LocalVar RESULT_VAR = new LocalVar("_", SourcePos.NONE);
+
   private final @NotNull MutableList<ConcreteJdg> localContext;
   private final @NotNull MutableList<String> moduleContext;
   private final @NotNull XY xy;
@@ -49,6 +58,21 @@ public class ContextWalker implements SyntaxNodeAction.Cursor {
     }
 
     Cursor.super.doAccept(stmt);
+  }
+
+  @Override
+  public void visitTelescope(@NotNull SeqView<Expr.Param> params, @Nullable WithPos<Expr> result) {
+    var telescope = params;
+    if (result != null) telescope = telescope.appended(new Expr.Param(result.sourcePos(), RESULT_VAR, result, true));
+
+    var idx = telescope.indexWhere(it -> accept(xy, it.sourcePos()));
+    if (idx != -1) {
+      telescope = telescope.take(idx);
+    }
+
+    // the key is skipping the variable that are not accessible, the expr doesn't matter,
+    // as they will be skipped by [visitExpr] if the cursor is not inside
+    Cursor.super.visitTelescope(telescope, result);
   }
 
   // endregion Context Restriction
