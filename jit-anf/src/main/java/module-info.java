47c60edb1a07597e5fbaf62d5257b788edcdf5d9
Linxuan M <davidmaamoaix@gmail.com>
2025-04-13 03:15:24 -0400

    anf: init module

---
+++ jit-anf/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module aya.jit {
+  requires aya.base;
+  requires aya.util;
+  requires kala.collection.primitive;
+
+  requires static org.jetbrains.annotations;
+
+  exports org.aya.jit.anf;
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
-// Copyright (c) 2020-2024 Tesla (Yinsen) Zhang.
+// Copyright (c) 2020-2025 Tesla (Yinsen) Zhang.
 // Use of this source code is governed by the MIT license that can be found in the LICENSE.md file.
 
 rootProject.name = "aya-prover"
@@ -12,6 +12,7 @@ include(
   "cli-console",
   "tools",
   // Uses kala-primitives
+  "jit-anf",
   "jit-compiler",
   "tools-kala",
   "tools-md",
