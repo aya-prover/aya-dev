2e9c6475a27b1a7d5586b25d4c289256c63740fd
Linxuan M <davidmaamoaix@gmail.com>
2025-06-27 12:34:05 -0400

    anf: rename

--- anf/src/test/java/CompilerTestCase.java
+++ anf/src/test/java/CompilerTestCase.java
@@ -2,9 +2,9 @@
 // Use of this source code is governed by the MIT license that can be found in the LICENSE.md file.
 import org.jetbrains.annotations.NotNull;
 
-public class CompilerTest {
+public class CompilerTestCase {
 
   private final @NotNull String sourcePath;
 
-  public CompilerTest(@NotNull String sourceFile) { sourcePath = sourceFile; }
+  public CompilerTestCase(@NotNull String sourceFile) { sourcePath = sourceFile; }
 }
--- anf/src/test/java/TestCaseSource.java
+++ anf/src/test/java/TestCaseSource.java
@@ -4,17 +4,17 @@ import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 
-public sealed interface TestSource permits TestSource.TestFile, TestSource.NoFile {
+public sealed interface TestCaseSource permits TestCaseSource.TestFile, TestCaseSource.NoFile {
 
   @NotNull String readableSource();
   @NotNull String code() throws IOException;
 
-  record TestFile(@NotNull String path) implements TestSource {
+  record TestFile(@NotNull String path) implements TestCaseSource {
     @Override
     public @NotNull String readableSource() { return path; }
     @Override
     public @NotNull String code() throws IOException {
-      var stream = TestSource.class.getResourceAsStream(path);
+      var stream = TestCaseSource.class.getResourceAsStream(path);
       assert stream != null;
       var code = new String(stream.readAllBytes());
       stream.close();
@@ -22,7 +22,7 @@ public sealed interface TestSource permits TestSource.TestFile, TestSource.NoFil
     }
   }
 
-  record NoFile(@NotNull String code) implements TestSource {
+  record NoFile(@NotNull String code) implements TestCaseSource {
     @Override
     public @NotNull String readableSource() { return "<no source>"; }
   }
