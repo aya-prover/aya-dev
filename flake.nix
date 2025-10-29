080980b64fb4fea81701cbf8825fb0b5ce9f69e6
definfo <hjsdbb1@gmail.com>
2025-10-29 17:52:53 +0800

    nix: optimize nixpkgs import settings, init direnv config
    We can later switch to jdk25 without touching flake inputs

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     # Provide jdk22
-    nixpkgs-jdk.url = "github:NixOS/nixpkgs/release-24.05";
+    nixpkgs-24_05.url = "github:NixOS/nixpkgs/release-24.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -12,19 +12,26 @@
     {
       self,
       nixpkgs,
-      nixpkgs-jdk,
+      nixpkgs-24_05,
       flake-utils,
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
-        pkgs-jdk = nixpkgs-jdk.legacyPackages.${system};
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            # workaround for JDK 22 deprecation in latest nixpkgs
+            (self: super: {
+              jdk22 = nixpkgs-24_05.legacyPackages.${system}.jdk22;
+            })
+          ];
+        };
 
         # Parse gradle/libs.versions.toml for required project/jdk versions
         inherit (builtins.fromTOML (builtins.readFile ./gradle/libs.versions.toml)) versions;
 
-        jdk = pkgs-jdk."jdk${versions.java}";
+        jdk = pkgs."jdk${versions.java}";
         gradle = pkgs.gradle_9;
       in
       {
