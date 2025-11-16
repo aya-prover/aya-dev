fb9822e39f8d7303ddefd11de293bbbb76e0e9c4
HoshinoTented <hoshinotented@qq.com>
2025-11-16 17:13:32 +0800

    instance: something

--- syntax/src/main/java/org/aya/generic/Instance.java
+++ syntax/src/main/java/org/aya/generic/Instance.java
@@ -5,8 +5,10 @@ package org.aya.generic;
 import kala.collection.SeqView;
 import org.aya.pretty.doc.Doc;
 import org.aya.syntax.core.annotation.Bound;
+import org.aya.syntax.core.annotation.NoInherit;
 import org.aya.syntax.core.def.FnDefLike;
 import org.aya.syntax.core.term.FreeTermLike;
+import org.aya.syntax.core.term.Term;
 import org.aya.syntax.core.term.call.ClassCall;
 import org.aya.syntax.ref.LocalVar;
 import org.aya.util.PrettierOptions;
@@ -14,11 +16,18 @@ import org.jetbrains.annotations.NotNull;
 
 public sealed interface Instance extends AyaDocile {
   @Bound @NotNull Instance bindTele(@NotNull SeqView<LocalVar> vars);
+  @NoInherit @NotNull Instance instTele(@NotNull SeqView<Term> tele);
 
+  // TODO: seems incorrect, [ref] should be able to be bind so that it refer to meta args
   record Local(@NotNull FreeTermLike ref, @Override @NotNull ClassCall type) implements Instance {
     @Override
     public @NotNull Instance bindTele(@NotNull SeqView<LocalVar> vars) {
       return new Local(ref, (ClassCall) type.bindTele(vars));
+    }
+
+    @Override
+    public @NoInherit @NotNull Instance instTele(@NotNull SeqView<Term> tele) {
+      return new Local(ref, (ClassCall) type.instTele(tele));
     }
   }
 
@@ -26,6 +35,11 @@ public sealed interface Instance extends AyaDocile {
   record Global(@NotNull FnDefLike def) implements Instance {
     @Override
     public @NotNull Instance bindTele(@NotNull SeqView<LocalVar> vars) {
+      return this;
+    }
+
+    @Override
+    public @NoInherit @NotNull Instance instTele(@NotNull SeqView<Term> tele) {
       return this;
     }
   }
