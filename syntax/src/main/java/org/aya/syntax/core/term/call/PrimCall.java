331eb3c627821a68bcb15882c4c7f6289c19b86c
ice1000 <ice1000kotlin@foxmail.com>
2025-12-09 22:51:38 -0500

    test: fix last error (all this time we've never ever serialized a primcall is CRAZY)

--- syntax/src/main/java/org/aya/syntax/core/term/call/PrimCall.java
+++ syntax/src/main/java/org/aya/syntax/core/term/call/PrimCall.java
@@ -4,11 +4,8 @@ package org.aya.syntax.core.term.call;
 
 import kala.collection.immutable.ImmutableSeq;
 import org.aya.generic.TermVisitor;
-import org.aya.syntax.concrete.stmt.decl.PrimDecl;
-import org.aya.syntax.core.def.PrimDef;
 import org.aya.syntax.core.def.PrimDefLike;
 import org.aya.syntax.core.term.Term;
-import org.aya.syntax.ref.DefVar;
 import org.jetbrains.annotations.NotNull;
 
 public record PrimCall(
@@ -23,12 +20,5 @@ public record PrimCall(
 
   @Override public @NotNull Term descent(@NotNull TermVisitor visitor) {
     return update(Callable.descent(args, visitor));
-  }
-
-  public PrimCall(
-    @NotNull DefVar<@NotNull PrimDef, PrimDecl> ref,
-    int ulift, @NotNull ImmutableSeq<@NotNull Term> args
-  ) {
-    this(new PrimDef.Delegate(ref), ulift, args);
   }
 }
