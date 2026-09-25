030cf25db924886d628a0b9f8f836eaf6680e7db
ice1000 <ice1000kotlin@gmail.com>
2026-09-25 02:14:31 -0400

    build: new cli args for jdk27

--- ide-lsp/src/main/shell/aya-lsp.bat
+++ ide-lsp/src/main/shell/aya-lsp.bat
@@ -1,6 +1,9 @@
 @echo off
 set DIR="%~dp0"
 set JAVA_EXEC="%DIR:"=%\..\jre\bin\java"
-set CDS_JVM_OPTS=%CDS_JVM_OPTS% -XX:+UseCompactObjectHeaders --enable-native-access=org.aya.prover.merged.module
+set CDS_JVM_OPTS=%CDS_JVM_OPTS% -XX:+UseCompactObjectHeaders ^
+  --enable-native-access=org.jline.terminal.ffm ^
+  --enable-native-access=org.jline.nativ ^
+  --enable-final-field-mutation=com.google.gson
 
 %JAVA_EXEC% %CDS_JVM_OPTS% --enable-preview -p "%~dp0/../app" -m aya.ide.lsp/org.aya.lsp.LspMain %*
