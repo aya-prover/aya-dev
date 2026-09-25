e22993d97b307daeebeb2f62665d51aa6edd54fc
ice1000 <ice1000kotlin@gmail.com>
2026-09-25 03:08:13 -0400

    test: extensively test cofibration equality

--- base/src/test/java/org/aya/tyck/CofConversionTest.java
+++ base/src/test/java/org/aya/tyck/CofConversionTest.java
@@ -5,22 +5,12 @@ package org.aya.tyck;
 import kala.collection.immutable.ImmutableSeq;
 import org.aya.normalize.Normalizer;
 import org.aya.prettier.AyaPrettierOptions;
-import org.aya.prettier.BasePrettier;
 import org.aya.prettier.CorePrettier;
-import org.aya.states.TyckState;
-import org.aya.syntax.SyntaxTestUtil;
 import org.aya.syntax.core.def.FnDef;
-import org.aya.syntax.core.def.PrimDef;
-import org.aya.syntax.core.term.Term;
+import org.aya.syntax.core.term.xtt.CofNF;
 import org.aya.syntax.ref.LocalVar;
-import org.aya.syntax.ref.MapLocalCtx;
 import org.aya.tyck.tycker.Stateful;
-import org.aya.unify.Unifier;
-import org.aya.util.Decision;
-import org.aya.util.Ordering;
-import org.aya.util.position.SourcePos;
-import org.jspecify.annotations.NonNull;
-import org.junit.jupiter.api.Assertions;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -54,39 +44,85 @@ public class CofConversionTest {
       def test2L (i j k : I) => (i =f k) ∧f (k =f j)
       def test2R (i j : I) => i =f j
       
-      // case 3: distributivity of ∧f over ∨f
-      def test3L (φ ψ χ : Cof) => φ ∧f (ψ ∨f χ)
-      def test3R (φ ψ χ : Cof) => (φ ∧f ψ) ∨f (φ ∧f χ)
+      // case 3: distributivity of ∧f over ∨f, using an equality just to make it more complicated
+      def test3L (φ ψ : Cof) (i j : I) => φ ∧f (ψ ∨f (i =f j))
+      def test3R (φ ψ : Cof) (i j : I) => (φ ∧f ψ) ∨f (φ ∧f (i =f j))
       
       // case 4: distributivity of ∧f over ∨f (other direction)
-      def test4L (φ ψ χ : Cof) => (φ ∧f ψ) ∨f (φ ∧f χ)
-      def test4R (φ ψ χ : Cof) => φ ∧f (ψ ∨f χ)
+      def test4L (φ ψ : Cof) (i j : I) => (φ ∧f ψ) ∨f (φ ∧f (i =f j))
+      def test4R (φ ψ : Cof) (i j : I) => φ ∧f (ψ ∨f (i =f j))
+      
+      // case 5: transitivity of ∧f
+      def test5L (φ ψ χ : Cof) => (φ ∧f ψ) ∧f χ
+      def test5R (φ ψ χ : Cof) => φ ∧f (ψ ∧f χ)
+      
+      // case 6: transitivity of ∨f
+      def test6L (φ ψ χ : Cof) => (φ ∨f ψ) ∨f χ
+      def test6R (φ ψ χ : Cof) => φ ∨f (ψ ∨f χ)
+      
+      // case 7: exfalso
+      def test7L (i : I) => (i =f 0) ∧f (i =f 1)
+      def test7R (i : I) (φ : Cof) => φ
+      
+      // case 8: weakening
+      def test8L (φ : Cof) => φ
+      def test8R (φ : Cof) => φ ∧f φ
+      
+      // cocase 1: not all propositions are true
+      def cotest1L (i : I) => (i =f 1)
+      def cotest1R (i : I) (φ : Cof) => φ
+      
+      // cocase 2: the reversed version of transitivity is not true, because there is no knowledge about k
+      def cotest2L (i j : I) => i =f j
+      def cotest2R (i j k : I) => (i =f k) ∧f (k =f j)
       """);
   }
 
   @ParameterizedTest
+  @ValueSource(strings = {"1", "2", "3", "4", "5", "6", "7", "8"})
+  public void testCofImplies(String caseNum) {
+    var result = computeImplication(caseNum, "test");
+    if (!result.implies()) {
+      var prettier = new CorePrettier(AyaPrettierOptions.debug());
+      fail("In test" + caseNum + "L and R, the proposition (" +
+        prettier.visitCofDisj(result.left()).debugRender() + ") does not imply (" +
+        prettier.visitCofDisj(result.right()).debugRender() + ")");
+    }
+  }
+
+  @ParameterizedTest
   @ValueSource(strings = {"1", "2"})
-  public void testCofConversion(String caseNum) {
-    var testL = (FnDef) definfo.defs().find(d -> d.ref().name().equals("test" + caseNum + "L")).get();
-    var testR = (FnDef) definfo.defs().find(d -> d.ref().name().equals("test" + caseNum + "R")).get();
+  public void testCofNotImplies(String caseNum) {
+    var result = computeImplication(caseNum, "cotest");
+    if (result.implies()) {
+      var prettier = new CorePrettier(AyaPrettierOptions.debug());
+      fail("In cotest" + caseNum + "L and R, the proposition (" +
+        prettier.visitCofDisj(result.left()).debugRender() + ") incorrectly implied (" +
+        prettier.visitCofDisj(result.right()).debugRender() + ")");
+    }
+  }
+
+  private static @NotNull CofConversionTest.ImplicationResult computeImplication(String caseNum, String prefix) {
+    var testL = (FnDef) definfo.defs().find(d -> d.ref().name().equals(prefix + caseNum + "L")).get();
+    var testR = (FnDef) definfo.defs().find(d -> d.ref().name().equals(prefix + caseNum + "R")).get();
     var paramsSize = Math.max(testL.telescope().size(), testR.telescope().size());
     var params = ImmutableSeq.fill(paramsSize, i -> new LocalVar(Character.toString('a' + i))).view();
-    var normalizer = new Normalizer(definfo.info().makeTyckState());
 
     var leftTerm = testL.body().getLeftValue().instTeleVar(params.take(testL.telescope().size()));
     var rightTerm = testR.body().getLeftValue().instTeleVar(params.take(testR.telescope().size()));
 
+    var normalizer = new Normalizer(definfo.info().makeTyckState());
     var left = normalizer.expand(leftTerm);
     var right = normalizer.expand(rightTerm);
     assertNotNull(left);
     assertNotNull(right);
 
     Stateful tool = () -> normalizer.state;
-    if (!tool.withConnection(left, () -> tool.state().isTrue(right))) {
-      var prettier = new CorePrettier(AyaPrettierOptions.debug());
-      fail("In test" + caseNum + "L and R, the proposition (" +
-        prettier.visitCofDisj(left).debugRender() + ") does not imply (" +
-        prettier.visitCofDisj(right).debugRender() + ")");
-    }
+    return new ImplicationResult(left, right, tool.withConnection(left, () -> tool.state().isTrue(right)));
   }
+
+  private record ImplicationResult(
+    CofNF.OrVar<CofNF.Disj> left,
+    CofNF.OrVar<CofNF.Disj> right, boolean implies
+  ) { }
 }
