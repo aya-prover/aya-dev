651ff01d2410691811bae1b697d40a55a66904be
ice1000 <ice1000kotlin@gmail.com>
2026-09-25 03:57:16 -0400

    build: fewer warnings to suppress

--- ide-lsp/src/main/shell/aya-lsp.bat
+++ ide-lsp/src/main/shell/aya-lsp.bat
@@ -3,7 +3,6 @@ set DIR="%~dp0"
 set JAVA_EXEC="%DIR:"=%\..\jre\bin\java"
 set CDS_JVM_OPTS=%CDS_JVM_OPTS% -XX:+UseCompactObjectHeaders ^
   --enable-native-access=org.jline.terminal.ffm ^
-  --enable-native-access=org.jline.nativ ^
-  --enable-final-field-mutation=com.google.gson
+  --enable-native-access=org.jline.nativ
 
 %JAVA_EXEC% %CDS_JVM_OPTS% --enable-preview -p "%~dp0/../app" -m aya.ide.lsp/org.aya.lsp.LspMain %*
